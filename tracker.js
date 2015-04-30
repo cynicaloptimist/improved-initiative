280efcb760600a2a29db5186f3302b738354d149
Evan Bailey <baileyevang@gmail.com>
2015-04-29 23:08:02 -0700

    Building Encounter and Creature classes

--- tracker.js
+++ tracker.js
@@ -1,33 +1,38 @@
 var Encounter = function(){
-  this.creatures = new Map();
+  this.creatures = [];
   return this;
 };
+
 Encounter.prototype.addCreature = function(creatureJson){
   console.log("adding %O", creatureJson);
-  var encounterIndex = creatures.push(creatureJson);
+  var encounterIndex = this.creatures.push(creatureJson) - 1;
   var newElement = $('.encounter').append("<p encounter-index='" + encounterIndex + "'>" + creatureJson.Name + "</p>");
   var creature = new Creature(creatureJson);
   newElement.data(creature);
 };
+
 Encounter.prototype.getCreatures = function(filter){
   if(typeof filter === 'function'){
     return this.creatures.filter(filter);
   }
   return this.creatures;
-}
+};
+
 var Creature = function(creatureJson){
   if(!creatureJson){
     throw "Couldn't create Creature- no Json passed in.";
   }
+  this.Name = creatureJson.Name;
   this.MaxHP = creatureJson.HP.Value;
   this.CurrentHP = creatureJson.HP.Value;
 
   this.InitiativeModifier = this.CalculateModifier(creatureJson.Attributes.Dex);
-}
+};
+
 Creature.prototype.CalculateModifier = function(attribute)
 {
-  return Math.floor((attribute - 10) / 5)
-}
+  return Math.floor((attribute - 10) / 2)
+};
 
 encounter = new Encounter();
 chrome.app.window.create('creatures.html', {
