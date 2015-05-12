aaae59fa6ba775920a6c389107a4f76bf59dffc2
Evan Bailey <baileyevang@gmail.com>
2015-05-11 22:27:29 -0700

    Lots of changes

--- tracker.js
+++ tracker.js
@@ -1,10 +1,22 @@
 /// <reference path="typings/jquery/jquery.d.ts" />
 /// <reference path="typings/knockout/knockout.d.ts" />
+/// <reference path="typings/knockout/bindinghandlers.d.ts" />
 /// <reference path="typings/mousetrap/mousetrap.d.ts" />
 var StatBlock = (function () {
     function StatBlock() {
     }
-    StatBlock.Empty = { Name: 'None', HP: { Value: 0 }, Attributes: { Str: 0, Dex: 0, Con: 0, Cha: 0, Int: 0, Wis: 0 } };
+    StatBlock.Empty = function () { return ({
+        Name: '', Type: '',
+        HP: { Value: 1, Notes: '' }, AC: { Value: 10, Notes: '' },
+        Speed: [],
+        Attributes: { Str: 10, Dex: 10, Con: 10, Cha: 10, Int: 10, Wis: 10 },
+        DamageVulnerabilities: [], DamageResistances: [], DamageImmunities: [], ConditionImmunities: [],
+        Saves: [], Skills: [], Senses: [], Languages: [],
+        Challenge: 0,
+        Traits: [],
+        Actions: [],
+        LegendaryActions: []
+    }); };
     return StatBlock;
 })();
 var DefaultRules = (function () {
@@ -44,6 +56,9 @@ var Encounter = (function () {
             console.log("adding %O", creatureJson);
             _this.creatures.push(new Creature(creatureJson, _this));
         };
+        this.RemoveSelectedCreature = function () {
+            _this.creatures.remove(_this.SelectedCreature());
+        };
         this.SelectPreviousCombatant = function () {
             _this.relativeNavigateFocus(-1);
         };
@@ -70,6 +85,11 @@ var Encounter = (function () {
                 _this.moveCreature(creature, index + 1);
             }
         };
+        this.EditSelectedCreatureName = function () {
+            if (_this.SelectedCreature()) {
+                _this.SelectedCreature().EditName(true);
+            }
+        };
         this.RollInitiative = function () {
             if (_this.Rules.GroupSimilarCreatures) {
                 var initiatives = [];
@@ -84,14 +104,28 @@ var Encounter = (function () {
                 _this.creatures().forEach(function (c) { c.RollInitiative(); });
             }
             _this.sortByInitiative();
+            _this.ActiveCreature(_this.creatures()[0]);
         };
+        this.NextTurn = function () {
+            var nextIndex = _this.creatures().indexOf(_this.ActiveCreature()) + 1;
+            if (nextIndex >= _this.creatures().length) {
+                nextIndex = 0;
+            }
+            _this.ActiveCreature(_this.creatures()[nextIndex]);
+        };
+        this.Rules = rules || new DefaultRules();
         this.creatures = ko.observableArray();
         this.SelectedCreature = ko.observable();
-        this.Rules = rules || new DefaultRules();
         this.SelectedCreatureStatblock = ko.computed(function () {
             return _this.SelectedCreature()
                 ? _this.SelectedCreature().StatBlock
-                : StatBlock.Empty;
+                : StatBlock.Empty();
+        });
+        this.ActiveCreature = ko.observable();
+        this.ActiveCreatureStatblock = ko.computed(function () {
+            return _this.ActiveCreature()
+                ? _this.ActiveCreature().StatBlock
+                : StatBlock.Empty();
         });
     }
     return Encounter;
@@ -101,15 +135,36 @@ var Creature = (function () {
         var _this = this;
         this.setAlias = function (name) {
             var others = _this.Encounter.creatures().filter(function (c) { return c !== _this && c.Name === name; });
+            if (others.length === 0) {
+                return ko.observable(name);
+            }
             if (others.length === 1) {
                 others[0].Alias(name + " 1");
             }
             return ko.observable(name + " " + (others.length + 1));
         };
+        this.calculateModifiers = function () {
+            var modifiers = StatBlock.Empty().Attributes;
+            for (var attribute in _this.StatBlock.Attributes) {
+                modifiers[attribute] = _this.Encounter.Rules.CalculateModifier(_this.StatBlock.Attributes[attribute]);
+            }
+            return modifiers;
+        };
+        this.calculateSaves = function () {
+            var saves = _this.AbilityModifiers;
+            for (var _i = 0, _a = _this.StatBlock.Saves; _i < _a.length; _i++) {
+                var save = _a[_i];
+                saves[save.Name] = save.Value;
+            }
+            return saves;
+        };
         this.CommitHP = function () {
             _this.CurrentHP(_this.CurrentHP() - _this.HPChange());
             _this.HPChange(null);
             _this.FocusHP(false);
+        };
+        this.CommitName = function () {
+            _this.EditName(false);
         };
         this.GetHPColor = function () {
             var green = Math.floor((_this.CurrentHP() / _this.MaxHP) * 220);
@@ -129,31 +184,49 @@ var Creature = (function () {
             mods.push(_this.Encounter.Rules.CalculateModifier(abilityScore));
             return _this.Encounter.Rules.Check(mods);
         };
+        this.StatBlock = StatBlock.Empty();
+        jQuery.extend(this.StatBlock, creatureJson);
         this.Encounter = encounter;
-        this.Name = creatureJson.Name;
+        this.Name = this.StatBlock.Name;
         this.Alias = this.setAlias(this.Name);
-        this.MaxHP = creatureJson.HP.Value;
-        this.CurrentHP = ko.observable(creatureJson.HP.Value);
+        this.MaxHP = this.StatBlock.HP.Value;
+        this.CurrentHP = ko.observable(this.StatBlock.HP.Value);
         this.HPChange = ko.observable(null);
-        this.InitiativeModifier = this.Encounter.Rules.CalculateModifier(creatureJson.Attributes.Dex);
+        this.AbilityModifiers = this.calculateModifiers();
+        this.AC = this.StatBlock.AC.Value;
+        this.Saves = this.calculateSaves();
+        this.InitiativeModifier = this.Encounter.Rules.CalculateModifier(this.StatBlock.Attributes.Dex);
         this.Initiative = ko.observable(0);
-        this.StatBlock = creatureJson;
         this.FocusHP = ko.observable(false);
+        this.EditName = ko.observable(false);
     }
     return Creature;
 })();
 var ViewModel = (function () {
     function ViewModel() {
-        var self = this;
+        var _this = this;
         this.encounter = ko.observable(new Encounter());
         this.creatures = ko.observableArray();
+        this.LibraryFilter = ko.observable('');
+        this.FilteredCreatures = ko.computed(function () {
+            var filter = _this.LibraryFilter();
+            if (filter.length == 0) {
+                return _this.creatures();
+            }
+            return _this.creatures().filter(function (v) {
+                return v.Name.toLocaleLowerCase().indexOf(filter.toLocaleLowerCase()) > -1;
+            });
+        });
     }
     return ViewModel;
 })();
 function RegisterKeybindings(viewModel) {
     Mousetrap.bind('j', viewModel.encounter().SelectNextCombatant);
     Mousetrap.bind('k', viewModel.encounter().SelectPreviousCombatant);
+    Mousetrap.bind('n', viewModel.encounter().NextTurn);
     Mousetrap.bind('t', viewModel.encounter().FocusSelectedCreatureHP);
+    Mousetrap.bind('del', viewModel.encounter().RemoveSelectedCreature);
+    Mousetrap.bind('f2', viewModel.encounter().EditSelectedCreatureName);
     Mousetrap.bind('alt+r', viewModel.encounter().RollInitiative);
     Mousetrap.bind('alt+j', viewModel.encounter().MoveSelectedCreatureDown);
     Mousetrap.bind('alt+k', viewModel.encounter().MoveSelectedCreatureUp);
@@ -164,8 +237,6 @@ $(function () {
     ko.applyBindings(viewModel);
     $.getJSON('creatures.json', function (json) {
         viewModel.creatures(json);
-        viewModel.creatures().forEach(viewModel.encounter().AddCreature);
-        viewModel.creatures().forEach(viewModel.encounter().AddCreature);
         viewModel.creatures().forEach(viewModel.encounter().AddCreature);
     });
 });
--- tracker.ts
+++ tracker.ts
@@ -1,5 +1,6 @@
 /// <reference path="typings/jquery/jquery.d.ts" />
 /// <reference path="typings/knockout/knockout.d.ts" />
+/// <reference path="typings/knockout/bindinghandlers.d.ts" />
 /// <reference path="typings/mousetrap/mousetrap.d.ts" />
 
 interface IHaveValue{
@@ -15,14 +16,32 @@ interface IHaveAttributes{
   Wis: number;
 }
 
+interface IHaveAModifier{
+  Name: string;
+  Value: number;
+}
+
 interface IHaveTrackerStats{
   Name: string;
   HP: IHaveValue;
+  AC: IHaveValue;
   Attributes: IHaveAttributes;
+  Saves: IHaveAModifier[];
 }
 
 class StatBlock {
-  static Empty = { Name: 'None', HP: { Value: 0 }, Attributes: { Str: 0, Dex: 0, Con: 0, Cha: 0, Int: 0, Wis: 0} }
+  static Empty = () => ({ 
+    Name: '', Type: '', 
+    HP: { Value: 1, Notes: ''},  AC: { Value: 10, Notes: ''},
+    Speed: [],
+    Attributes: { Str: 10, Dex: 10, Con: 10, Cha: 10, Int: 10, Wis: 10 },
+    DamageVulnerabilities: [], DamageResistances: [], DamageImmunities: [], ConditionImmunities: [],
+    Saves: [], Skills: [], Senses: [], Languages: [],
+    Challenge: 0,
+    Traits: [],
+    Actions: [],
+    LegendaryActions: []
+  })
 }
 
 interface Rules{
@@ -45,21 +64,30 @@ class DefaultRules implements Rules {
 
 class Encounter {
   constructor(rules?: Rules){
+    this.Rules = rules || new DefaultRules();
     this.creatures = ko.observableArray<Creature>();
     this.SelectedCreature = ko.observable<Creature>();
-    this.Rules = rules || new DefaultRules();
     this.SelectedCreatureStatblock = ko.computed(() => 
     {
       return this.SelectedCreature() 
-                 ? this.SelectedCreature().StatBlock 
-                 : StatBlock.Empty;
-    })
+                 ? this.SelectedCreature().StatBlock
+                 : StatBlock.Empty();
+    });
+    this.ActiveCreature = ko.observable<Creature>();
+    this.ActiveCreatureStatblock = ko.computed(() => 
+    {
+      return this.ActiveCreature() 
+                 ? this.ActiveCreature().StatBlock
+                 : StatBlock.Empty();
+    });
   }
   
+  Rules: Rules;
   creatures: KnockoutObservableArray<Creature>;
   SelectedCreature: KnockoutObservable<Creature>;
   SelectedCreatureStatblock: KnockoutComputed<IHaveTrackerStats>;
-  Rules: Rules;
+  ActiveCreature: KnockoutObservable<Creature>;
+  ActiveCreatureStatblock: KnockoutComputed<IHaveTrackerStats>;
   
   private sortByInitiative = () => {
     this.creatures.sort((l,r) => (r.Initiative() - l.Initiative()) || 
@@ -87,6 +115,10 @@ class Encounter {
   {
     console.log("adding %O", creatureJson);
     this.creatures.push(new Creature(creatureJson, this));
+  }
+  
+  RemoveSelectedCreature = () => {
+    this.creatures.remove(this.SelectedCreature());
   }
   
   SelectPreviousCombatant = () =>
@@ -125,6 +157,13 @@ class Encounter {
     }
   }
   
+  EditSelectedCreatureName = () => 
+  {
+    if(this.SelectedCreature()){
+      this.SelectedCreature().EditName(true);
+    }
+  }
+  
   RollInitiative = () =>
   {
     if(this.Rules.GroupSimilarCreatures)
@@ -143,6 +182,15 @@ class Encounter {
     }
     
     this.sortByInitiative();
+    this.ActiveCreature(this.creatures()[0]);
+  }
+  
+  NextTurn = () => {
+    var nextIndex = this.creatures().indexOf(this.ActiveCreature()) + 1;
+    if(nextIndex >= this.creatures().length){
+      nextIndex = 0;
+    }
+    this.ActiveCreature(this.creatures()[nextIndex]);
   }
 }
 
@@ -152,48 +200,82 @@ class Creature{
   MaxHP: number;
   CurrentHP: KnockoutObservable<number>;
   HPChange: KnockoutObservable<number>;
+  AC: number;
+  AbilityModifiers: IHaveAttributes;
+  Saves: IHaveAttributes;
   InitiativeModifier: number;
   Initiative: KnockoutObservable<number>;
   StatBlock: IHaveTrackerStats;
   Encounter: Encounter;
   FocusHP: KnockoutObservable<boolean>;
+  EditName: KnockoutObservable<boolean>;
   constructor(creatureJson: IHaveTrackerStats, encounter: Encounter, rules?: Rules){
+    this.StatBlock = StatBlock.Empty();
+    jQuery.extend(this.StatBlock, creatureJson);
     this.Encounter = encounter;
-    this.Name = creatureJson.Name;
+    this.Name = this.StatBlock.Name;
     this.Alias = this.setAlias(this.Name);
-    this.MaxHP = creatureJson.HP.Value;
-    this.CurrentHP = ko.observable(creatureJson.HP.Value);
+    this.MaxHP = this.StatBlock.HP.Value;
+    this.CurrentHP = ko.observable(this.StatBlock.HP.Value);
     this.HPChange = ko.observable(null);
-    this.InitiativeModifier = this.Encounter.Rules.CalculateModifier(creatureJson.Attributes.Dex);
+    this.AbilityModifiers = this.calculateModifiers();
+    this.AC = this.StatBlock.AC.Value;
+    this.Saves = this.calculateSaves();
+    this.InitiativeModifier = this.Encounter.Rules.CalculateModifier(this.StatBlock.Attributes.Dex);
     this.Initiative = ko.observable(0);
-    this.StatBlock = creatureJson;
     this.FocusHP = ko.observable(false);
+    this.EditName = ko.observable(false);
   }
   
   private setAlias = (name: string) => {
     var others = this.Encounter.creatures().filter(c => c !== this && c.Name === name);
+    if(others.length === 0){
+      return ko.observable(name);
+    }
     if(others.length === 1){
       others[0].Alias(name + " 1")
     }
-    
     return ko.observable(name + " " + (others.length + 1));
   }
   
+  private calculateModifiers = () => {
+    var modifiers = StatBlock.Empty().Attributes;
+    for(var attribute in this.StatBlock.Attributes){
+      modifiers[attribute] = this.Encounter.Rules.CalculateModifier(this.StatBlock.Attributes[attribute]);
+    }
+    return modifiers;
+  }
+  
+  private calculateSaves = () => {
+    var saves = this.AbilityModifiers;
+    for(var save of this.StatBlock.Saves){
+      saves[save.Name] = save.Value;
+    }
+    return saves;
+  }
+
   CommitHP = () => {
     this.CurrentHP(this.CurrentHP() - this.HPChange());
     this.HPChange(null);
     this.FocusHP(false);
   }
+  
+  CommitName = () => {
+    this.EditName(false);
+  }
+  
   GetHPColor = () => {
     var green = Math.floor((this.CurrentHP() / this.MaxHP) * 220);
     var red = Math.floor((this.MaxHP - this.CurrentHP()) / this.MaxHP * 255);
     return "rgb(" + red + "," + green + ",0)";
   }
+  
   RollInitiative = () => {
     var roll = this.Encounter.Rules.Check([this.InitiativeModifier]);
     this.Initiative(roll);
     return roll;
   }
+  
   AbilityCheck = (attribute: string, mods: number[]) => {
     var abilityScore = this.StatBlock.Attributes[attribute];
     if(abilityScore === undefined){
@@ -206,18 +288,32 @@ class Creature{
 
 class ViewModel{
   constructor(){
-    var self = this;
     this.encounter = ko.observable<Encounter>(new Encounter());
     this.creatures = ko.observableArray<IHaveTrackerStats>();
+    this.LibraryFilter = ko.observable('');
+    this.FilteredCreatures = ko.computed(() => {
+      var filter = this.LibraryFilter();
+      if(filter.length == 0){
+        return this.creatures();
+      }
+      return this.creatures().filter(v => {
+        return v.Name.toLocaleLowerCase().indexOf(filter.toLocaleLowerCase()) > -1;
+      });
+    })
   }
   encounter: KnockoutObservable<Encounter>;
   creatures: KnockoutObservableArray<IHaveTrackerStats>;
+  FilteredCreatures: KnockoutComputed<IHaveTrackerStats []>
+  LibraryFilter: KnockoutObservable<string>;
 }
 
 function RegisterKeybindings(viewModel: ViewModel){
   Mousetrap.bind('j',viewModel.encounter().SelectNextCombatant);
   Mousetrap.bind('k',viewModel.encounter().SelectPreviousCombatant);
+  Mousetrap.bind('n',viewModel.encounter().NextTurn);
   Mousetrap.bind('t',viewModel.encounter().FocusSelectedCreatureHP);
+  Mousetrap.bind('del',viewModel.encounter().RemoveSelectedCreature);
+  Mousetrap.bind('f2', viewModel.encounter().EditSelectedCreatureName);
   Mousetrap.bind('alt+r',viewModel.encounter().RollInitiative);
   Mousetrap.bind('alt+j',viewModel.encounter().MoveSelectedCreatureDown);
   Mousetrap.bind('alt+k',viewModel.encounter().MoveSelectedCreatureUp);
@@ -229,8 +325,6 @@ $(() => {
     ko.applyBindings(viewModel);
     $.getJSON('creatures.json', function(json){
     	viewModel.creatures(json);
-      viewModel.creatures().forEach(viewModel.encounter().AddCreature);
-      viewModel.creatures().forEach(viewModel.encounter().AddCreature);
       viewModel.creatures().forEach(viewModel.encounter().AddCreature);
     });
 });
