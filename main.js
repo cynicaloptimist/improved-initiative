9be80f95e2b7831f498bb358bc33b95fe18ca72b
Evan Bailey <baileyevang@gmail.com>
2015-07-06 20:20:28 -0700

    Cleanup unused files

--- main.js
+++
@@ -1,6 +0,0 @@
-  <script type="text/javascript" src="node_modules/jquery/dist/jquery.js"></script>
-  <script type="text/javascript" src="node_modules/jquery-ui/jquery-ui.js"></script>
-  <script type="text/javascript" src="node_modules/knockout/build/output/knockout-latest.debug.js"></script>
-  <script type="text/javascript" src="node_modules/mousetrap/mousetrap.js"></script>
-      
-  <script type="text/javascript" src="js/ImprovedInitiative.js"></script>
