65497818051b656c02682058d24443773d0c0650
Evan Bailey <baileyevang@gmail.com>
2015-10-13 01:45:33 -0700

    Add server.ts

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
+/// <reference path="../typings/node/node.d.ts" />
 var express = require('express');
 var port = process.env.PORT || 80;
 var app = express();
-
 app.use(express.static('./'));
-
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     res.render('index.html');
 });
-
-var server = app.listen(port, function() {
-	var host = server.address().address;
-  	var port = server.address().port;
-
-	console.log('Improved Initiative listening at http://%s:%s', host, port);
-})
+var server = app.listen(port, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('Improved Initiative listening at http://%s:%s', host, port);
+});
+//# sourceMappingURL=server.js.map
