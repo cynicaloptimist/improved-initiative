ccc68bba0a5b7af9c0c10b2b13f3bcd317e67c4e
Evan Bailey <baileyevang@gmail.com>
2016-03-01 02:14:13 -0800

    paginated tips!

--- public/js/ImprovedInitiative.js
+++ public/js/ImprovedInitiative.js
@@ -505,6 +505,29 @@ var ImprovedInitiative;
     ko.components.loaders.unshift(templateLoader);
     ko.components.register('settings', {
         viewModel: function (params) {
+            var tips = [
+                "You can use the player view URL to track your combat on any device.",
+                "Editing a creature after it has been added to combat will only change that individual creature.",
+                "You can restore a creature's hit points by applying negative damage to it.",
+                "Temporary hit points obey the 5th edition rules- applying temporary hitpoints will ignore temporary hit points a creature already has.",
+                "The 'alt' key can be used for secrecy. Adding a creature or a tag while holding 'alt' will hide it from the player view.",
+                "Hold the control key while clicking to select multiple combatants. You can apply damage to multiple creatures at the same time this way.",
+                "Moving a creature in the initiative order will automatically adjust their initiative count.",
+                "The active creature will have its traits and actions displayed first for ease of reference.",
+                "The player view will only display a colored, qualitative indicator for HP. You can use temporary HP to obfuscate this.",
+                "Want to contribute? The source code for Improved Initiative is available on <a href='http://github.com/cynicaloptimist/improved-initiative' target='_blank'>Github.</a>"
+            ];
+            var currentTipIndex = ko.observable(Math.floor(Math.random() * tips.length));
+            var cycleTipIndex = function (amount) {
+                var newIndex = currentTipIndex() + amount;
+                if (newIndex < 0) {
+                    newIndex = tips.length - 1;
+                }
+                else if (newIndex > tips.length - 1) {
+                    newIndex = 0;
+                }
+                currentTipIndex(newIndex);
+            };
             return {
                 Commander: params.commander,
                 ShowTab: function (tabSelector) {
@@ -520,7 +543,10 @@ var ImprovedInitiative;
                     if (file) {
                         ImprovedInitiative.Store.ImportAll(file);
                     }
-                }
+                },
+                Tip: ko.computed(function () { return tips[currentTipIndex() % tips.length]; }),
+                NextTip: cycleTipIndex.with(1),
+                PreviousTip: cycleTipIndex.with(-1)
             };
         },
         template: { name: 'settings' }
