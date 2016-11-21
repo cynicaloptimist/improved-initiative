3add36a756c0461370a7043c755e8d149cc347b0
Evan Bailey <baileyevang@gmail.com>
2016-11-20 20:03:37 -0800

    Toggleable full event log

--- client/EventLog.ts
+++ client/EventLog.ts
@@ -1,10 +1,31 @@
 module ImprovedInitiative {
     export class EventLog {
         Events = ko.observableArray<string>();
-        LatestEvent = ko.computed(() => {
-            var events = this.Events();
-            return events.length > 0 ? events[events.length - 1] : "Welcome to Improved Initiative!";
-        });
-        AddEvent = (event: string) => this.Events.push(event);
+        
+        LatestEvent = ko.computed(() => this.Events()[this.Events().length - 1] || "Welcome to Improved Initiative!");
+        EventsTail = ko.computed(() => this.Events().slice(0, this.Events().length - 1));
+        
+        AddEvent = (event: string) => {
+            this.Events.push(event);
+            this.scrollToBottomOfLog();
+        }
+
+        ToggleFullLog = () => {
+            if(this.ShowFullLog()) {
+                this.ShowFullLog(false);
+                $('.combatants').css('flex-shrink', 1);
+            } else {
+                this.ShowFullLog(true);
+                $('.combatants').css('flex-shrink', 0);
+                this.scrollToBottomOfLog();
+            }
+        }
+
+        ShowFullLog = ko.observable<boolean>(false);
+
+        private scrollToBottomOfLog = () => {
+            let scrollHeight = $('.event-log')[0].scrollHeight;
+            $('.event-log').scrollTop(scrollHeight);
+        }
     }
 }
