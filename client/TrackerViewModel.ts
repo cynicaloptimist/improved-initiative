6777cada635cf941ddda8b7a371ab1390b2c06cb
Evan Bailey <baileyevang@gmail.com>
2016-12-13 22:25:18 -0800

    Move keybinding handling to Settings

--- client/TrackerViewModel.ts
+++ client/TrackerViewModel.ts
@@ -1,8 +1,5 @@
 module ImprovedInitiative {
     export class TrackerViewModel {
-        constructor() {
-            this.EncounterCommander.RegisterKeyBindings();
-        }
         UserPollQueue = new UserPollQueue();
         EventLog = new EventLog();
         StatBlockEditor = new StatBlockEditor();
