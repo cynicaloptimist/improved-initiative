08cddd88f79ad1340f553455b0735da3b1883b7f
Evan Bailey <baileyevang@gmail.com>
2016-12-20 23:55:02 -0700

    deep extend posted statblocks

--- client/TrackerViewModel.ts
+++ client/TrackerViewModel.ts
@@ -9,22 +9,22 @@ module ImprovedInitiative {
         CombatantCommander = new CombatantCommander(this.Encounter, this.UserPollQueue, this.StatBlockEditor, this.EventLog);
 
         LaunchPostedEncounterIfAvailable = () => {
+            const deepExtend = (a,b) => $.extend(true, {}, a, b);
             const postedEncounterJSON = $('html')[0].getAttribute('postedEncounter');
             const postedEncounter: { Combatants: any [] } = JSON.parse(postedEncounterJSON);
             if(postedEncounter.Combatants) {
                 postedEncounter.Combatants.forEach(c => {
-                    const fullStatBlock = { ...StatBlock.Empty(), ...c }
                     if(c.Id){
                         $.ajax(`/creatures/${c.Id}`)
-                         .done(statBlock => {
-                             const statBlockFromLibrary = { ...c, ...statBlock };
-                             this.Encounter.AddCreature(statBlockFromLibrary);
+                         .done(statBlockFromLibrary => {
+                             const modifiedStatBlockFromLibrary = deepExtend(statBlockFromLibrary, c);
+                             this.Encounter.AddCreature(modifiedStatBlockFromLibrary);
                          })
                          .fail(_ => {
-                            this.Encounter.AddCreature(fullStatBlock)
+                            this.Encounter.AddCreature(deepExtend(StatBlock.Empty(), c))
                          })
                     } else {
-                        this.Encounter.AddCreature(fullStatBlock)
+                        this.Encounter.AddCreature(deepExtend(StatBlock.Empty(), c))
                     }
                 })
             }
