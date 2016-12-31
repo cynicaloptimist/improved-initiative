7b76e2fa139ef48c322aa066f135b75e9336d799
Evan Bailey <baileyevang@gmail.com>
2016-12-30 23:37:57 -0800

    Clean up files and stray comma

--- client/Tracker.tsx
+++
@@ -1,8 +0,0 @@
-/// <reference path="../typings/index.d.ts" />
-
-module ImprovedInitiative {
-    ReactDOM.render(
-    <h1>Hello, world!</h1>,
-    document.getElementById('root')
-    );
-}
--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-var path = require('path');
-
-module.exports = {
-    entry: './client/Tracker.tsx',
-    output: {
-        path: path.resolve(__dirname, 'public/js'),
-        filename: 'improved-initiative.bundle.js'
-    }
-};
