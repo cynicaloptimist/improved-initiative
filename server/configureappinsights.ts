9e82a2195e85ad203feb22b189ccc10b7efd3b1f
Evan Bailey <baileyevang@gmail.com>
2017-04-11 23:46:12 -0700

    Filter socket.io requests out of app insights

--- server/configureappinsights.ts
+++ server/configureappinsights.ts
@@ -1,7 +1,18 @@
 import appInsights = require("applicationinsights");
 
+const filterSocketReporting = (envelope: ContractsModule.Envelope) => {
+    if (envelope.data.baseType !== "Microsoft.ApplicationInsights.RequestData") {
+        return true;
+    }
+    const data = <ContractsModule.RequestData>envelope.data.baseData;
+    return data.name.indexOf("socket.io") === -1;
+}
+
 export default function () {
     if (process.env.APPINSIGHTS_INSTRUMENTATIONKEY) {
-        appInsights.setup(process.env.APPINSIGHTS_INSTRUMENTATIONKEY).start();
+        appInsights.setup(process.env.APPINSIGHTS_INSTRUMENTATIONKEY)
+            .start();
+        const client: any = appInsights.client;
+        client.addTelemetryProcessor(filterSocketReporting);
     }
 }
