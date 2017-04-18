ab261d6f3dcfc787adbf4d4e208c42356feda969
Evan Bailey <baileyevang@gmail.com>
2017-04-17 23:36:51 -0700

    Don't need name in keywords

--- server/statblock.ts
+++ server/statblock.ts
@@ -1,5 +1,3 @@
-import * as _ from "lodash";
-
 export interface StatBlock {
     Name: string;
     Id: string;
@@ -7,8 +5,4 @@ export interface StatBlock {
     Source: string;
 }
 
-export const GetStatBlockKeywords = (statBlock: StatBlock) => {
-    const nameWords = statBlock.Name.replace(/[^\w\s]/g, "").split(" ");
-    const typeWords = statBlock.Type.replace(/[^\w\s]/g, "").split(" ");
-    return _.union(nameWords, typeWords);
-};
+export const GetStatBlockKeywords = (statBlock: StatBlock) => statBlock.Type.replace(/[^\w\s]/g, "").split(" ");
