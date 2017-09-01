a9b1e0720f15938dd0a9df4ffec372b8aec40673
Evan Bailey <baileyevang@gmail.com>
2017-08-31 22:53:45 -0700

    Add DB initialization

--- server/dbconnection.ts
+++ server/dbconnection.ts
@@ -1,8 +1,26 @@
 import mongo = require("mongodb");
+const client = mongo.MongoClient;
 
 interface Db extends mongo.Db {
     users: mongo.Collection;
 }
+    
+export const initializeDb = () => {
+    if (!process.env.DB_CONNECTION_STRING) {
+        console.error("No connection string found.");
+        return;
+    }
+
+    client.connect(process.env.DB_CONNECTION_STRING, function (err, db: Db) {
+        if (err) {
+            return;
+        }
+
+        if (!db.users) {
+            db.createCollection("users");
+        }
+    });
+};
 
 export const upsertUser = (patreonId: string, accessKey: string, refreshKey: string, accountStatus: string, res) => {
     if (!process.env.DB_CONNECTION_STRING) {
@@ -10,10 +28,10 @@ export const upsertUser = (patreonId: string, accessKey: string, refreshKey: str
         return;
     }
 
-    const client = mongo.MongoClient;
     client.connect(process.env.DB_CONNECTION_STRING, function (err, db: Db) {
         if (err) {
             res.json(err);
+            return;
         }
         db.users.updateOne(
             {
--- server/server.ts
+++ server/server.ts
@@ -8,11 +8,13 @@ import LaunchServer from "./launchserver";
 import ConfigureRoutes from "./routes";
 import ConfigureSockets from "./sockets";
 import * as L from "./library";
+import * as DB from "./dbconnection";
 
 ConfigureAppInsights();
 
 const app = express();
 const http = require("http").Server(app);
+DB.initialize();
 
 const statBlockLibrary = L.Library.FromFile<L.StatBlock>("ogl_creatures.json", "/statblocks/", L.GetStatBlockKeywords);
 const spellLibrary = L.Library.FromFile<L.Spell>("ogl_spells.json", "/spells/", L.GetSpellKeywords);
