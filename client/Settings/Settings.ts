cb59a34e3d40eb570e07ad702459a80c439bad4e
Evan Bailey <baileyevang@gmail.com>
2017-09-21 23:17:37 -0700

    Add command settings to settings pojo

--- client/Settings/Settings.ts
+++ client/Settings/Settings.ts
@@ -1,15 +1,17 @@
 module ImprovedInitiative {
     export interface Settings {
+        EncounterCommands: CommandSetting[];
+        CombatantCommands: CommandSetting[];
         Rules: {
             RollMonsterHp: boolean;
             AllowNegativeHP: boolean;
             AutoCheckConcentration: boolean;
-        };
+        }
         TrackerView: {
             DisplayRoundCounter: boolean;
             DisplayTurnTimer: boolean;
             DisplayDifficulty: boolean;
-        };
+        }
         PlayerView: {
             AllowPlayerSuggestions: boolean;
             MonsterHPVerbosity: string;
