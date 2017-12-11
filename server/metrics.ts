26f30b4bf4d19d02186dcf3cfe8709b7a2bdf601
Evan Bailey <baileyevang@gmail.com>
2017-12-10 22:03:54 -0800

    Fail recordEvent when there's no metrics vars

--- server/metrics.ts
+++ server/metrics.ts
@@ -14,12 +14,16 @@ export default function (app: express.Application) {
     });
 
     app.post("/recordEvent/:eventName", (req: Req, res: Res) => {
+        if (!keenProjectId || !keenWriteKey) {
+            return res.status(501).send("No metrics pipeline configured.");
+        }
+
         const eventName = req.params.eventName;
         const eventData = req.body || {};
         eventData.sessionId = req.session.id;
         eventData.userId = req.session.userId || null;
         keenClient.recordEvent(eventName, eventData);
 
-        res.sendStatus(201);
+        return res.sendStatus(200);
     });
 }
