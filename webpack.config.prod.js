1fa29d7a7dcd82a06da3c6fb655bf134b956c6a5
Evan Bailey <baileyevang@gmail.com>
2017-12-10 23:13:14 -0800

    Add webpack to grunt build

---
+++ webpack.config.prod.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+  entry: './client/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ]
+  },
+  output: {
+    filename: 'ImprovedInitiative.js',
+    path: path.resolve(__dirname, 'build')
+  }
+};
