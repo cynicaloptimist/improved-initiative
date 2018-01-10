1da524e47419a5340f933eabe68d2e8581c0a003
Evan Bailey <baileyevang@gmail.com>
2018-01-09 23:18:59 -0800

    Use Listing.GetAsync

--- client/Library/Components/Listing.tsx
+++ client/Library/Components/Listing.tsx
@@ -1,16 +1,20 @@
 import * as React from "react";
 import { MouseEvent } from "react";
-import { Listable } from "../Listing";
+import { Listing, Listable } from "../Listing";
 import { Button } from "./Button";
 
 export interface ListingProps<T extends Listable>  {
     name: string;
-    listing: T;
-    onAdd: (listing: T) => void;
+    listing: Listing<T>;
+    onAdd: (item: T) => void;
 }
 
 export class ListingViewModel<T extends Listable> extends React.Component<ListingProps<T>> {
+    private getListingAndAdd = () => {
+        this.props.listing.GetAsync(this.props.onAdd);
+    }
+
     public render() {
-        return <li key={this.props.listing.Id}><Button name={this.props.name} onClick={() => this.props.onAdd(this.props.listing)}/></li>;
+        return <li key={this.props.listing.Id}><Button name={this.props.name} onClick={this.getListingAndAdd}/></li>;
     }
 }
