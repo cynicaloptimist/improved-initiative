e20b1dd1f02173ef3c6c95032c371cc8d37e72ec
Evan Bailey <baileyevang@gmail.com>
2018-01-20 22:55:32 -0800

    Add UpdateCSS to PlayerViewClient

--- client/Player/PlayerViewClient.ts
+++ client/Player/PlayerViewClient.ts
@@ -4,12 +4,16 @@ import { StaticCombatantViewModel } from "../Combatant/StaticCombatantViewModel"
 
 export class PlayerViewClient {
     constructor(private socket: SocketIOClient.Socket) { }
-    
+
     public JoinEncounter(encounterId: string): any {
         this.socket.emit("join encounter", encounterId);
     }
 
     public UpdateEncounter(encounterId: string, updatedEncounter: SavedEncounter<StaticCombatantViewModel>) {
         this.socket.emit("update encounter", encounterId, updatedEncounter);
+    }
+
+    public UpdateCSS(encounterId: string, updatedCSS: string) {
+        this.socket.emit("update custom CSS", encounterId, updatedCSS);
     }
 }
