cd5c8c346a2bef12727aabffb5fff9cc17986765
Evan Bailey <baileyevang@gmail.com>
2018-01-20 18:21:21 -0800

    Load hasEpicInitiative into Environment

--- client/Environment.ts
+++ client/Environment.ts
@@ -3,6 +3,7 @@ interface Environment {
     PostedEncounter: { Combatants: any[] };
     IsLoggedIn: boolean;
     HasStorage: boolean;
+    HasEpicInitiative: boolean;
     PatreonLoginUrl: string;
 }
 
@@ -10,18 +11,22 @@ export const env: Environment = {
     EncounterId: null,
     PostedEncounter: null,
     HasStorage: false,
+    HasEpicInitiative: false,
     IsLoggedIn: false,
     PatreonLoginUrl: "http://www.patreon.com/"
 };
 
 export function LoadEnvironment() {
-    env.EncounterId = $("html")[0].getAttribute("encounterId");
-    const encounterJSON = $("html")[0].getAttribute("postedEncounter");
+    const html = document.getElementsByTagName("html")[0];
+
+    env.EncounterId = html.getAttribute("encounterId");
+    const encounterJSON = html.getAttribute("postedEncounter");
     if (encounterJSON) {
         env.PostedEncounter = JSON.parse(encounterJSON);
     }
-    env.HasStorage = $("html")[0].getAttribute("hasStorage") == "true";
-    env.IsLoggedIn = $("html")[0].getAttribute("isLoggedIn") == "true";
+    env.HasStorage = html.getAttribute("hasStorage") == "true";
+    env.HasEpicInitiative = html.getAttribute("hasEpicInitiative") == "true";
+    env.IsLoggedIn = html.getAttribute("isLoggedIn") == "true";
     if (window["patreonUrl"]) {
         env.PatreonLoginUrl = window["patreonUrl"];
     }
