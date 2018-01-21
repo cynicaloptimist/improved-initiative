d85003994a05f01e1ddd244e8dfb49458c8338cd
Evan Bailey <baileyevang@gmail.com>
2018-01-21 11:36:10 -0800

    Log to console on CSS update, add some types

--- server/sockets.ts
+++ server/sockets.ts
@@ -1,21 +1,28 @@
+import { SavedEncounter } from "../client/Encounter/SavedEncounter";
+import { StaticCombatantViewModel } from "../client/Combatant/StaticCombatantViewModel";
+
 export default function(io: SocketIO.Server, playerViews) {
     io.on("connection", function (socket: SocketIO.Socket) {
 
         let encounterId = null;
 
-        socket.on("update encounter", function (id, updatedEncounter) {
+        socket.on("update encounter", function (id: string, updatedEncounter: SavedEncounter<StaticCombatantViewModel>) {
             encounterId = id;
             socket.join(encounterId);
             playerViews[encounterId] = updatedEncounter;
             socket.broadcast.to(encounterId).emit("update encounter", updatedEncounter);
         });
 
-        socket.on("join encounter", function (id) {
+        socket.on("update custom CSS", (id: string, updatedCSS: string) => {
+            console.log(`${id} - custom css: ${updatedCSS}`);
+        });
+
+        socket.on("join encounter", function (id: string) {
             encounterId = id;
             socket.join(id);
         });
 
-        socket.on("suggest damage", function (id, suggestedCombatantIds: string[], suggestedDamage: number, suggester: string) {
+        socket.on("suggest damage", function (id: string, suggestedCombatantIds: string[], suggestedDamage: number, suggester: string) {
             if (id !== encounterId) {
                 return;
             }
