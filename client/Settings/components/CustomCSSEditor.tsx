7f574c1c8c48aa0842622b8fc42215a2318981df
Evan Bailey <baileyevang@gmail.com>
2018-01-28 18:42:24 -0800

    Extract ColorChooser

--- client/Settings/components/CustomCSSEditor.tsx
+++ client/Settings/components/CustomCSSEditor.tsx
@@ -12,23 +12,47 @@ export interface CustomCSSEditorProps {
 
 interface State {
     manualCSS: string;
-    styles: PlayerViewCustomStyles;
-    selectedStyle: keyof PlayerViewCustomStyles | null;
 }
 
 export class CustomCSSEditor extends React.Component<CustomCSSEditorProps, State> {
     constructor(props: CustomCSSEditorProps) {
         super(props);
         this.state = {
-            manualCSS: this.props.currentCSS,
-            styles: this.props.currentStyles,
-            selectedStyle: null
+            manualCSS: this.props.currentCSS
         };
     }
 
     private updateCSS = (event: ChangeEvent<HTMLTextAreaElement>) => {
         this.setState({ manualCSS: event.target.value });
         this.props.updateCSS(event.target.value);
+    }
+
+    public render() {
+        return <div className="custom-css-editor">
+            <p>Epic Initiative is enabled.</p>
+            <ColorChooser currentStyles={this.props.currentStyles} updateStyle={this.props.updateStyle} />
+            <h4>Additional CSS</h4>
+            <textarea rows={10} onChange={this.updateCSS} value={this.props.currentCSS} />
+        </div>;
+    }
+}
+
+interface ColorChooserProps {
+    currentStyles: PlayerViewCustomStyles;
+    updateStyle: (name: keyof PlayerViewCustomStyles, value: string) => void;
+}
+interface ColorChooserState {
+    styles: PlayerViewCustomStyles;
+    selectedStyle: keyof PlayerViewCustomStyles | null;
+}
+
+class ColorChooser extends React.Component<ColorChooserProps, ColorChooserState> {
+    constructor(props) {
+        super(props);
+        this.state = {
+            styles: this.props.currentStyles,
+            selectedStyle: null
+        };
     }
 
     private handleChangeComplete = (color: ColorResult) => {
@@ -45,21 +69,17 @@ export class CustomCSSEditor extends React.Component<CustomCSSEditorProps, State
     }
 
     public render() {
-        return <div className="custom-css-editor">
-            <p>Epic Initiative is enabled.</p>
-            <h4>Colors</h4>
+        return <div><h4>Colors</h4>
             <p>Combatant:
-                Text <ColorBlock color={this.state.styles.combatantText} click={this.bindClickToSelectStyle("combatantText")} />
+            Text <ColorBlock color={this.state.styles.combatantText} click={this.bindClickToSelectStyle("combatantText")} />
                 Background: <ColorBlock color={this.state.styles.combatantBackground} click={this.bindClickToSelectStyle("combatantBackground")} />
             </p>
             <p>Header:
-                Text <ColorBlock color={this.state.styles.headerText} click={this.bindClickToSelectStyle("headerText")} />
+            Text <ColorBlock color={this.state.styles.headerText} click={this.bindClickToSelectStyle("headerText")} />
                 Background: <ColorBlock color={this.state.styles.headerBackground} click={this.bindClickToSelectStyle("headerBackground")} />
             </p>
             <p>Main Background: <ColorBlock color={this.state.styles.mainBackground} click={this.bindClickToSelectStyle("mainBackground")} /></p>
             {this.state.selectedStyle !== null && <SketchPicker width="210px" color={this.state.styles[this.state.selectedStyle]} onChangeComplete={this.handleChangeComplete} />}
-            <h4>Additional CSS</h4>
-            <textarea rows={10} onChange={this.updateCSS} value={this.props.currentCSS} />
         </div>;
     }
 }
