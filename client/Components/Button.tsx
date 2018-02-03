a231dfe4972a5111e65202bd29e16435614d6520
Evan Bailey <baileyevang@gmail.com>
2018-02-03 11:18:41 -0800

    Add additionalClassNames to button

--- client/Components/Button.tsx
+++ client/Components/Button.tsx
@@ -3,6 +3,7 @@ import * as React from "react";
 export interface ButtonProps {
     text?: string;
     faClass?: string;
+    additionalClassNames?: string;
     onClick: React.MouseEventHandler<HTMLSpanElement>;
     onMouseOver?: React.MouseEventHandler<HTMLSpanElement>;
 }
@@ -11,9 +12,12 @@ export class Button extends React.Component<ButtonProps> {
     public render() {
         const text = this.props.text || "";
         const classNames = ["c-button", "fa"];
-        if(this.props.faClass){
+        if (this.props.faClass) {
             classNames.push(`fa-${this.props.faClass}`);
         }
+        if (this.props.additionalClassNames) {
+            classNames.push(this.props.additionalClassNames);
+        }
         return <span className={classNames.join(" ")} onClick={this.props.onClick} onMouseOver={this.props.onMouseOver}>{text}</span>;
-    } 
+    }
 }
