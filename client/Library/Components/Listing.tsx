998027aa37e7aff9ba8a0840f63da83e9c3e6bd6
Evan Bailey <baileyevang@gmail.com>
2018-02-07 18:49:32 -0800

    Capture alt key state on listing click

--- client/Library/Components/Listing.tsx
+++ client/Library/Components/Listing.tsx
@@ -6,14 +6,16 @@ import { ListingButton } from "./ListingButton";
 export interface ListingProps<T extends Listable> {
     name: string;
     listing: Listing<T>;
-    onAdd: (listing: Listing<T>) => void;
+    onAdd: (listing: Listing<T>, modified: boolean) => void;
     onDelete?: (listing: Listing<T>) => void;
     onEdit?: (listing: Listing<T>) => void;
     onPreview?: (listing: Listing<T>) => void;
 }
 
 export class ListingViewModel<T extends Listable> extends React.Component<ListingProps<T>> {
-    private addFn = () => this.props.onAdd(this.props.listing);
+    private addFn = (event: React.MouseEvent<HTMLSpanElement>) => {
+        this.props.onAdd(this.props.listing, event.altKey);
+    }
     private deleteFn = () => this.props.onDelete(this.props.listing);
     private editFn = () => this.props.onEdit(this.props.listing);
     private previewFn = () => this.props.onPreview(this.props.listing);
