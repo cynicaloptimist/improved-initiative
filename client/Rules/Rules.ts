7db49e977775953e6aada21bcd739eb085c1f992
Evan Bailey <baileyevang@gmail.com>
2018-02-22 23:08:41 -0800

    Add get FormattedString

--- client/Rules/Rules.ts
+++ client/Rules/Rules.ts
@@ -5,10 +5,35 @@ export interface IRules {
 }
 
 export class RollResult {
-    constructor(public Rolls: number[], public Modifier: number) { }
+    constructor(public Rolls: number[], public Modifier: number, public DieSize: number) {
+
+    }
+
     get Total(): number { return this.Rolls.reduce((p, c) => c + p, 0) + this.Modifier; }
+
     get String(): string {
         let output = `[${this.Rolls}]`;
+        if (this.Modifier > 0) {
+            output += ` + ${this.Modifier}`;
+        }
+        if (this.Modifier < 0) {
+            output += ` - ${-this.Modifier}`;
+        }
+        return output + ` = ${this.Total}`;
+    }
+
+    get FormattedString(): string {
+        const formattedRolls = this.Rolls.map(r => {
+            if (r == this.DieSize) {
+                return `<span class='s-roll-max'>${r.toString()}</span>`;
+            }
+            if (r == 1) {
+                return `<span class='s-roll-min'>${r.toString()}</span>`;
+            }
+            return `<span class='s-roll'>${r.toString()}</span>`;
+        }).join(", ");
+
+        let output = `[${formattedRolls}]`;
         if (this.Modifier > 0) {
             output += ` + ${this.Modifier}`;
         }
@@ -33,7 +58,7 @@ export class Dice {
         if (match[4]) {
             const modifier = parseInt(match[4].replace(/[\s]*/g, ""));
             const d20Roll = Math.ceil(Math.random() * 20);
-            return new RollResult([d20Roll], modifier);
+            return new RollResult([d20Roll], modifier, 20);
         }
 
         const howMany = (typeof match[1] == "undefined") ? 1 : parseInt(match[1]);
@@ -44,7 +69,7 @@ export class Dice {
             rolls[i] = Math.ceil(Math.random() * dieSize);
         }
         const modifier = (typeof match[3] == "undefined") ? 0 : parseInt(match[3].replace(/[\s]*/g, ""));
-        return new RollResult(rolls, modifier);
+        return new RollResult(rolls, modifier, dieSize);
     }
 }
 
