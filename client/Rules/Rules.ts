4b0818f4567c8f2c69ec851158f389e74b908367
Evan Bailey <baileyevang@gmail.com>
2018-02-25 22:38:40 -0800

    Allow Ability Check to specify advantage

--- client/Rules/Rules.ts
+++ client/Rules/Rules.ts
@@ -1,3 +1,5 @@
+import * as _ from "lodash";
+
 export interface IRules {
     GetModifierFromScore: (attribute: number) => number;
     AbilityCheck: (mod: number) => number;
@@ -77,7 +79,13 @@ export class DefaultRules implements IRules {
     public GetModifierFromScore = (abilityScore: number) => {
         return Math.floor((abilityScore - 10) / 2);
     }
-    public AbilityCheck = (mod = 0) => {
+    public AbilityCheck = (mod = 0, advantageType?: "advantage" | "disadvantage") => {
+        if (advantageType == "advantage") {
+            return _.max([Math.ceil(Math.random() * 20) + mod, Math.ceil(Math.random() * 20) + mod]);
+        }
+        if (advantageType == "disadvantage") {
+            return _.min([Math.ceil(Math.random() * 20) + mod, Math.ceil(Math.random() * 20) + mod]);
+        }
         return Math.ceil(Math.random() * 20) + mod;
     }
     public EnemyHPTransparency = "whenBloodied";
