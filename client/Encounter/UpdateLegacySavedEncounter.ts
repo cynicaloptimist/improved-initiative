3442b5df256dc8f63e90a802b9376cc2acd31409
Evan Bailey <baileyevang@gmail.com>
2018-04-05 23:27:38 -0700

    Use native property accessors

--- client/Encounter/UpdateLegacySavedEncounter.ts
+++ client/Encounter/UpdateLegacySavedEncounter.ts
@@ -11,13 +11,13 @@ function updateLegacySavedCreature(savedCreature: any) {
 }
 
 export function UpdateLegacySavedEncounter(savedEncounter: any): SavedEncounter<SavedCombatant> {
-    savedEncounter.Combatants = savedEncounter.Combatants || savedEncounter["Creatures"];
-    savedEncounter.ActiveCombatantId = savedEncounter.ActiveCombatantId || savedEncounter["ActiveCreatureId"];
+    savedEncounter.Combatants = savedEncounter.Combatants || savedEncounter.Creatures;
+    savedEncounter.ActiveCombatantId = savedEncounter.ActiveCombatantId || savedEncounter.ActiveCreatureId;
     savedEncounter.Path = savedEncounter.Path || "";
 
     savedEncounter.Combatants.forEach(updateLegacySavedCreature);
 
-    let legacyCombatantIndex = savedEncounter["ActiveCreatureIndex"];
+    const legacyCombatantIndex = savedEncounter.ActiveCreatureIndex;
     if (legacyCombatantIndex !== undefined && legacyCombatantIndex != -1) {
         savedEncounter.ActiveCombatantId = savedEncounter.Combatants[legacyCombatantIndex].Id;
     }
