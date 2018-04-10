43ee699af27b3c8e6d39b8f23cf227a70b67d1a2
Evan Bailey <baileyevang@gmail.com>
2018-04-10 00:51:06 -0700

    Apply manual width to scrollframe div

--- client/Commands/components/Toolbar.tsx
+++ client/Commands/components/Toolbar.tsx
@@ -3,20 +3,28 @@ import { Button } from "../../Components/Button";
 import { Command } from "../Command";
 
 interface ToolbarProps {
-    encounterCommands: Command [];
-    combatantCommands: Command [];
+    encounterCommands: Command[];
+    combatantCommands: Command[];
 }
 
 interface ToolbarState {
     displayWide: boolean;
+    widthStyle: string;
 }
 
 export class Toolbar extends React.Component<ToolbarProps, ToolbarState> {
+    private outerElement: HTMLDivElement;
     constructor(props: ToolbarProps) {
         super(props);
         this.state = {
-            displayWide: false
+            displayWide: false,
+            widthStyle: null
         };
+    }
+
+    public componentDidMount() {
+        const width = this.outerElement.offsetWidth - this.outerElement.clientWidth;
+        this.setState({widthStyle: width.toString() + "px"});
     }
 
     private toggleWidth = () => {
@@ -28,14 +36,17 @@ export class Toolbar extends React.Component<ToolbarProps, ToolbarState> {
         const commandToButton = c => <Button onClick={c.ActionBinding} additionalClassNames={c.ActionBarIcon} />;
         const encounterCommandButtons = this.props.encounterCommands.filter(c => c.ShowOnActionBar()).map(commandToButton);
         const combatantCommandButtons = this.props.combatantCommands.filter(c => c.ShowOnActionBar()).map(commandToButton);
+
         //TODO: Ensure subscription to ShowOnActionBar changes
 
-        return <div className={className}>
-            <div className="commands-encounter">
-                {encounterCommandButtons}
-            </div>
-            <div className="commands-combatant">
-                {combatantCommandButtons}
+        return <div className={className} ref={e => this.outerElement = e}>
+            <div className="scrollframe" style={{ width: this.state.widthStyle }}>
+                <div className="commands-encounter">
+                    {encounterCommandButtons}
+                </div>
+                <div className="commands-combatant">
+                    {combatantCommandButtons}
+                </div>
             </div>
         </div>;
     }
