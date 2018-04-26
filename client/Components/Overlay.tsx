f5211a2d6ddb9e84078876a444871ce304aba108
OmegaJak <jak.kruger@gmail.com>
2018-04-26 00:37:29 -0500

    Overlays can no longer go off the bottom of the window (#218)

--- client/Components/Overlay.tsx
+++ client/Components/Overlay.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import * as ReactDOM from "react-dom";
 
 interface OverlayProps {
     maxHeightPx?: number;
@@ -10,11 +11,14 @@ interface OverlayProps {
 interface OverlayState { }
 
 export class Overlay extends React.Component<OverlayProps, OverlayState> {
+    private height?: number;
+
     public render() {
+        const overflowAmount = Math.max((this.props.top || 0) + (this.height || 0) - window.innerHeight + 4, 0);
         const style: React.CSSProperties = {
             maxHeight: this.props.maxHeightPx || "100%",
             left: this.props.left || 0,
-            top: this.props.top || 0,
+            top: (this.props.top - overflowAmount) || 0,
         };
 
         return <div
@@ -24,5 +28,24 @@ export class Overlay extends React.Component<OverlayProps, OverlayState> {
             onMouseLeave={this.props.handleMouseEvents}>
             {this.props.children}
         </div>;
+    }
+
+    public componentDidMount() {
+        this.updateHeight();
+    }
+
+    public componentDidUpdate() {
+        this.updateHeight();
+    }
+
+    private updateHeight() {
+        let domElement = ReactDOM.findDOMNode(this);
+        if (domElement instanceof Element) {
+            let newHeight = domElement.getBoundingClientRect().height;
+            if (newHeight != this.height) {
+                this.height = newHeight;
+                this.forceUpdate();
+            }
+        }
     }
 }
