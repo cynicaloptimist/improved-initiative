deb92c16ba0fd8b454482ca0e766c38fb8188cdf
Evan Bailey <baileyevang@gmail.com>
2018-05-04 18:56:24 -0700

    Migrate SpellPrompt component to React

--- client/Commands/Prompts/SpellPrompt.tsx
+++ client/Commands/Prompts/SpellPrompt.tsx
@@ -6,37 +6,54 @@ import { Prompt } from "./Prompt";
 
 const numberSuffixes = ["0th", "1st", "2nd", "3rd", "4th", "5th", "6th", "7th", "8th", "9th"];
 
-interface SpellPromptProps {}
-interface SpellPromptState {}
-class SpellPrompt extends React.Component<SpellPromptProps, SpellPromptState> {
+interface SpellPromptProps {
+    Spell: Spell;
+    TextEnricher: StatBlockTextEnricher;
+}
+ 
+class SpellPrompt extends React.Component<SpellPromptProps, null> {
+    private getSpellType = () => {
+        const ritual = this.props.Spell.Ritual ? " (ritual)" : "";
+        if (this.props.Spell.Level === 0) {
+            return `${this.props.Spell.School} cantrip${ritual}`;
+        }
+        const numberSuffix = numberSuffixes[this.props.Spell.School];
+        if (numberSuffix) {
+            return `${numberSuffix}-level ${this.props.Spell.School}${ritual}`;
+        }
+
+        return `Level ${this.props.Spell.Level} ${this.props.Spell.School}${ritual}`;
+    }
+    
     public render() {
-        return "";
+        return <React.Fragment>
+            <div className="spell">
+                <h3>{this.props.Spell.Name}</h3>
+                <p className="spell-type">{this.getSpellType()}</p>
+                <p><label>Casting Time:</label> {this.props.Spell.CastingTime}</p>
+                <p><label>Range:</label> {this.props.Spell.Range}</p>
+                <p><label>Components:</label> {this.props.Spell.Components}</p>
+                <p><label>Duration:</label> {this.props.Spell.Duration}</p>
+                <p><label>Classes:</label> {this.props.Spell.Classes.join(", ")}</p>
+                <p>{this.props.TextEnricher.EnrichText(this.props.Spell.Description)}</p>
+                <p className="spell-source">Source: {this.props.Spell.Source}</p>
+            </div>
+            <button type="submit" className="fa fa-check button"></button>
+        </React.Fragment>;
     }
 }
 
 export class SpellPromptWrapper implements Prompt {
     public InputSelector = "button";
-    public ComponentName = "spellprompt";
-    public Spell = ko.observable(Spell.Default());
+    public ComponentName = "reactprompt";
 
-    public GetType = (spell: Spell) => {
-        const ritual = spell.Ritual ? " (ritual)" : "";
-        if (spell.Level === 0) {
-            return `${spell.School} cantrip${ritual}`;
-        }
-        const numberSuffix = numberSuffixes[spell.School];
-        if (numberSuffix) {
-            return `${numberSuffix}-level ${spell.School}${ritual}`;
-        }
-
-        return `Level ${spell.Level} ${spell.School}${ritual}`;
-    }
+    protected component = ko.observable();
 
     constructor(listing: Listing<Spell>, private textEnricher: StatBlockTextEnricher) {
-        listing.GetAsyncWithUpdatedId(spell => this.Spell(spell));
+        listing.GetAsyncWithUpdatedId(spell => {
+            this.component(<SpellPrompt Spell={spell} TextEnricher={this.textEnricher} />);
+        });
     }
-
-    public EnrichDescription = this.textEnricher.EnrichText;
 
     public Resolve = () => { };
 }
