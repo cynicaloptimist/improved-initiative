f7496a8fc3ea1815d7ce0e47ce2a8ea65d0c9a5b
Evan Bailey <baileyevang@gmail.com>
2018-05-28 22:03:04 -0700

    Add buttons and state to prompt

--- client/Commands/Prompts/PrivacyPolicyPrompt.tsx
+++ client/Commands/Prompts/PrivacyPolicyPrompt.tsx
@@ -1,6 +1,8 @@
 import * as React from "react";
+import { Button } from "../../Components/Button";
 import { StatBlock } from "../../StatBlock/StatBlock";
 import { Metrics } from "../../Utility/Metrics";
+import { Store } from "../../Utility/Store";
 import { Prompt } from "./Prompt";
 
 const privacyPolicyText: string = require("../../../PRIVACY.md");
@@ -8,14 +10,28 @@ const privacyPolicyText: string = require("../../../PRIVACY.md");
 const promptClassName = "p-privacy-policy";
 const inputClassName = promptClassName + "-input";
 
-interface PrivacyPolicyPromptProps { }
-interface PrivacyPolicyPromptState { }
+interface PrivacyPolicyPromptProps {
+    callback: (optin: boolean) => void;
+ }
+interface PrivacyPolicyPromptState {
+    displayFullText: boolean;
+}
 class PrivacyPolicyPrompt extends React.Component<PrivacyPolicyPromptProps, PrivacyPolicyPromptState> {
+    constructor(props) {
+        super(props);
+        this.state = { displayFullText: false };
+    }
 
     public render() {
+        const privacyBrief = "Improved Initiative has a privacy policy. Your data is never sold to third parties. You can help improve the app by sharing your usage data.";
+        const moreInfoButton = <a href="#" onClick={e => this.setState({ displayFullText: true })}>More Info.</a>;
+        const noThanksButton = <Button text="No Thanks" onClick={() => this.props.callback(false)} />;
+        const optInButton = <Button text="Opt In" onClick={() => this.props.callback(true)} />;
+
         return <div className={promptClassName}>
-            <p>Improved Initiative has a privacy policy. Your data is never sold to third parties.</p>
-            <div className={promptClassName + "-full"} dangerouslySetInnerHTML={{ __html: privacyPolicyText }} />
+            <p>{privacyBrief} {moreInfoButton}</p>
+            {this.state.displayFullText && <div className={promptClassName + "-full"} dangerouslySetInnerHTML={{ __html: privacyPolicyText }} />}
+            <div className={promptClassName + "-buttons"}>{noThanksButton} {optInButton}</div>
         </div>;
     }
 }
@@ -26,9 +42,16 @@ export class PrivacyPolicyPromptWrapper implements Prompt {
 
     constructor() { }
 
+    private promptCallback = (optin: boolean) => {
+        if (optin) {
+            Store.Save(Store.User, "AllowTracking", true);
+            Metrics.TrackLoad();
+        }
+    }
+
     public Resolve = (form: HTMLFormElement) => {
 
     }
 
-    private component = <PrivacyPolicyPrompt />;
+    private component = <PrivacyPolicyPrompt callback={this.promptCallback} />;
 }
