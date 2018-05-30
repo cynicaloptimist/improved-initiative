23701c7846afccc767a34f0cd52b05d1850297ec
Evan Bailey <baileyevang@gmail.com>
2018-05-29 21:30:18 -0700

    Test ability score modifier calculation

--- test/Encounter.test.ts
+++
@@ -1,3 +0,0 @@
-test("identity", () => {
-    expect(1).toBe(1);
-});
---
+++ test/Rules.test.ts
@@ -0,0 +1,21 @@
+import { DefaultRules, IRules } from "../client/Rules/Rules";
+
+describe("DefaultRules", () => {
+    let rules: IRules;
+
+    beforeEach(() => {
+        rules = new DefaultRules();
+    });
+
+    test("Ability Score 0", () => {
+        expect(rules.GetModifierFromScore(0)).toBe(-5);
+    });
+
+    test("Ability Score 10", () => {
+        expect(rules.GetModifierFromScore(10)).toBe(0);
+    });
+
+    test("Ability Score 18", () => {
+        expect(rules.GetModifierFromScore(18)).toBe(4);
+    });
+});
