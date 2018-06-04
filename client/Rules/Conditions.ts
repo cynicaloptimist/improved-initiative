5e2f5e2de9d15d682886535277b428b19ab98edd
Evan Bailey <baileyevang@gmail.com>
2018-06-03 22:12:14 -0700

    Add exhaustion to condition reference

--- client/Rules/Conditions.ts
+++ client/Rules/Conditions.ts
@@ -65,5 +65,15 @@ export const Conditions = {
     <li>The creature automatically fails Strength and Dexterity saving throws.</li>
     <li>Attack rolls against the creature have advantage.</li>
     <li>Any Attack that hits the creature is a critical hit if the attacker is within 5 feet of the creature.</li>
-    </ul>`
+    </ul>`,
+    "Exhaustion": `<p>Some special abilities and environmental hazards, such as starvation and the long-­term effects of freezing or scorching temperatures, can lead to a special condition called exhaustion. Exhaustion is measured in six levels. An effect can give a creature one or more levels of exhaustion, as specified in the effect’s description.</p>
+    <p>Table: Exhaustion Effects</p>
+    <p>Level	Effect <br />
+    1	Disadvantage on ability checks <br />
+    2	Speed halved <br />
+    3	Disadvantage on attack rolls and saving throws <br />
+    4	Hit point maximum halved <br />
+    5	Speed reduced to 0 <br />
+    6	Death </p>
+    <p>If an already exhausted creature suffers another effect that causes exhaustion, its current level of exhaustion increases by the amount specified in the effect’s description. A creature suffers the effect of its current level of exhaustion as well as all lower levels. For example, a creature suffering level 2 exhaustion has its speed halved and has disadvantage on ability checks. An effect that removes exhaustion reduces its level as specified in the effect’s description, with all exhaustion effects ending if a creature’s exhaustion level is reduced below 1. Finishing a long rest reduces a creature’s exhaustion level by 1, provided that the creature has also ingested some food and drink.</p>`
 };
