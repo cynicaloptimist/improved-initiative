f477ad4d025767852afd63180ece4c757cf0d161
Evan Bailey <baileyevang@gmail.com>
2018-06-08 22:38:17 -0700

    Add basic initiative ordering test

--- client/test/Encounter.test.ts
+++ client/test/Encounter.test.ts
@@ -40,4 +40,16 @@ describe("Encounter", () => {
     test("Combat should not be active", () => {
         expect(encounter.State()).toBe("inactive");
     });
+
+    test("Initiative Ordering", () => {
+        const slow = encounter.AddCombatantFromStatBlock(StatBlock.Default());
+        const fast = encounter.AddCombatantFromStatBlock(StatBlock.Default());
+        expect(encounter.Combatants()[0]).toBe(slow);
+
+        fast.Initiative(20);
+        slow.Initiative(1);
+        encounter.StartEncounter();
+        expect(encounter.Combatants()[0]).toBe(fast);
+        expect(encounter.Combatants()[1]).toBe(slow);
+    });
 });
