e22f7feba2563198f1e8d1e43f5187ef40b077b3
Evan Bailey <baileyevang@gmail.com>
2018-06-09 23:39:08 -0700

    Clean up scopes

--- client/test/PlayerViewModel.test.ts
+++ client/test/PlayerViewModel.test.ts
@@ -4,31 +4,28 @@ import { PlayerViewModel } from "../PlayerViewModel";
 import { CurrentSettings, InitializeSettings } from "../Settings/Settings";
 import { buildEncounter } from "./buildEncounter";
 
-const mockIo: any = {
-    on: jest.fn(),
-    emit: jest.fn()
-};
-
 describe("PlayerViewModel", () => {
     let playerViewModel: PlayerViewModel;
     let encounter: Encounter;
+
     beforeEach(() => {
         InitializeSettings();
+
+        const mockIo: any = {
+            on: jest.fn(),
+            emit: jest.fn()
+        };
+        
         playerViewModel = new PlayerViewModel(mockIo);
         encounter = buildEncounter();
-        
-    });
-
-    afterEach(() => {
-
+        playerViewModel.LoadSettings(CurrentSettings().PlayerView);
+        playerViewModel.LoadEncounter(encounter.SavePlayerDisplay());
     });
 
     test("Loading the encounter populates combatants", () => {
-        encounter.AddCombatantFromStatBlock({ ...StatBlock.Default(), HP: { Value: 10, Notes: "" } });
-
         expect(playerViewModel.combatants().length).toBe(0);
 
-        playerViewModel.LoadSettings(CurrentSettings().PlayerView);
+        encounter.AddCombatantFromStatBlock({ ...StatBlock.Default(), HP: { Value: 10, Notes: "" } });
         playerViewModel.LoadEncounter(encounter.SavePlayerDisplay());
         
         expect(playerViewModel.combatants().length).toBe(1);
