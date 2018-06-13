4d56f7cf657e0dd3fd38baee187f701a781819dd
Evan Bailey <baileyevang@gmail.com>
2018-06-12 22:44:05 -0700

    Extract method buildStatBlockTextEnricher

--- client/test/buildEncounter.ts
+++ client/test/buildEncounter.ts
@@ -1,11 +1,15 @@
 import { PromptQueue } from "../Commands/Prompts/PromptQueue";
 import { Encounter } from "../Encounter/Encounter";
-import { DefaultRules } from "../Rules/Rules";
+import { DefaultRules, IRules } from "../Rules/Rules";
 import { StatBlockTextEnricher } from "../StatBlock/StatBlockTextEnricher";
+
+export function buildStatBlockTextEnricher(rules: IRules) {
+    return new StatBlockTextEnricher(jest.fn(), jest.fn(), jest.fn(), null, rules);
+}
 
 export function buildEncounter() {
     const rules = new DefaultRules();
-    const enricher = new StatBlockTextEnricher(jest.fn(), jest.fn(), jest.fn(), null, rules);
+    const enricher = buildStatBlockTextEnricher(rules);
     const encounter = new Encounter(new PromptQueue(), null, jest.fn().mockReturnValue(null), jest.fn(), rules, enricher);
     return encounter;
 }
