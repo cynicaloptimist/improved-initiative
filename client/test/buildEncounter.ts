847d01925cd01bd65fba99800cdd225ce0f76e12
Evan Bailey <baileyevang@gmail.com>
2018-06-12 22:52:17 -0700

    Use an actual SpellLibrary in test StatBlockTextEnricher

--- client/test/buildEncounter.ts
+++ client/test/buildEncounter.ts
@@ -1,10 +1,11 @@
 import { PromptQueue } from "../Commands/Prompts/PromptQueue";
 import { Encounter } from "../Encounter/Encounter";
+import { SpellLibrary } from "../Library/SpellLibrary";
 import { DefaultRules, IRules } from "../Rules/Rules";
 import { StatBlockTextEnricher } from "../StatBlock/StatBlockTextEnricher";
 
 export function buildStatBlockTextEnricher(rules: IRules) {
-    return new StatBlockTextEnricher(jest.fn(), jest.fn(), jest.fn(), null, rules);
+    return new StatBlockTextEnricher(jest.fn(), jest.fn(), jest.fn(), new SpellLibrary(), rules);
 }
 
 export function buildEncounter() {
