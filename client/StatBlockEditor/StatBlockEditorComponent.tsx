213369e6d0fbc26e524f3f8e28bd39241ab8054b
Evan Bailey <baileyevang@gmail.com>
2018-06-30 12:58:15 -0700

    Add name field and submit button

--- client/StatBlockEditor/StatBlockEditorComponent.tsx
+++ client/StatBlockEditor/StatBlockEditorComponent.tsx
@@ -1,13 +1,26 @@
 import React = require("react");
+import { Form, Text,  } from "react-form";
 import { StatBlock } from "../../common/StatBlock";
 
 export class StatBlockEditor extends React.Component<StatBlockEditorProps, StatBlockEditorState> {
-    public saveAndClose = () => {
-        this.props.onSave(this.props.statBlock);
+    public saveAndClose = (submittedValues?) => {
+        const editedStatBlock = {
+            ...this.props.statBlock,
+            Name: submittedValues.name
+        };
+        
+        this.props.onSave(editedStatBlock);
     }
-    
+
     public render() {
-        return "";
+        const statBlock = this.props.statBlock;
+        return <Form onSubmit={this.saveAndClose}
+            render={api => (
+            <form onSubmit={api.submitForm}>
+                    <Text field="name" value={statBlock.Name} />
+                    <button type="submit"><span className="button fa fa-save"></span></button>
+            </form>
+        )} />;
     }
 }
 
@@ -16,4 +29,4 @@ interface StatBlockEditorProps {
     onSave: (statBlock: StatBlock) => void;
 }
 
-interface StatBlockEditorState {}
+interface StatBlockEditorState { }
