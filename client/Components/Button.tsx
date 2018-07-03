7ce077b5df221b09c4a8c01402eb60c8de033bc5
Evan Bailey <baileyevang@gmail.com>
2018-07-02 23:32:12 -0700

    Disable save-as button

--- client/Components/Button.tsx
+++ client/Components/Button.tsx
@@ -5,6 +5,7 @@ export interface ButtonProps {
     faClass?: string;
     tooltip?: string;
     additionalClassNames?: string;
+    disabled?: boolean;
     onClick: React.MouseEventHandler<HTMLSpanElement>;
     onMouseOver?: React.MouseEventHandler<HTMLSpanElement>;
 }
@@ -13,16 +14,20 @@ export class Button extends React.Component<ButtonProps> {
     public render() {
         const text = this.props.text || "";
         const classNames = ["c-button", "fa"];
+        const disabled = this.props.disabled || false;
         if (this.props.faClass) {
             classNames.push(`fa-${this.props.faClass}`);
         }
         if (this.props.additionalClassNames) {
             classNames.push(this.props.additionalClassNames);
         }
+        if (disabled) {
+            classNames.push("s-disabled");
+        }
         return <span
             className={classNames.join(" ")}
-            onClick={this.props.onClick}
-            onMouseOver={this.props.onMouseOver}
+            onClick={!disabled && this.props.onClick}
+            onMouseOver={!disabled && this.props.onMouseOver}
             title={this.props.tooltip}
         >{text}</span>;
     }
