c7e3e4728621e0929cc9ccd5d23dea74dbc0fbe1
Evan Bailey <baileyevang@gmail.com>
2018-07-04 00:36:10 -0700

    Remove duplicate test

--- client/StatBlockEditor/StatBlockEditor.test.tsx
+++ client/StatBlockEditor/StatBlockEditor.test.tsx
@@ -1,7 +1,6 @@
 import * as Enzyme from "enzyme";
 import * as Adapter from "enzyme-adapter-react-16";
 import * as React from "react";
-import * as renderer from "react-test-renderer";
 
 import { StatBlockEditor } from "./StatBlockEditorComponent";
 
@@ -10,17 +9,6 @@ import { StatBlock } from "../../common/StatBlock";
 Enzyme.configure({ adapter: new Adapter() });
 
 describe("StatBlockEditor", () => {
-    test("Calls saveCallback with the provided statblock", () => {
-        expect.assertions(1);
-        const statBlock = StatBlock.Default();
-
-        const editor = renderer.create(<StatBlockEditor statBlock={statBlock} editMode="library" onClose={jest.fn()} onSave={editedStatBlock => {
-            expect(editedStatBlock).toEqual(statBlock);
-        }} />).getInstance() as (renderer.ReactTestInstance & StatBlockEditor);
-
-        editor.saveAndClose(statBlock);
-    });
-
     test.only("Calls saveCallback with the provided statblock", done => {
         expect.assertions(1);
         const statBlock = StatBlock.Default();
