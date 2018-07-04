13668de8db3b97bcea737eb308cb802a725b9126
Evan Bailey <baileyevang@gmail.com>
2018-07-04 00:36:02 -0700

    Convert renderer to enzyme

--- client/Components/StatBlock.test.tsx
+++ client/Components/StatBlock.test.tsx
@@ -1,20 +1,24 @@
+import * as Enzyme from "enzyme";
+import * as Adapter from "enzyme-adapter-react-16";
 import * as React from "react";
-import * as renderer from "react-test-renderer";
+
 import { StatBlock } from "../../common/StatBlock";
 import { DefaultRules } from "../Rules/Rules";
 import { buildStatBlockTextEnricher } from "../test/buildEncounter";
 import { StatBlockComponent } from "./StatBlock";
 
+Enzyme.configure({ adapter: new Adapter() });
+
 describe("StatBlock component", () => {
     test("Shows the statblock's name", () => {
         const rules = new DefaultRules();
-        const component = renderer.create(
+        const component = Enzyme.render(
             <StatBlockComponent
                 statBlock={{ ...StatBlock.Default(), Name: "Snarglebargle" }}
                 enricher={buildStatBlockTextEnricher(rules)}
                 displayMode="default"
             />);
-        const header = component.toJSON().children.filter(c => c.type == "h3").pop();
-        expect(header.children).toEqual(["Snarglebargle"]);
+        const headerText = component.find("h3").text();
+        expect(headerText).toEqual("Snarglebargle");
     });
 });
