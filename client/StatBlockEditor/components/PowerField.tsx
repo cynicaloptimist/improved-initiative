313cd5f8d00917c0b5b1888d0d0bd93ea0b0d96e
Evan Bailey <baileyevang@gmail.com>
2018-07-06 22:54:23 -0700

    Autofocus new powers

--- client/StatBlockEditor/components/PowerField.tsx
+++ client/StatBlockEditor/components/PowerField.tsx
@@ -10,11 +10,20 @@ interface PowerFieldProps {
 interface PowerFieldState { }
 
 export class PowerField extends React.Component<PowerFieldProps, PowerFieldState> {
+    private nameInput: HTMLInputElement;
+
+    public componentDidMount() {
+        if (this.nameInput.value == "") {
+            this.nameInput.focus();
+        }
+    }
+
     public render() {
         return <div>
             <div className="inline">
                 <Field type="text" className="name" placeholder="Name"
                     name={`${this.props.powerType}[${this.props.index}].Name`}
+                    innerRef={f => this.nameInput = f}
                 />
                 <span className="fa-clickable fa-trash"
                     onClick={() => this.props.remove(this.props.index)}
