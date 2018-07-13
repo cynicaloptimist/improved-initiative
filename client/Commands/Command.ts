c98b3924ec464847e1655317a3848e504242cb85
Evan Bailey <baileyevang@gmail.com>
2018-07-13 00:43:42 -0700

    Refactor buttons for fewer classnames

--- client/Commands/Command.ts
+++ client/Commands/Command.ts
@@ -36,31 +36,31 @@ export class Command {
 }
 
 export let BuildEncounterCommandList = (c: EncounterCommander, saveEncounterFn: () => void) => [
-    new Command("Toggle Menu", c.ToggleToolbarWidth, "alt+m", "fa-bars", true, true),
-    new Command("Start Encounter", c.StartEncounter, "alt+r", "fa-play"),
-    new Command("Reroll Initiative", c.RerollInitiative, "alt+shift+i", "fa-refresh", false),
-    new Command("End Encounter", c.EndEncounter, "alt+e", "fa-stop"),
-    new Command("Clear Encounter", c.ClearEncounter, "alt+del", "fa-trash"),
-    new Command("Open Library", c.ShowLibraries, "alt+a", "fa-user-plus"),
-    new Command("Quick Add Combatant", c.QuickAddStatBlock, "alt+q", "fa-asterisk"),
-    new Command("Show Player Window", c.LaunchPlayerWindow, "alt+w", "fa-users"),
-    new Command("Next Turn", c.NextTurn, "n", "fa-step-forward"),
-    new Command("Previous Turn", c.PreviousTurn, "alt+n", "fa-step-backward"),
-    new Command("Save Encounter", saveEncounterFn, "alt+s", "fa-save"),
-    new Command("Settings", c.ShowSettings, "?", "fa-gear", true, true)
+    new Command("Toggle Menu", c.ToggleToolbarWidth, "alt+m", "bars", true, true),
+    new Command("Start Encounter", c.StartEncounter, "alt+r", "play"),
+    new Command("Reroll Initiative", c.RerollInitiative, "alt+shift+i", "refresh", false),
+    new Command("End Encounter", c.EndEncounter, "alt+e", "stop"),
+    new Command("Clear Encounter", c.ClearEncounter, "alt+del", "trash"),
+    new Command("Open Library", c.ShowLibraries, "alt+a", "user-plus"),
+    new Command("Quick Add Combatant", c.QuickAddStatBlock, "alt+q", "asterisk"),
+    new Command("Show Player Window", c.LaunchPlayerWindow, "alt+w", "users"),
+    new Command("Next Turn", c.NextTurn, "n", "step-forward"),
+    new Command("Previous Turn", c.PreviousTurn, "alt+n", "step-backward"),
+    new Command("Save Encounter", saveEncounterFn, "alt+s", "save"),
+    new Command("Settings", c.ShowSettings, "?", "gear", true, true)
 ];
 
 export let BuildCombatantCommandList: (c: CombatantCommander) => Command[] = c => [
-    new Command("Damage/Heal", c.EditHP, "t", "fa-plus-circle"),
-    new Command("Apply Temporary HP", c.AddTemporaryHP, "alt+t", "fa-medkit"),
-    new Command("Add Note", c.AddTag, "g", "fa-tag", false),
-    new Command("Remove from Encounter", c.Remove, "del", "fa-remove"),
-    new Command("Rename", c.EditName, "f2", "fa-i-cursor"),
-    new Command("Edit Statblock", c.EditStatBlock, "alt+e", "fa-edit", false),
-    new Command("Edit Initiative", c.EditInitiative, "alt+i", "fa-play-circle-o", false),
-    new Command("Link Initiative", c.LinkInitiative, "alt+l", "fa-link", false),
-    new Command("Move Down", c.MoveDown, "alt+j", "fa-angle-double-down"),
-    new Command("Move Up", c.MoveUp, "alt+k", "fa-angle-double-up"),
-    new Command("Select Next", c.SelectNext, "j", "fa-arrow-down", false),
-    new Command("Select Previous", c.SelectPrevious, "k", "fa-arrow-up", false)
+    new Command("Damage/Heal", c.EditHP, "t", "plus-circle"),
+    new Command("Apply Temporary HP", c.AddTemporaryHP, "alt+t", "medkit"),
+    new Command("Add Note", c.AddTag, "g", "tag", false),
+    new Command("Remove from Encounter", c.Remove, "del", "remove"),
+    new Command("Rename", c.EditName, "f2", "i-cursor"),
+    new Command("Edit Statblock", c.EditStatBlock, "alt+e", "edit", false),
+    new Command("Edit Initiative", c.EditInitiative, "alt+i", "play-circle-o", false),
+    new Command("Link Initiative", c.LinkInitiative, "alt+l", "link", false),
+    new Command("Move Down", c.MoveDown, "alt+j", "angle-double-down"),
+    new Command("Move Up", c.MoveUp, "alt+k", "angle-double-up"),
+    new Command("Select Next", c.SelectNext, "j", "arrow-down", false),
+    new Command("Select Previous", c.SelectPrevious, "k", "arrow-up", false)
 ];
--- client/Components/Button.tsx
+++ client/Components/Button.tsx
@@ -13,22 +13,21 @@ export interface ButtonProps {
 export class Button extends React.Component<ButtonProps> {
     public render() {
         const text = this.props.text || "";
-        const classNames = ["c-button", "fa"];
+        
         const disabled = this.props.disabled || false;
-        if (this.props.faClass) {
-            classNames.push(`fa-${this.props.faClass}`);
-        }
+
+        const classNames = [disabled ? "c-button--disabled" : "c-button"];
         if (this.props.additionalClassNames) {
             classNames.push(this.props.additionalClassNames);
         }
-        if (disabled) {
-            classNames.push("s-disabled");
-        }
+
+        const faElement = this.props.faClass && <span className={`fa fa-${this.props.faClass}`} />;
+
         return <span
             className={classNames.join(" ")}
             onClick={!disabled && this.props.onClick}
             onMouseOver={!disabled && this.props.onMouseOver}
             title={this.props.tooltip}
-        >{text}</span>;
+        >{faElement}{text}</span>;
     }
 }
