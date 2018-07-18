f7af1b89012e9d5f4d7b852874a4f83d5a1a9b66
Evan Bailey <baileyevang@gmail.com>
2018-07-17 20:54:46 -0700

    Rename component className

--- client/StatBlockEditor/components/TextField.tsx
+++ client/StatBlockEditor/components/TextField.tsx
@@ -2,7 +2,7 @@ import { Field } from "formik";
 import * as React from "react";
 
 export const TextField = (props: { label: string, fieldName: string }) =>
-    <label className="c-statblock-editor-text">
+    <label className="c-text-field">
         <span className="label">{props.label}</span>
         <Field type="text" name={props.fieldName} />
     </label>;
