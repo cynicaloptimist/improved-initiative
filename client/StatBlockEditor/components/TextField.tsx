4568092cfc7b90bee9b59bb000f7fee204425588
Evan Bailey <baileyevang@gmail.com>
2018-07-24 22:02:32 -0700

    Style generic text field like labelled text

--- client/StatBlockEditor/components/TextField.tsx
+++ client/StatBlockEditor/components/TextField.tsx
@@ -3,6 +3,6 @@ import * as React from "react";
 
 export const TextField = (props: { label: string, fieldName: string }) =>
     <label className="c-text-field">
-        <span className="label">{props.label}</span>
+        <div className="label">{props.label}</div>
         <Field type="text" name={props.fieldName} />
     </label>;
