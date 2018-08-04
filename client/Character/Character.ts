39ec391569f336381333fd204df528bfd765c721
Evan Bailey <baileyevang@gmail.com>
2018-08-03 20:48:07 -0700

    Add Notes field to Character

--- client/Character/Character.ts
+++ client/Character/Character.ts
@@ -3,11 +3,13 @@ import { StatBlock } from "../../common/StatBlock";
 export function InitializeCharacter(statBlock: StatBlock): Character {
     return {
         CurrentHP: statBlock.HP.Value,
-        StatBlock: statBlock
+        StatBlock: statBlock,
+        Notes: ""
     };
 }
 
 export interface Character {
     CurrentHP: number;
     StatBlock: StatBlock;
+    Notes: string;
 }
