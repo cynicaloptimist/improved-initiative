7d54e8e4289ceee0bb85edd546a4b8d364ec8b8b
Evan Bailey <baileyevang@gmail.com>
2018-08-03 21:30:59 -0700

    Add aspirational tests

--- client/Character/Character.test.ts
+++ client/Character/Character.test.ts
@@ -9,3 +9,17 @@ describe("InitializeCharacter", () => {
         expect(character.CurrentHP).toBe(10);
     });
 });
+
+it("Should update existing PlayerCharacter statblocks to Character");
+
+it("SavedCombatants should load their CurrentHP from the Character");
+
+it("Should allow the user to save notes");
+
+it("Should update the Character when a linked Combatant's hp changes");
+
+it("Should update the statblock when it is edited from the library");
+
+it("Should update the statblock when it is edited from the combatant");
+
+it("Should render combatant notes with markdown");
