4b6fd7899c9b2de6e8e91a236848f787562119c9
Evan Bailey <baileyevang@gmail.com>
2018-08-05 23:44:19 -0700

    Add ID to PersistentCharacter

--- common/PersistentCharacter.ts
+++ common/PersistentCharacter.ts
@@ -1,7 +1,9 @@
 import { StatBlock } from "./StatBlock";
+import { probablyUniqueString } from "./Toolbox";
 
 export function InitializeCharacter(statBlock: StatBlock): PersistentCharacter {
     return {
+        Id: probablyUniqueString(),
         CurrentHP: statBlock.HP.Value,
         StatBlock: statBlock,
         Notes: ""
@@ -11,6 +13,7 @@ export function InitializeCharacter(statBlock: StatBlock): PersistentCharacter {
 export const DefaultPersistentCharacter = () => InitializeCharacter(StatBlock.Default());
 
 export interface PersistentCharacter {
+    Id: string;
     CurrentHP: number;
     StatBlock: StatBlock;
     Notes: string;
