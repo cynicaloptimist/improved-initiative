b58d2229a524880a01e00a0a933d4792545345cc
Evan Bailey <baileyevang@gmail.com>
2018-08-12 16:03:31 -0700

    Test upsertUser

--- server/dbconnection.test.ts
+++ server/dbconnection.test.ts
@@ -1,4 +1,5 @@
 import MongodbMemoryServer from "mongodb-memory-server";
+import { initialize, upsertUser } from "./dbconnection";
 
 describe("User Accounts", () => {
     let mongod: MongodbMemoryServer;
@@ -6,14 +7,20 @@ describe("User Accounts", () => {
     beforeAll(async () => {
         mongod = new MongodbMemoryServer();
         const uri = await mongod.getUri();
+        initialize(uri);
     });
 
     afterAll(async () => {
         await mongod.stop();
     });
 
-    test("", async () => {
-        const name = await mongod.getDbName();
-        console.log(name);
+    test("Should initialize user with empty entity sets", async (done) => {
+        const user = await upsertUser("patreonId", "accessKey", "refreshKey", "none");
+        expect(user.encounters).toEqual({});
+        expect(user.playercharacters).toEqual({});
+        expect(user.statblocks).toEqual({});
+        expect(user.spells).toEqual({});
+        expect(user.persistentcharacters).toEqual({});
+        done();
     });
 });
