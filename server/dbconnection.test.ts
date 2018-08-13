9d9cd0dc12945fcb8b73f32bada1798a67674c39
Evan Bailey <baileyevang@gmail.com>
2018-08-12 18:30:25 -0700

    Add baseline mongodb test

--- server/dbconnection.test.ts
+++ server/dbconnection.test.ts
@@ -1,13 +1,17 @@
+import { MongoClient } from "mongodb";
 import MongodbMemoryServer from "mongodb-memory-server";
-import { initialize, upsertUser } from "./dbconnection";
+import { StatBlock } from "../common/StatBlock";
+import { probablyUniqueString } from "../common/Toolbox";
+import * as DB from "./dbconnection";
 
 describe("User Accounts", () => {
     let mongod: MongodbMemoryServer;
+    let uri;
 
     beforeAll(async () => {
         mongod = new MongodbMemoryServer();
-        const uri = await mongod.getUri();
-        initialize(uri);
+        uri = await mongod.getUri();
+        DB.initialize(uri);
     });
 
     afterAll(async () => {
@@ -15,12 +19,22 @@ describe("User Accounts", () => {
     });
 
     test("Should initialize user with empty entity sets", async (done) => {
-        const user = await upsertUser("patreonId", "accessKey", "refreshKey", "none");
+        const user = await DB.upsertUser(probablyUniqueString(), "accessKey", "refreshKey", "pledge");
         expect(user.encounters).toEqual({});
         expect(user.playercharacters).toEqual({});
         expect(user.statblocks).toEqual({});
         expect(user.spells).toEqual({});
         expect(user.persistentcharacters).toEqual({});
+        done();
+    });
+
+    test("baseline", async (done) => {
+        expect.assertions(1);
+        const db = await MongoClient.connect(uri);
+        const test = await db.collection("test").insertOne({ "foo": "bar" });
+        const id = test.insertedId;
+        const lookup = await db.collection("test").findOne({ _id: id });
+        expect(lookup.foo).toEqual("bar");
         done();
     });
 });
