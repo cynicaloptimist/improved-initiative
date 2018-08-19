b847baf419d839ba58932479a33625e822c141dd
Evan Bailey <baileyevang@gmail.com>
2018-08-18 19:20:27 -0700

    Test initiative ordering by group mod

--- client/Encounter/Encounter.test.ts
+++ client/Encounter/Encounter.test.ts
@@ -24,15 +24,29 @@ describe("Encounter", () => {
         expect(encounter.State()).toBe("inactive");
     });
 
-    test("Initiative Ordering", () => {
-        const slow = encounter.AddCombatantFromStatBlock(StatBlock.Default());
-        const fast = encounter.AddCombatantFromStatBlock(StatBlock.Default());
-        expect(encounter.Combatants()[0]).toBe(slow);
-
-        fast.Initiative(20);
-        slow.Initiative(1);
-        encounter.StartEncounter();
-        expect(encounter.Combatants()[0]).toBe(fast);
-        expect(encounter.Combatants()[1]).toBe(slow);
+    describe("Initiative Ordering", () => {
+        test("By roll", () => {
+            const slow = encounter.AddCombatantFromStatBlock(StatBlock.Default());
+            const fast = encounter.AddCombatantFromStatBlock(StatBlock.Default());
+            expect(encounter.Combatants()[0]).toBe(slow);
+    
+            fast.Initiative(20);
+            slow.Initiative(1);
+            encounter.StartEncounter();
+            expect(encounter.Combatants()[0]).toBe(fast);
+            expect(encounter.Combatants()[1]).toBe(slow);
+        });
+        
+        test("By group modifier", () => {
+            const slow = encounter.AddCombatantFromStatBlock({ ...StatBlock.Default(), InitiativeModifier: 0 });
+            const fast = encounter.AddCombatantFromStatBlock({ ...StatBlock.Default(), InitiativeModifier: 2 });
+            const loner = encounter.AddCombatantFromStatBlock({ ...StatBlock.Default(), InitiativeModifier: 1 });
+            slow.InitiativeGroup("group");
+            fast.InitiativeGroup("group");
+            encounter.StartEncounter();
+            expect(encounter.Combatants()[0]).toBe(fast);
+            expect(encounter.Combatants()[1]).toBe(slow);
+            expect(encounter.Combatants()[2]).toBe(loner);
+        });
     });
 });
