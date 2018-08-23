51c510eddac62e2d40a739f5c560ae42fd9ff4fb
Evan Bailey <baileyevang@gmail.com>
2018-08-22 23:12:39 -0700

    Clean up default interface a bit

--- client/Commands/Command.ts
+++ client/Commands/Command.ts
@@ -45,20 +45,20 @@ export let BuildEncounterCommandList = (c: EncounterCommander, saveEncounterFn: 
     new Command("start-encounter", "Start Encounter", c.StartEncounter, "alt+r", "play"),
     new Command("reroll-initiative", "Reroll Initiative", c.RerollInitiative, "alt+shift+i", "sync", false),
     new Command("end-encounter", "End Encounter", c.EndEncounter, "alt+e", "stop"),
-    new Command("clear-encounter", "Clear Encounter", c.ClearEncounter, "alt+shift+del", "trash"),
+    new Command("clear-encounter", "Clear Encounter", c.ClearEncounter, "alt+shift+del", "trash", false),
     new Command("clean-encounter", "Clean Encounter", c.CleanEncounter, "alt+del", "broom"),
     new Command("open-library", "Open Library", c.ShowLibraries, "alt+a", "book"),
-    new Command("quick-add", "Quick Add Combatant", c.QuickAddStatBlock, "alt+q", "bolt"),
+    new Command("quick-add", "Quick Add Combatant", c.QuickAddStatBlock, "alt+q", "bolt", false),
     new Command("player-window", "Show Player Window", c.LaunchPlayerWindow, "alt+w", "users"),
     new Command("next-turn", "Next Turn", c.NextTurn, "n", "step-forward"),
-    new Command("previous-turn", "Previous Turn", c.PreviousTurn, "alt+n", "step-backward"),
+    new Command("previous-turn", "Previous Turn", c.PreviousTurn, "alt+n", "step-backward", false),
     new Command("save-encounter", "Save Encounter", saveEncounterFn, "alt+s", "save"),
     new Command("settings", "Settings", c.ShowSettings, "?", "cog", true, true)
 ];
 
 export let BuildCombatantCommandList: (c: CombatantCommander) => Command[] = c => [
     new Command("apply-damage", "Damage/Heal", c.EditHP, "t", "plus-circle"),
-    new Command("apply-temporary-hp", "Apply Temporary HP", c.AddTemporaryHP, "alt+t", "medkit"),
+    new Command("apply-temporary-hp", "Apply Temporary HP", c.AddTemporaryHP, "alt+t", "medkit", false),
     new Command("add-tag", "Add Note", c.AddTag, "g", "tag", false),
     new Command("remove", "Remove from Encounter", c.Remove, "del", "times"),
     new Command("set-alias", "Rename", c.SetAlias, "f2", "i-cursor"),
