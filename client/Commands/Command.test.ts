cb92452a6327a28409fa4483344824fcff72f0e7
Evan Bailey <baileyevang@gmail.com>
2018-09-01 20:34:30 -0700

    Use Store.Settings

--- client/Commands/Command.test.ts
+++ client/Commands/Command.test.ts
@@ -1,3 +1,4 @@
+import { getDefaultSettings } from "../Settings/Settings";
 import { Store } from "../Utility/Store";
 import { Command } from "./Command";
 
@@ -8,12 +9,21 @@ describe("Command", () => {
     });
 
     test("Should load a saved keybinding", () => {
-        Store.Save(Store.KeyBindings, "some-command-id", "saved-keybinding");
+        const settings = getDefaultSettings();
+        settings.Commands = [
+            {
+                Name: "some-command-id",
+                KeyBinding: "saved-keybinding",
+                ShowOnActionBar: true
+            }
+        ];
+        Store.Save(Store.User, "Settings", settings);
+        
         const command = new Command("some-command-id", "Some Command", jest.fn(), "default-keybinding", "square");
         expect(command.KeyBinding).toEqual("saved-keybinding");
     });
 
-    test("Should load a saved keybinding", () => {
+    test("Should load a legacy keybinding", () => {
         Store.Save(Store.KeyBindings, "Add Note", "legacy-keybinding");
         const command = new Command("add-tag", "Add Tag", jest.fn(), "default-keybinding", "square");
         expect(command.KeyBinding).toEqual("legacy-keybinding");
