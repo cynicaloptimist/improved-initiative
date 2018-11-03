da54cd72cc22283e750cfb196b32483a99ba950f
Evan Bailey <baileyevang@gmail.com>
2018-11-03 14:54:45 -0700

    Move methods to file

---
+++ client/Utility/TransferLocalStorage.ts
@@ -0,0 +1,29 @@
+import { Store } from "./Store";
+
+function transferLocalStorageToCanonicalUrl(canonicalUrl: string) {
+    const iframe = document.getElementById("localstorage-transfer-target") as HTMLIFrameElement;
+    iframe.onload = () => iframe.contentWindow.postMessage({ transferredLocalStorage: JSON.stringify(localStorage) }, "*");
+    window.onmessage = getTransferCompleteCallback(canonicalUrl);
+}
+
+function getTransferCompleteCallback(canonicalUrl: string) {
+    return (e: MessageEvent) => {
+        if (e.origin !== canonicalUrl) {
+            return;
+        }
+        Store.Save(Store.User, "StorageTransferred", true);
+        window.location.href = canonicalUrl;
+    };
+}
+
+export function TransferLocalStorageToCanonicalURLIfNeeded(canonicalUrl: string) {
+    const notAtCanonicalUrl = canonicalUrl.length > 0 && window.location.href != canonicalUrl + "/";
+    if (notAtCanonicalUrl) {
+        const isFirstVisit = Store.Load(Store.User, "SkipIntro") === null;
+        if (isFirstVisit) {
+            window.location.href = canonicalUrl;
+        } else {
+            transferLocalStorageToCanonicalUrl(canonicalUrl);
+        }
+    }
+}
