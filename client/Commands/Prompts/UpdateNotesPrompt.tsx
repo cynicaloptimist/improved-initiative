19892fe68620fccee39a7e9f7ce29ce00002417f
Evan Bailey <baileyevang@gmail.com>
2018-11-30 17:55:27 -0800

    Add UpdateNotesPrompt

--- client/Commands/Prompts/UpdateNotesPrompt.tsx
+++ client/Commands/Prompts/UpdateNotesPrompt.tsx
@@ -1,14 +1,21 @@
 import * as React from "react";
 
+import { PersistentCharacter } from "../../../common/PersistentCharacter";
+import { PersistentCharacterLibrary } from "../../Library/PersistentCharacterLibrary";
 import { Prompt } from "./Prompt";
 
-interface UpdateNotesPromptComponentProps { }
+interface UpdateNotesPromptComponentProps {
+    currentNotes: string;
+ }
 
 interface UpdateNotesPromptComponentState { }
 
 class UpdateNotesPromptComponent extends React.Component<UpdateNotesPromptComponentProps, UpdateNotesPromptComponentState> {
     public render() {
-        return "";
+        return <React.Fragment>
+            <textarea className="response" defaultValue={this.props.currentNotes} placeholder="Track long term resources and conditions" />
+            <button type="submit" className="fas fa-check button"></button>
+        </React.Fragment> ;
     }
 }
 
@@ -17,11 +24,14 @@ export class UpdateNotesPrompt implements Prompt {
     public ComponentName = "reactprompt";
     public component: JSX.Element;
 
-    constructor() {
-        this.component = <UpdateNotesPromptComponent />;
+    constructor(private persistentCharacter: PersistentCharacter, private library: PersistentCharacterLibrary) {
+        this.component = <UpdateNotesPromptComponent currentNotes={persistentCharacter.Notes} />;
     }
 
     public Resolve = (form: HTMLFormElement) => {
-
+        const input = form.getElementsByClassName("response")[0] as HTMLTextAreaElement;
+        this.library.UpdatePersistentCharacter(this.persistentCharacter.Id, {
+            Notes: input.value
+        });
     }
 }
