af9681f57c238550008f203922d4355d0eb3344e
Evan Bailey <baileyevang@gmail.com>
2018-12-01 17:11:19 -0800

    Restore buildStatBlockTextEnricher in its own file

---
+++ client/test/buildStatBlockTextEnricher.ts
@@ -0,0 +1,7 @@
+import { SpellLibrary } from "../Library/SpellLibrary";
+import { IRules } from "../Rules/Rules";
+import { TextEnricher } from "../TextEnricher/TextEnricher";
+
+export function buildStatBlockTextEnricher(rules: IRules) {
+    return new TextEnricher(jest.fn(), jest.fn(), jest.fn(), new SpellLibrary(null), rules);
+}
