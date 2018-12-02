88116155b595c58c2cd0332966a4f7986e26b43b
Evan Bailey <baileyevang@gmail.com>
2018-12-01 23:13:47 -0800

    Update default binding for end-encounter

--- client/Commands/BuildEncounterCommandList.ts
+++ client/Commands/BuildEncounterCommandList.ts
@@ -5,7 +5,7 @@ export const BuildEncounterCommandList = (c: EncounterCommander, saveEncounterFn
     new Command("toggle-menu", "Toggle Menu", c.ToggleToolbarWidth, "alt+m", "bars", true, true),
     new Command("start-encounter", "Start Encounter", c.StartEncounter, "alt+r", "play"),
     new Command("reroll-initiative", "Reroll Initiative", c.RerollInitiative, "alt+shift+i", "sync", false),
-    new Command("end-encounter", "End Encounter", c.EndEncounter, "alt+e", "stop"),
+    new Command("end-encounter", "End Encounter", c.EndEncounter, "shift+alt+r", "stop"),
     new Command("clear-encounter", "Clear Encounter", c.ClearEncounter, "alt+shift+del", "trash", false),
     new Command("clean-encounter", "Clean Encounter", c.CleanEncounter, "alt+del", "broom"),
     new Command("open-library", "Open Library", c.ShowLibraries, "alt+a", "book"),
