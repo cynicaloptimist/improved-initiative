1d303e0be4ddc9a4b476227a2f01d923aafeda13
Evan Bailey <baileyevang@gmail.com>
2018-12-01 17:09:08 -0800

    Remove enricher from test encounter

--- client/test/buildEncounter.ts
+++ client/test/buildEncounter.ts
@@ -1,16 +1,8 @@
 import { Encounter } from "../Encounter/Encounter";
-import { SpellLibrary } from "../Library/SpellLibrary";
-import { PlayerViewClient } from "../Player/PlayerViewClient";
 import { DefaultRules, IRules } from "../Rules/Rules";
-import { TextEnricher } from "../TextEnricher/TextEnricher";
-
-export function buildStatBlockTextEnricher(rules: IRules) {
-    return new TextEnricher(jest.fn(), jest.fn(), jest.fn(), new SpellLibrary(null), rules);
-}
 
 export function buildEncounter() {
     const rules = new DefaultRules();
-    const enricher = buildStatBlockTextEnricher(rules);
-    const encounter = new Encounter(null, jest.fn().mockReturnValue(null), jest.fn(), rules, enricher);
+    const encounter = new Encounter(null, jest.fn().mockReturnValue(null), jest.fn(), rules);
     return encounter;
 }
