5958c61fc7c1d36f7115def9589658aa750aea60
Evan Bailey <baileyevang@gmail.com>
2018-12-11 17:54:47 -0800

    Widen text field and indicate markdown in update notes prompt

--- client/Commands/Prompts/UpdateNotesPrompt.tsx
+++ client/Commands/Prompts/UpdateNotesPrompt.tsx
@@ -7,21 +7,24 @@ import { Prompt } from "./Prompt";
 
 interface UpdateNotesPromptComponentProps {
     currentNotes: string;
- }
+}
 
 interface UpdateNotesPromptComponentState { }
 
 class UpdateNotesPromptComponent extends React.Component<UpdateNotesPromptComponentProps, UpdateNotesPromptComponentState> {
     public render() {
-        return <React.Fragment>
-            <textarea className="response" defaultValue={this.props.currentNotes} placeholder="Track long term resources and conditions" />
+        return <div className="p-update-notes">
+            <textarea
+                className="p-update-notes__notes"
+                defaultValue={this.props.currentNotes}
+                placeholder="Track long term resources and conditions. *Markdown* is supported." />
             <button type="submit" className="fas fa-check button"></button>
-        </React.Fragment> ;
+        </div>;
     }
 }
 
 export class UpdateNotesPrompt implements Prompt {
-    public InputSelector = ".response";
+    public InputSelector = ".p-update-notes__notes";
     public ComponentName = "reactprompt";
     public component: JSX.Element;
 
@@ -30,7 +33,7 @@ export class UpdateNotesPrompt implements Prompt {
     }
 
     public Resolve = (form: HTMLFormElement) => {
-        const input = form.getElementsByClassName("response")[0] as HTMLTextAreaElement;
+        const input = form.getElementsByClassName("p-update-notes__notes")[0] as HTMLTextAreaElement;
         this.library.UpdatePersistentCharacter(this.persistentCharacter.Id, {
             Notes: input.value
         });
