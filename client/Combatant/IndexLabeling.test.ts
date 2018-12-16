62b27f1171a3850e48562cfacc53b11e1b2dfddc
Evan Bailey <baileyevang@gmail.com>
2018-12-16 12:05:18 -0800

    When a labelled combatant is removed, its index label is not reused

--- client/Combatant/IndexLabeling.test.ts
+++ client/Combatant/IndexLabeling.test.ts
@@ -66,7 +66,7 @@ describe("Index labeling", () => {
 
         const combatant1 = encounter.AddCombatantFromState(combatantStates[0]);
         const combatant2 = encounter.AddCombatantFromState(combatantStates[1]);
-        
+
         const combatant3 = encounter.AddCombatantFromState(combatantStates[0]);
         const combatant4 = encounter.AddCombatantFromState(combatantStates[1]);
 
@@ -75,5 +75,20 @@ describe("Index labeling", () => {
         expect(combatant3.DisplayName()).toEqual("Goblin 3");
         expect(combatant4.DisplayName()).toEqual("Goblin 4");
 
+    });
+
+    test("When a labelled combatant is removed, its index label is not reused.", () => {
+        const statBlock = { ...StatBlock.Default(), Name: "Goblin" };
+        
+        const combatant1 = encounter.AddCombatantFromStatBlock(statBlock);
+        const combatant2 = encounter.AddCombatantFromStatBlock(statBlock);
+        encounter.Combatants.remove(combatant2);
+
+        const combatant3 = encounter.AddCombatantFromStatBlock(statBlock);
+        
+        expect(combatant1.DisplayName()).toEqual("Goblin 1");
+        expect(combatant2.DisplayName()).toEqual("Goblin 2");
+        expect(combatant3.DisplayName()).toEqual("Goblin 3");
+        
     });
 });
