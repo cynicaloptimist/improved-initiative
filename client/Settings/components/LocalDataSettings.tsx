897c14d4624417b59f7b19940816d8619824e837
Evan Bailey <baileyevang@gmail.com>
2019-01-14 21:58:59 -0800

    Move component to file

--- client/Settings/components/LocalDataSettings.tsx
+++ client/Settings/components/LocalDataSettings.tsx
@@ -2,30 +2,7 @@ import { saveAs } from "browser-filesaver";
 import * as React from "react";
 import { Button } from "../../Components/Button";
 import { Store } from "../../Utility/Store";
-
-const FileUploadButton = (props: {
-  handleFile: (file: File) => void;
-  acceptFileType: string;
-}) => {
-  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const file = event.target.files[0];
-    if (file) {
-      props.handleFile(file);
-    }
-  };
-
-  return (
-    <label>
-      <input
-        className="hidden-file-input"
-        type="file"
-        accept={props.acceptFileType}
-        onChange={onChange}
-      />
-      <span className="c-button fas fa-upload" />
-    </label>
-  );
-};
+import { FileUploadButton } from "./FileUploadButton";
 
 export class LocalDataSettings extends React.Component<{}> {
   public render() {
