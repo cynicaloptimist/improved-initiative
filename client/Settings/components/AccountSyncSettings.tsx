1b77a447b695dea6c655e978de48c16a6ef11684
Evan Bailey <baileyevang@gmail.com>
2019-01-17 18:12:29 -0800

    Extract methods for account sync fallback UIs

--- client/Settings/components/AccountSyncSettings.tsx
+++ client/Settings/components/AccountSyncSettings.tsx
@@ -27,37 +27,13 @@ export class AccountSyncSettings extends React.Component<
   }
   public render() {
     if (!env.IsLoggedIn) {
-      return (
-        <React.Fragment>
-          <p>
-            Log in with Patreon to access patron benefits. Account Sync allows
-            you to access your custom statblocks and encounters from anywhere!
-          </p>
-          <a className="login button" href={env.PatreonLoginUrl}>
-            Log In with Patreon
-          </a>
-        </React.Fragment>
-      );
+      return this.loginMessage();
     }
+
     if (!env.HasStorage) {
-      return (
-        <React.Fragment>
-          <p>
-            You're logged in with Patreon, but you have not selected the
-            <a
-              href="https://www.patreon.com/bePatron?c=716070&rid=1322253"
-              target="_blank"
-            >
-              Account Sync
-            </a>
-            reward level.
-          </p>
-          <a className="button logout" href="/logout">
-            Log Out
-          </a>
-        </React.Fragment>
-      );
+      return this.noSyncMessage();
     }
+
     return (
       <React.Fragment>
         <h3>Account Sync</h3>
@@ -76,6 +52,41 @@ export class AccountSyncSettings extends React.Component<
       </React.Fragment>
     );
   }
+
+  private loginMessage() {
+    return (
+      <React.Fragment>
+        <p>
+          Log in with Patreon to access patron benefits. Account Sync allows you
+          to access your custom statblocks and encounters from anywhere!
+        </p>
+        <a className="login button" href={env.PatreonLoginUrl}>
+          Log In with Patreon
+        </a>
+      </React.Fragment>
+    );
+  }
+
+  private noSyncMessage() {
+    return (
+      <React.Fragment>
+        <p>
+          You're logged in with Patreon, but you have not selected the
+          <a
+            href="https://www.patreon.com/bePatron?c=716070&rid=1322253"
+            target="_blank"
+          >
+            Account Sync
+          </a>
+          reward level.
+        </p>
+        <a className="button logout" href="/logout">
+          Log Out
+        </a>
+      </React.Fragment>
+    );
+  }
+
   private syncAll = () => {
     this.setState({ syncError: "" });
     let blob = Store.ExportAll();
