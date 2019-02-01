89ac46bb2968ae99b232300abdd4f6d539d1c40e
Evan Bailey <baileyevang@gmail.com>
2019-01-31 22:33:13 -0800

    Fully specify type

--- common/PlayerViewState.ts
+++ common/PlayerViewState.ts
@@ -1,6 +1,8 @@
+import { EncounterState } from "./EncounterState";
+import { PlayerViewCombatantState } from "./PlayerViewCombatantState";
 import { PlayerViewSettings } from "./PlayerViewSettings";
 
 export interface PlayerViewState {
-  encounterState: any | null;
+  encounterState: EncounterState<PlayerViewCombatantState> | null;
   settings: PlayerViewSettings | null;
 }
