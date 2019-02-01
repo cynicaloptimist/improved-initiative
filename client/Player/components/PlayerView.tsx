b56cd0505a8ff4c941f54f4f034268a0fa0b9295
Evan Bailey <baileyevang@gmail.com>
2019-01-31 22:33:28 -0800

    Show full combatant rows in player view

--- client/Player/components/PlayerView.tsx
+++ client/Player/components/PlayerView.tsx
@@ -1,11 +1,18 @@
 import * as React from "react";
 
 import { PlayerViewState } from "../../../common/PlayerViewState";
+import { PlayerViewCombatant } from "./PlayerViewCombatant";
 
 export class PlayerView extends React.Component<PlayerViewState> {
   public render() {
     return this.props.encounterState.Combatants.map(combatant => (
-      <span key={combatant.Id}>{combatant.Name}</span>
+      <PlayerViewCombatant
+        combatant={combatant}
+        areSuggestionsAllowed={this.props.settings.AllowPlayerSuggestions}
+        isPortraitVisible={this.props.settings.DisplayPortraits}
+        isActive={this.props.encounterState.ActiveCombatantId == combatant.Id}
+        key={combatant.Id}
+      />
     ));
   }
 }
---
+++ client/Player/components/PlayerViewCombatant.tsx
@@ -0,0 +1,64 @@
+import * as React from "react";
+
+import { PlayerViewCombatantState } from "../../../common/PlayerViewCombatantState";
+
+interface PlayerViewCombatantProps {
+  combatant: PlayerViewCombatantState;
+  isActive: boolean;
+  isPortraitVisible: boolean;
+  areSuggestionsAllowed: boolean;
+}
+
+export class PlayerViewCombatant extends React.Component<
+  PlayerViewCombatantProps
+> {
+  public render() {
+    const classNames = ["combatant"];
+    if (this.props.isActive) {
+      classNames.push("active");
+    }
+    if (this.props.combatant.IsPlayerCharacter) {
+      classNames.push("playercharacter");
+    }
+    return (
+      <li className={classNames.join(" ")}>
+        <div className="combatant__initiative">
+          {this.props.combatant.Initiative}
+        </div>
+        {this.props.isPortraitVisible && (
+          <div className="combatant__portrait">
+            {this.props.combatant.ImageURL && (
+              <img
+                src={this.props.combatant.ImageURL}
+                onClick={() => alert("TODO: Splash Portrait")}
+              />
+            )}
+          </div>
+        )}
+        <div className="combatant__name" title={this.props.combatant.Name}>
+          {this.props.combatant.Name}
+        </div>
+        <div
+          className={
+            "combatant__hp" +
+            (!this.props.areSuggestionsAllowed && " disable-hover")
+          }
+        >
+          <span
+            className="current-hp"
+            style={{ color: this.props.combatant.HPColor }}
+            onClick={() => alert("TODO: Show Suggestion")}
+            dangerouslySetInnerHTML={{ __html: this.props.combatant.HPDisplay }}
+          />
+        </div>
+        <div className="combatant__tags">
+          {this.props.combatant.Tags.map((tag, index) => (
+            <div className="tag" key={tag.Text + index}>
+              {tag.Text}
+            </div>
+          ))}
+        </div>
+      </li>
+    );
+  }
+}
