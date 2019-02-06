3185c4e04678769f8d9056503c9e28b58a3c3c27
Evan Bailey <baileyevang@gmail.com>
2019-02-05 23:08:59 -0800

    Render custom colors and CSS

--- client/Player/components/PlayerView.tsx
+++ client/Player/components/PlayerView.tsx
@@ -1,17 +1,26 @@
 import * as React from "react";
 
 import { PlayerViewState } from "../../../common/PlayerViewState";
+import { CSSFrom } from "../../CSSFrom";
 import { PlayerViewCombatant } from "./PlayerViewCombatant";
 
 export class PlayerView extends React.Component<PlayerViewState> {
   public render() {
     return (
       <div className="c-player-view">
+        <style
+          dangerouslySetInnerHTML={{
+            __html: CSSFrom(this.props.settings.CustomStyles)
+          }}
+        />
+        <style
+          dangerouslySetInnerHTML={{ __html: this.props.settings.CustomCSS }}
+        />
         <div className="combatant--header">
           <div className="combatant__initiative">
             <span className="fas fa-forward" />
           </div>
-          <div className="combatant__portrait" data-bind="visible: hasImages" />
+          {this.hasImages() && <div className="combatant__portrait" />}
           <div className="combatant__name">Combatant</div>
           <div className="combatant__hp">
             <span className="fas fa-heart" />
@@ -36,4 +45,11 @@ export class PlayerView extends React.Component<PlayerViewState> {
       </div>
     );
   }
+
+  private hasImages = () => {
+    return (
+      this.props.settings.DisplayPortraits &&
+      this.props.encounterState.Combatants.some(c => c.ImageURL.length > 0)
+    );
+  };
 }
