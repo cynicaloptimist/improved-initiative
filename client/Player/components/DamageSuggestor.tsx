43fdb70c14c36340ee49650f12f0f0f8e1fb41fa
Evan Bailey <baileyevang@gmail.com>
2019-02-10 00:02:13 -0800

    Tweak layout

--- client/Player/components/DamageSuggestor.tsx
+++ client/Player/components/DamageSuggestor.tsx
@@ -8,10 +8,12 @@ export class DamageSuggestor extends React.Component<DamageSuggestorProps> {
       <React.Fragment>
         <div className="modal-blur" onClick={this.props.onClose} />
         <div className="damage-suggestion">
-          Suggest damage/healing for {this.props.combatant.Name}:
-          <input type="number" name="suggestedDamage" />
-          <Button fontAwesomeIcon="check" onClick={() => alert("BARF")} />
-          <div className="tip">
+          Apply damage to {this.props.combatant.Name}
+          <div className="damage-suggestion__input">
+            <input type="number" name="suggestedDamage" />
+            <Button fontAwesomeIcon="check" onClick={() => alert("BARF")} />
+          </div>
+          <div className="damage-suggestion__tip">
             Use a positive value to damage and a negative value to heal
           </div>
         </div>
