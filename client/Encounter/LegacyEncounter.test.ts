19a52b8c0aed7eded5841e0913b9e7857da01ffa
Evan Bailey <baileyevang@gmail.com>
2019-02-22 21:55:40 -0800

    extract method

--- client/Encounter/LegacyEncounter.test.ts
+++ client/Encounter/LegacyEncounter.test.ts
@@ -1,33 +1,35 @@
 import { UpdateLegacySavedEncounter } from "./UpdateLegacySavedEncounter";
 
+function makev0_1StatBlock() {
+  return {
+    Name: "v0.1 Creature",
+    Type: "",
+    HP: { Value: 1 },
+    AC: { Value: 10 },
+    Speed: ["Walk 30"],
+    Abilities: { Str: 10, Dex: 10, Con: 10, Cha: 10, Int: 10, Wis: 10 },
+    DamageVulnerabilities: [],
+    DamageResistances: [],
+    DamageImmunities: [],
+    ConditionImmunities: [],
+    Saves: [],
+    Skills: [],
+    Senses: [],
+    Languages: [],
+    Challenge: "",
+    Traits: [],
+    Actions: [],
+    LegendaryActions: []
+  };
+}
+
 describe("Legacy Encounter", () => {
   test("Loads a v0.1 encounter", () => {
-    const legacyStatBlock = {
-      Name: "v0.1 Creature",
-      Type: "",
-      HP: { Value: 1 },
-      AC: { Value: 10 },
-      Speed: ["Walk 30"],
-      Abilities: { Str: 10, Dex: 10, Con: 10, Cha: 10, Int: 10, Wis: 10 },
-      DamageVulnerabilities: [],
-      DamageResistances: [],
-      DamageImmunities: [],
-      ConditionImmunities: [],
-      Saves: [],
-      Skills: [],
-      Senses: [],
-      Languages: [],
-      Challenge: "",
-      Traits: [],
-      Actions: [],
-      LegendaryActions: []
-    };
-
     const v1Encounter = {
       Name: "V0.1 Encounter",
       Creatures: [
         {
-          Statblock: legacyStatBlock,
+          Statblock: makev0_1StatBlock(),
           CurrentHP: 1,
           TemporaryHP: 0,
           Initiative: 10,
