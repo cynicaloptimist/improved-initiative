8348b6a01768694575fdaa3df2a2d7de9fdd1cd7
Evan Bailey <baileyevang@gmail.com>
2019-02-26 21:30:27 -0800

    Add AutoRerollInitiativeOption tests

---
+++ client/Encounter/AutoRerollInitiativeOption.test.ts
@@ -0,0 +1,51 @@
+import { StatBlock } from "../../common/StatBlock";
+import {
+  AutoRerollInitiativeOption,
+  CurrentSettings,
+  InitializeSettings
+} from "../Settings/Settings";
+import { buildEncounter } from "../test/buildEncounter";
+
+describe("AutoRerollInitiativeOption", () => {
+  beforeEach(() => {
+    InitializeSettings();
+  });
+
+  const runEncounter = promptReroll => {
+    const encounter = buildEncounter();
+    const combatant1 = encounter.AddCombatantFromStatBlock(StatBlock.Default());
+    const combatant2 = encounter.AddCombatantFromStatBlock(StatBlock.Default());
+    combatant1.Initiative(10);
+    combatant2.Initiative(5);
+    encounter.StartEncounter();
+    encounter.NextTurn(promptReroll);
+    encounter.NextTurn(promptReroll);
+    return encounter;
+  };
+
+  test("Default", () => {
+    const promptReroll = jest.fn();
+    runEncounter(promptReroll);
+    expect(promptReroll).not.toBeCalled();
+  });
+
+  test("Prompt", () => {
+    const settings = CurrentSettings();
+    settings.Rules.AutoRerollInitiative = AutoRerollInitiativeOption.Prompt;
+    CurrentSettings(settings);
+    const promptReroll = jest.fn();
+    runEncounter(promptReroll);
+    expect(promptReroll).toBeCalled();
+  });
+
+  test("Automatic", () => {
+    const settings = CurrentSettings();
+    settings.Rules.AutoRerollInitiative = AutoRerollInitiativeOption.Automatic;
+    CurrentSettings(settings);
+    const promptReroll = jest.fn();
+    Math.random = () => 1;
+    const encounter = runEncounter(promptReroll);
+    expect(encounter.Combatants()[0].Initiative()).toEqual(20);
+    expect(encounter.Combatants()[1].Initiative()).toEqual(20);
+  });
+});
