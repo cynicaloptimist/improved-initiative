e8b4c24297ea3992453e78649e56bc7ea5cee854
Evan Bailey <baileyevang@gmail.com>
2019-02-26 21:35:04 -0800

    Add tests for rolling dice with advantage and disadvantage

--- client/Rules/Rules.test.ts
+++ client/Rules/Rules.test.ts
@@ -18,4 +18,31 @@ describe("DefaultRules", () => {
   test("Ability Score 18", () => {
     expect(rules.GetModifierFromScore(18)).toBe(4);
   });
+
+  test("Roll with advantage", () => {
+    Math.random = jest
+      .fn()
+      .mockReturnValueOnce(5 / 20)
+      .mockReturnValueOnce(15 / 20);
+    const roll = rules.AbilityCheck(0, "advantage");
+    expect(roll).toEqual(15);
+  });
+
+  test("Roll with disadvantage", () => {
+    Math.random = jest
+      .fn()
+      .mockReturnValueOnce(5 / 20)
+      .mockReturnValueOnce(15 / 20);
+    const roll = rules.AbilityCheck(0, "disadvantage");
+    expect(roll).toEqual(5);
+  });
+
+  test("Roll with take ten", () => {
+    Math.random = jest
+      .fn()
+      .mockReturnValueOnce(5 / 20)
+      .mockReturnValueOnce(15 / 20);
+    const roll = rules.AbilityCheck(0, "take-ten");
+    expect(roll).toEqual(10);
+  });
 });
