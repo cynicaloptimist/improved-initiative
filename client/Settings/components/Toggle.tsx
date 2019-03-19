47b4bf280f2e00c4bda1b6a1fdb682fc3cdaff7d
Evan Bailey <baileyevang@gmail.com>
2019-03-18 21:32:05 -0700

    extract ToggleButton component

--- client/Settings/components/Toggle.tsx
+++ client/Settings/components/Toggle.tsx
@@ -2,6 +2,39 @@ import { Field, FieldProps } from "formik";
 import * as React from "react";
 import { probablyUniqueString } from "../../../common/Toolbox";
 
+interface ToggleButtonProps {
+  fieldName: string;
+  id?: string;
+}
+
+export class ToggleButton extends React.Component<ToggleButtonProps> {
+  public render() {
+    return (
+      <Field name={this.props.fieldName}>
+        {(fieldProps: FieldProps) => {
+          const stateString = fieldProps.field.value ? "on" : "off";
+          return (
+            <button
+              id={this.props.id}
+              type="button"
+              className="c-toggle"
+              onClick={() => this.toggle(fieldProps)}
+            >
+              <span className={"c-toggle__icon fas fa-toggle-" + stateString} />
+            </button>
+          );
+        }}
+      </Field>
+    );
+  }
+
+  private toggle = (fieldProps: FieldProps) =>
+    fieldProps.form.setFieldValue(
+      this.props.fieldName,
+      !fieldProps.field.value
+    );
+}
+
 interface ToggleProps {
   fieldName: string;
 }
@@ -19,30 +52,8 @@ export class Toggle extends React.Component<ToggleProps> {
         <label className="c-toggle__label" htmlFor={this.id}>
           {this.props.children}
         </label>
-        <Field name={this.props.fieldName}>
-          {(fieldProps: FieldProps) => {
-            const stateString = fieldProps.field.value ? "on" : "off";
-            return (
-              <button
-                id={this.id}
-                type="button"
-                className="c-toggle"
-                onClick={() => this.toggle(fieldProps)}
-              >
-                <span
-                  className={"c-toggle__icon fas fa-toggle-" + stateString}
-                />
-              </button>
-            );
-          }}
-        </Field>
+        <ToggleButton fieldName={this.props.fieldName} id={this.id} />
       </div>
     );
   }
-
-  private toggle = (fieldProps: FieldProps) =>
-    fieldProps.form.setFieldValue(
-      this.props.fieldName,
-      !fieldProps.field.value
-    );
 }
