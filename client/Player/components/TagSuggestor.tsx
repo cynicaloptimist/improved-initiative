1db891c7a6af465b8dc0658da9dafa4e3f3deba2
Evan Bailey <baileyevang@gmail.com>
2019-04-01 21:39:58 -0700

    Call onApply even if text is empty

--- client/Player/components/TagSuggestor.tsx
+++ client/Player/components/TagSuggestor.tsx
@@ -23,37 +23,36 @@ export class TagSuggestor extends React.Component<TagSuggestorProps> {
     const form = e.target;
 
     const text: string = form["tag-text"].value;
-    if (text.length) {
-      if (form["tag-duration"] && form["tag-timing-id"]) {
-        const duration = parseInt(form["tag-duration"].value);
-        const timing =
-          form["tag-timing"].value == "end" ? EndOfTurn : StartOfTurn;
-        const timingId = form["tag-timing-id"].value;
 
-        // If tag is set to expire at the end of the current combatant's turn in one round,
-        // we need to add a grace round so it doesn't end immediately at the end of this turn.
-        const timingKeyedCombatantIsActive =
-          timingId == this.props.activeCombatantId;
-        const durationGraceRound =
-          timingKeyedCombatantIsActive && timing == EndOfTurn ? 1 : 0;
+    if (form["tag-duration"] && form["tag-timing-id"]) {
+      const duration = parseInt(form["tag-duration"].value);
+      const timing =
+        form["tag-timing"].value == "end" ? EndOfTurn : StartOfTurn;
+      const timingId = form["tag-timing-id"].value;
 
-        const tagState: TagState = {
-          Text: text,
-          DurationCombatantId: timingId,
-          DurationRemaining: duration + durationGraceRound,
-          DurationTiming: timing
-        };
+      // If tag is set to expire at the end of the current combatant's turn in one round,
+      // we need to add a grace round so it doesn't end immediately at the end of this turn.
+      const timingKeyedCombatantIsActive =
+        timingId == this.props.activeCombatantId;
+      const durationGraceRound =
+        timingKeyedCombatantIsActive && timing == EndOfTurn ? 1 : 0;
 
-        this.props.onApply(tagState);
-      } else {
-        const tagState: TagState = {
-          Text: text,
-          DurationCombatantId: "",
-          DurationRemaining: 0,
-          DurationTiming: null
-        };
-        this.props.onApply(tagState);
-      }
+      const tagState: TagState = {
+        Text: text,
+        DurationCombatantId: timingId,
+        DurationRemaining: duration + durationGraceRound,
+        DurationTiming: timing
+      };
+
+      this.props.onApply(tagState);
+    } else {
+      const tagState: TagState = {
+        Text: text,
+        DurationCombatantId: "",
+        DurationRemaining: 0,
+        DurationTiming: null
+      };
+      this.props.onApply(tagState);
     }
   };
 }
