0e9e8627898762f2202e0e166c8f04040381285c
Evan Bailey <baileyevang@gmail.com>
2019-04-06 15:13:32 -0700

    run prettier on missed files

--- common/Listable.ts
+++ common/Listable.ts
@@ -1,14 +1,14 @@
 export interface Listable {
-    Id: string;
-    Version: string;
-    Name: string;
-    Path: string;
+  Id: string;
+  Version: string;
+  Name: string;
+  Path: string;
 }
 
 export interface ServerListing {
-    Id: string;
-    Link: string;
-    Name: string;
-    SearchHint: string;
-    Path: string;
+  Id: string;
+  Link: string;
+  Name: string;
+  SearchHint: string;
+  Path: string;
 }
