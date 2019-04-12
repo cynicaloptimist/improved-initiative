c74428663ec99f19b921b12b29b0f13adcf1a0d0
Evan Bailey <baileyevang@gmail.com>
2019-04-11 22:45:10 -0700

    test consistency between SelectedCombatants and OrderedCombatants

--- client/Commands/CombatantCommander.test.ts
+++ client/Commands/CombatantCommander.test.ts
@@ -50,4 +50,27 @@ describe("CombatantCommander", () => {
 
     expect(playerView.Combatants[0].AC).toBe(10);
   });
+
+  test("Should maintain selection when initiative order changes", () => {
+    const combatant1 = encounter.AddCombatantFromStatBlock(StatBlock.Default());
+    const combatant2 = encounter.AddCombatantFromStatBlock(StatBlock.Default());
+
+    combatant1.Initiative(15);
+    combatant2.Initiative(10);
+    encounter.SortByInitiative(false);
+
+    expect(trackerViewModel.OrderedCombatants()[0].Combatant).toBe(combatant1);
+
+    const combatantViewModel = trackerViewModel.OrderedCombatants()[0];
+    expect(combatantViewModel.Combatant).toBe(combatant1);
+
+    combatantCommander.Select(combatantViewModel);
+    combatantViewModel.ApplyInitiative("5");
+
+    expect(trackerViewModel.OrderedCombatants()[1].Combatant).toBe(combatant1);
+
+    expect(combatantCommander.SelectedCombatants()[0]).toBe(
+      trackerViewModel.OrderedCombatants()[1]
+    );
+  });
 });
