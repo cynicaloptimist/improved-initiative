e83f76f1a8b3df38c65dc56d61b68bc11f67e396
Evan Bailey <baileyevang@gmail.com>
2019-05-18 22:38:56 -0700

    move class to file

---
+++ server/InMemoryPlayerViewManager.ts
@@ -0,0 +1,45 @@
+import { PlayerViewState } from "../common/PlayerViewState";
+import { probablyUniqueString } from "../common/Toolbox";
+import { PlayerViewManager } from "./playerviewmanager";
+
+export class InMemoryPlayerViewManager implements PlayerViewManager {
+  private playerViews: {
+    [encounterId: string]: PlayerViewState;
+  } = {};
+
+  constructor() {}
+
+  public Get(id: string) {
+    return this.playerViews[id];
+  }
+
+  public UpdateEncounter(id: string, newState: any) {
+    this.playerViews[id].encounterState = newState;
+  }
+
+  public UpdateSettings(id: string, newSettings: any) {
+    this.playerViews[id].settings = newSettings;
+  }
+
+  public InitializeNew() {
+    const encounterId = probablyUniqueString();
+    this.playerViews[encounterId] = {
+      encounterState: null,
+      settings: null
+    };
+    return encounterId;
+  }
+
+  public EnsureInitialized(id: string) {
+    if (this.playerViews[id] === undefined) {
+      this.playerViews[id] = {
+        encounterState: null,
+        settings: null
+      };
+    }
+  }
+
+  public Destroy(id: string) {
+    delete this.playerViews[id];
+  }
+}
--- server/playerviewmanager.ts
+++ server/playerviewmanager.ts
@@ -1,5 +1,4 @@
 import { PlayerViewState } from "../common/PlayerViewState";
-import { probablyUniqueString } from "../common/Toolbox";
 
 export interface PlayerViewManager {
   Get(id: string): PlayerViewState;
@@ -13,44 +12,4 @@ export interface PlayerViewManager {
   EnsureInitialized(id: string): void;
 
   Destroy(id: string): void;
-}
-
-export class InMemoryPlayerViewManager implements PlayerViewManager {
-  private playerViews: { [encounterId: string]: PlayerViewState } = {};
-
-  constructor() {}
-
-  public Get(id: string) {
-    return this.playerViews[id];
-  }
-
-  public UpdateEncounter(id: string, newState: any) {
-    this.playerViews[id].encounterState = newState;
-  }
-
-  public UpdateSettings(id: string, newSettings: any) {
-    this.playerViews[id].settings = newSettings;
-  }
-
-  public InitializeNew() {
-    const encounterId = probablyUniqueString();
-    this.playerViews[encounterId] = {
-      encounterState: null,
-      settings: null
-    };
-    return encounterId;
-  }
-
-  public EnsureInitialized(id: string) {
-    if (this.playerViews[id] === undefined) {
-      this.playerViews[id] = {
-        encounterState: null,
-        settings: null
-      };
-    }
-  }
-
-  public Destroy(id: string) {
-    delete this.playerViews[id];
-  }
 }
