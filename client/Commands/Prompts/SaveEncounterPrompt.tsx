e77995899d861652ecefc7e5ab066527720cd456
Evan Bailey <baileyevang@gmail.com>
2019-06-01 22:20:22 -0700

    Extract component

--- client/Commands/Prompts/SaveEncounterPrompt.tsx
+++ client/Commands/Prompts/SaveEncounterPrompt.tsx
@@ -4,11 +4,38 @@ import { probablyUniqueString } from "../../../common/Toolbox";
 import { SubmitButton } from "../../Components/Button";
 import { Encounter } from "../../Encounter/Encounter";
 import { EncounterLibrary } from "../../Library/EncounterLibrary";
-import { AutoHideField } from "../../StatBlockEditor/components/AutoHideField";
 import { AutocompleteTextInput } from "../../StatBlockEditor/components/AutocompleteTextInput";
 import { Metrics } from "../../Utility/Metrics";
 import { EventLog } from "../../Widgets/EventLog";
 import { PromptProps } from "./components/PendingPrompts";
+
+interface SaveEncounterPromptComponentProps {
+  autocompletePaths: string[];
+}
+interface SaveEncounterPromptComponentState {}
+class SaveEncounterPromptComponent extends React.Component<
+  SaveEncounterPromptComponentProps,
+  SaveEncounterPromptComponentState
+> {
+  public render() {
+    const fieldLabelId = probablyUniqueString();
+    return (
+      <div className="p-save-encounter">
+        <label htmlFor={fieldLabelId}>{"Save Encounter As: "}</label>
+        <label className="autohide-field__label label" htmlFor="Path">
+          {"Folder: "}
+        </label>
+        <AutocompleteTextInput
+          fieldName="Path"
+          options={this.props.autocompletePaths}
+          autoFocus
+        />
+        <Field id={fieldLabelId} name="Name" className="response" type="text" />
+        <SubmitButton />
+      </div>
+    );
+  }
+}
 
 interface SaveEncounterModel {
   Name: string;
@@ -21,7 +48,6 @@ export function SaveEncounterPrompt(
   logEvent: typeof EventLog.prototype.AddEvent,
   autocompletePaths: string[]
 ): PromptProps<SaveEncounterModel> {
-  const fieldLabelId = probablyUniqueString();
   return {
     initialValues: {
       Name: "",
@@ -29,25 +55,7 @@ export function SaveEncounterPrompt(
     },
     autoFocusSelector: ".response",
     children: (
-      <div className="p-save-encounter">
-        <label htmlFor={fieldLabelId}>{"Save Encounter As: "}</label>
-        <AutoHideField
-          faClass="folder"
-          fieldName="Path"
-          tooltip="Save in folder"
-        >
-          <label className="autohide-field__label label" htmlFor="Path">
-            {"Folder: "}
-          </label>
-          <AutocompleteTextInput
-            fieldName="Path"
-            options={autocompletePaths}
-            autoFocus
-          />
-        </AutoHideField>
-        <Field id={fieldLabelId} name="Name" className="response" type="text" />
-        <SubmitButton />
-      </div>
+      <SaveEncounterPromptComponent autocompletePaths={autocompletePaths} />
     ),
     onSubmit: (model: SaveEncounterModel) => {
       if (!model.Name) return false;
