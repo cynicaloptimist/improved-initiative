c28c969afa324e95fc7dee18c2ca6319ee8aa714
Evan Bailey <baileyevang@gmail.com>
2019-09-02 21:07:18 -0700

    Change class with static members to namespace

--- client/Utility/Store.ts
+++ client/Utility/Store.ts
@@ -3,53 +3,54 @@ import { Spell } from "../../common/Spell";
 import { StatBlock } from "../../common/StatBlock";
 import { DnDAppFilesImporter } from "../Importers/DnDAppFilesImporter";
 
-export class Store {
-  private static _prefix = "ImprovedInitiative";
+const _prefix = "ImprovedInitiative";
+export namespace Store {
+  export const PersistentCharacters = "PersistentCharacters";
+  export const PlayerCharacters = "PlayerCharacters";
+  export const StatBlocks = "Creatures";
+  export const Spells = "Spells";
+  export const SavedEncounters = "SavedEncounters";
+  export const AutoSavedEncounters = "AutoSavedEncounters";
+  export const User = "User";
 
-  public static readonly PersistentCharacters = "PersistentCharacters";
-  public static readonly PlayerCharacters = "PlayerCharacters";
-  public static readonly StatBlocks = "Creatures";
-  public static readonly Spells = "Spells";
-  public static readonly SavedEncounters = "SavedEncounters";
-  public static readonly AutoSavedEncounters = "AutoSavedEncounters";
-  public static readonly User = "User";
-
-  public static readonly DefaultSavedEncounterId = "default";
+  export const DefaultSavedEncounterId = "default";
 
   //Legacy
-  public static readonly KeyBindings = "KeyBindings";
-  public static readonly ActionBar = "ActionBar";
+  export const KeyBindings = "KeyBindings";
+  export const ActionBar = "ActionBar";
 
-  public static List(listName: string): string[] {
-    let listKey = `${Store._prefix}.${listName}`;
-    let list = Store.load(listKey);
+  export function List(listName: string): string[] {
+    let listKey = `${_prefix}.${listName}`;
+    let list = load(listKey);
     if (list && list.constructor === Array) {
       return list;
     }
-    Store.save(listKey, []);
+    save(listKey, []);
     return [];
   }
 
-  public static Save<T>(listName: string, key: string, value: T) {
+  export function Save<T>(listName: string, key: string, value: T) {
     if (typeof key !== "string") {
       throw `Can't save to non-string key ${key}`;
     }
-    let listKey = `${Store._prefix}.${listName}`;
-    let fullKey = `${Store._prefix}.${listName}.${key}`;
+    let listKey = `${_prefix}.${listName}`;
+    let fullKey = `${_prefix}.${listName}.${key}`;
     let list = Store.List(listName);
     if (list.indexOf(key) == -1) {
       list.push(key);
-      Store.save(listKey, list);
+      save(listKey, list);
     }
-    Store.save(fullKey, value);
+    save(fullKey, value);
   }
 
-  public static Load<T>(listName: string, key: string): T {
-    let fullKey = `${Store._prefix}.${listName}.${key}`;
-    return Store.load(fullKey);
+  export function Load<T>(listName: string, key: string): T {
+    let fullKey = `${_prefix}.${listName}.${key}`;
+    return load(fullKey);
   }
 
-  public static LoadAllAndUpdateIds<T extends Listable>(listName: string): T[] {
+  export function LoadAllAndUpdateIds<T extends Listable>(
+    listName: string
+  ): T[] {
     return Store.List(listName)
       .map(key => {
         const item = Store.Load<T>(listName, key);
@@ -61,30 +62,30 @@ export class Store {
       .filter(value => !!value);
   }
 
-  public static Delete(listName: string, key: string) {
-    let listKey = `${Store._prefix}.${listName}`;
-    let fullKey = `${Store._prefix}.${listName}.${key}`;
+  export function Delete(listName: string, key: string) {
+    let listKey = `${_prefix}.${listName}`;
+    let fullKey = `${_prefix}.${listName}.${key}`;
     let list = Store.List(listName);
     let keyIndex = list.indexOf(key);
     if (keyIndex != -1) {
       list.splice(keyIndex, 1);
-      Store.save(listKey, list);
+      save(listKey, list);
     }
     localStorage.removeItem(fullKey);
   }
 
-  public static DeleteAll() {
+  export function DeleteAll() {
     localStorage.clear();
     location.reload();
   }
 
-  public static ExportAll() {
+  export function ExportAll() {
     return new Blob([JSON.stringify(localStorage, null, 2)], {
       type: "application/json"
     });
   }
 
-  public static ImportAll(file: File) {
+  export function ImportAll(file: File) {
     let reader = new FileReader();
     reader.onload = (event: any) => {
       let json = event.target.result;
@@ -98,7 +99,7 @@ export class Store {
 
       this.importList(Store.StatBlocks, importedStorage);
       this.importList(Store.PersistentCharacters, importedStorage);
-      this.importList(Store.SavedEncounters, importedStorage);
+      this.importList(SavedEncounters, importedStorage);
       this.importList(Store.Spells, importedStorage);
 
       location.reload();
@@ -106,8 +107,8 @@ export class Store {
     reader.readAsText(file);
   }
 
-  private static importList(listName: string, importSource: any) {
-    const listKey = `${Store._prefix}.${listName}`;
+  function importList(listName: string, importSource: any) {
+    const listKey = `${_prefix}.${listName}`;
     const listingsJSON = importSource[listKey];
     if (!listingsJSON) {
       console.warn(`Couldn't import ${listName} from JSON`);
@@ -115,18 +116,18 @@ export class Store {
     }
     const listings: string[] = JSON.parse(listingsJSON);
     for (const key of listings) {
-      const fullKey = `${Store._prefix}.${listName}.${key}`;
+      const fullKey = `${_prefix}.${listName}.${key}`;
       const listingJSON = importSource[fullKey];
       if (!listingJSON) {
         console.warn(`Couldn't import ${fullKey} from JSON`);
       } else {
         const listing = JSON.parse(listingJSON);
-        Store.Save(listName, key, listing);
+        Save(listName, key, listing);
       }
     }
   }
 
-  public static ImportAllAndReplace(file: File) {
+  export function ImportAllAndReplace(file: File) {
     let reader = new FileReader();
     reader.onload = (event: any) => {
       let json = event.target.result;
@@ -154,7 +155,7 @@ export class Store {
     reader.readAsText(file);
   }
 
-  public static ImportFromDnDAppFile(file: File) {
+  export function ImportFromDnDAppFile(file: File) {
     const statBlocksCallback = (statBlocks: StatBlock[]) => {
       statBlocks.forEach(c => {
         this.Save(Store.StatBlocks, c.Id, c);
@@ -176,7 +177,7 @@ export class Store {
     }
   }
 
-  public static ExportStatBlocks() {
+  export function ExportStatBlocks() {
     let statBlocks = this.List(Store.StatBlocks).map(id =>
       Store.Load(Store.StatBlocks, id)
     );
@@ -185,9 +186,8 @@ export class Store {
     });
   }
 
-  private static save = (key, value) =>
-    localStorage.setItem(key, JSON.stringify(value));
-  private static load = key => {
+  const save = (key, value) => localStorage.setItem(key, JSON.stringify(value));
+  const load = key => {
     let value = localStorage.getItem(key);
     if (value === "undefined") {
       return null;
