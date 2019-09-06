fae1406b8536798bc5ee83122e565eaca25ef1cb
Evan Bailey <baileyevang@gmail.com>
2019-09-05 22:24:50 -0700

    Also test delete

--- client/Utility/Store.test.ts
+++ client/Utility/Store.test.ts
@@ -1,11 +1,14 @@
 import { LegacySynchronousLocalStore } from "./LegacySynchronousLocalStore";
 
 describe("LegacySynchronousLocalStore", () => {
-  it("Saves, Lists, and Loads", () => {
+  it("Saves, Lists, Loads, and Deletes", () => {
     LegacySynchronousLocalStore.Save("TestList", "TestKey", "TestValue");
     const list = LegacySynchronousLocalStore.List("TestList");
     expect(list).toEqual(["TestKey"]);
     const item = LegacySynchronousLocalStore.Load("TestList", "TestKey");
     expect(item).toEqual("TestValue");
+    LegacySynchronousLocalStore.Delete("TestList", "TestKey");
+    const emptyList = LegacySynchronousLocalStore.List("TestList");
+    expect(emptyList).toEqual([]);
   });
 });
