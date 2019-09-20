21a7da92b848f5dc52bc4a49795facd0d055cce3
Evan Bailey <baileyevang@gmail.com>
2019-09-19 22:49:25 -0700

    Open metrics dbconnection on app load

--- server/metrics.ts
+++ server/metrics.ts
@@ -1,14 +1,19 @@
 import express = require("express");
 import { MongoClient } from "mongodb";
 
-const metricsDbConnectionString = process.env.METRICS_DB_CONNECTION_STRING;
+let dbClient: MongoClient | null = null;
+if (process.env.METRICS_DB_CONNECTION_STRING != undefined) {
+  new MongoClient(process.env.METRICS_DB_CONNECTION_STRING)
+    .connect()
+    .then(client => (dbClient = client));
+}
 
 type Req = Express.Request & express.Request;
 type Res = Express.Response & express.Response;
 
 export function configureMetricsRoutes(app: express.Application) {
   app.post("/recordEvent/:eventName", async (req: Req, res: Res) => {
-    if (metricsDbConnectionString == undefined) {
+    if (dbClient == null) {
       return res.status(204).send("No metrics pipeline configured.");
     }
 
@@ -23,30 +28,32 @@ export function configureMetricsRoutes(app: express.Application) {
     eventData.userId = session.userId || null;
     eventData.ipAddress = req.ip;
 
-    const client = await new MongoClient(metricsDbConnectionString).connect();
-    const events = client.db().collection("events");
-    await events.insertOne({
-      eventName,
-      eventData
-    });
+    await dbClient
+      .db()
+      .collection("events")
+      .insertOne({
+        eventName,
+        eventData
+      });
 
     return res.sendStatus(202);
   });
 
   app.post("/recordAnonymousEvent/:eventName", async (req: Req, res: Res) => {
-    if (metricsDbConnectionString == undefined) {
+    if (dbClient == null) {
       return res.status(204).send("No metrics pipeline configured.");
     }
 
     const eventName = req.params.eventName;
     const eventData = req.body || {};
 
-    const client = await new MongoClient(metricsDbConnectionString).connect();
-    const events = client.db().collection("events");
-    await events.insertOne({
-      eventName,
-      eventData
-    });
+    await dbClient
+      .db()
+      .collection("events")
+      .insertOne({
+        eventName,
+        eventData
+      });
 
     return res.sendStatus(200);
   });
