ef54f972a62ea0f1598b77ab80bc9765b8263747
Evan Bailey <baileyevang@gmail.com>
2019-11-04 23:16:11 -0800

    change param type

--- client/TextEnricher/Counter.tsx
+++ client/TextEnricher/Counter.tsx
@@ -26,11 +26,11 @@ export function CounterOrBracketedText(
     const counterProps = {
       current,
       maximum,
-      onChange: newValue => {
+      onChange: (newValue: number) => {
         const location = element.props.sourcePosition.start.offset;
         const newText =
           text.substr(0, location) +
-          newValue +
+          newValue.toString() +
           text.substr(location + matches[0].length);
         updateText(newText);
       }
@@ -47,11 +47,11 @@ export function CounterOrBracketedText(
 export function BeanCounter(props: {
   current: number;
   maximum: number;
-  onChange: (newValue: string) => void;
+  onChange: (newValue: number) => void;
 }) {
   return (
     <span className="bean-counter">
-      <i className="fa-clickable fa-ban" onClick={_ => props.onChange("0")} />
+      <i className="fa-clickable fa-ban" onClick={_ => props.onChange(0)} />
       {_.range(1, props.maximum + 1).map(index => {
         return (
           <i
@@ -60,7 +60,7 @@ export function BeanCounter(props: {
                 ? "fa-clickable fa-circle"
                 : "far-clickable fa-circle"
             }
-            onClick={_ => props.onChange(index.toString())}
+            onClick={_ => props.onChange(index)}
           />
         );
       })}
@@ -71,16 +71,19 @@ export function BeanCounter(props: {
 export function Counter(props: {
   current: number;
   maximum: number;
-  onChange: (newValue: string) => void;
+  onChange: (newValue: number) => void;
 }) {
   return (
     <input
       className="counter"
       type="number"
-      min="0"
+      min={0}
       max={props.maximum}
       defaultValue={props.current}
-      onBlur={e => props.onChange(e.target.value)}
+      onBlur={e => {
+        const newValue = parseInt(e.target.value);
+        props.onChange(newValue);
+      }}
     />
   );
 }
