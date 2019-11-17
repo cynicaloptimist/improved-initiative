71fac3758e3756159272f527c9628d7a64767d39
Evan Bailey <baileyevang@gmail.com>
2019-11-16 23:38:38 -0800

    Clear async Store before replacing data

--- client/Settings/components/LocalDataSettings.tsx
+++ client/Settings/components/LocalDataSettings.tsx
@@ -66,6 +66,7 @@ export class LocalDataSettings extends React.Component<{}> {
         } and reload?`
       )
     ) {
+      await Store.DeleteAll();
       await Store.ImportAll(file);
       LegacySynchronousLocalStore.ImportAllAndReplace(file);
       location.reload();
