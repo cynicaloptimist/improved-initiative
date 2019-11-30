5a2cbc2bcb3feeab73ecd004ad6259a5f39cbc1f
Evan Bailey <baileyevang@gmail.com>
2019-11-29 22:47:14 -0800

    switch to ReactChild for content

--- client/Components/Info.tsx
+++ client/Components/Info.tsx
@@ -1,9 +1,9 @@
 import Tippy from "@tippy.js/react";
 import * as React from "react";
 
-export function Info(props: { content: string }) {
+export function Info(props: { children: React.ReactChild }) {
   return (
-    <Tippy content={props.content}>
+    <Tippy content={props.children}>
       <i className="c-info fas fa-info-circle" />
     </Tippy>
   );
