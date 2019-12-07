5e001c896a7d5f632b6cd9e9ce7447cad90f64d2
Evan Bailey <baileyevang@gmail.com>
2019-12-06 23:23:02 -0800

    Add info for several commands

--- client/Settings/components/CommandInfo.ts
+++ client/Settings/components/CommandInfo.ts
@@ -1,3 +1,26 @@
 export const CommandInfoById = {
-  "start-encounter": "The first combatant in the initiative order will become active"
-}
+  "start-encounter":
+    "The first combatant in the initiative order will become active.",
+  "clear-encounter":
+    "Remove all combatants, including player characters, and end the encounter.",
+  "clean-encounter": "Remove all creatures and NPCs, and end the encounter.",
+  "quick-add":
+    "Add a simple combatant with just the basics: Name, HP, AC, and Initiative Modifier.",
+  "player-window":
+    "You can share the URL of your player view so players can follow your combat on any other device.",
+  "apply-temporary-hp":
+    "Temporary HP follows the D&D 5e rules. When a combatant is granted THP, it won't stack with existing THP.",
+  "add-tag":
+    "Tags can be used to track conditions like \"Stunned\" or \"Concentrating\". " +
+    "You can set a timer to automatically remove a tag after a set number of rounds have passed.",
+  "update-notes":
+    "You can keep long-form notes to track things like illnesses, inventory, and spell slots. " +
+    "These notes will persist across encounters when they are set on Characters.",
+  "edit-statblock":
+    "If you edit a creature's statblock, it will only affect this combatant. " +
+    "If you edit a character's statblock, it will persist across encounters.",
+  "link-initiative":
+    "Creatures in an initiative group will keep the same initiative count. " +
+    "Group members keep their initiative in sync when you roll or edit " +
+    "initiative, and you can unlink individual members."
+};
