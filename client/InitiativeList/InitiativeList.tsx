1b559646c6496e719b153f94e6cddb94ab02e2da
Evan Bailey <baileyevang@gmail.com>
2019-12-12 21:05:12 -0800

    Move component to file

--- client/InitiativeList/InitiativeList.tsx
+++ client/InitiativeList/InitiativeList.tsx
@@ -1,8 +1,8 @@
-import Tippy from "@tippy.js/react";
 import * as React from "react";
 
 import { CombatantState } from "../../common/CombatantState";
 import { EncounterState } from "../../common/EncounterState";
+import { InitiativeListHeader } from "./InitiativeListHeader";
 
 export function InitiativeList(props: EncounterState<CombatantState>) {
   return (
@@ -14,31 +14,5 @@ export function InitiativeList(props: EncounterState<CombatantState>) {
         />
       </ul>
     </div>
-  );
-}
-
-function InitiativeListHeader(props: { encounterActive: boolean }) {
-  const encounterStateIcon = props.encounterActive ? "fa-play" : "fa-pause";
-  const encounterStateTip = props.encounterActive
-    ? "Encounter Active"
-    : "Encounter Inactive";
-  return (
-    <li className="combatant--header">
-      <span className="combatant__leftsection">
-        <span className="combatant__initiative">
-          <Tippy content={encounterStateTip}>
-            <span className={"fas " + encounterStateIcon}></span>
-          </Tippy>
-        </span>
-        <span className="combatant__name">Name</span>
-        <span className="combatant__hp">
-          <span className="fas fa-heart"></span>
-        </span>
-        <span className="combatant__ac">
-          <span className="fas fa-shield-alt"></span>
-        </span>
-      </span>
-      <span className="combatant__rightsection"></span>
-    </li>
   );
 }
