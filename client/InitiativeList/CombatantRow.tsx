ebb8d80d1182319ee49feb3e11f8fe45751b6034
Evan Bailey <baileyevang@gmail.com>
2019-12-13 23:02:27 -0800

    Add active and selected classNames to combatant

--- client/InitiativeList/CombatantRow.tsx
+++ client/InitiativeList/CombatantRow.tsx
@@ -8,13 +8,21 @@ export function CombatantRow(props: {
   isSelected: boolean;
   showIndexLabel: boolean;
 }) {
+  const classNames = ["combatant"];
+  if (props.isActive) {
+    classNames.push("active");
+  }
+  if (props.isSelected) {
+    classNames.push("selected");
+  }
+
   const displayName = props.combatantState.Alias.length
     ? props.combatantState.Alias
     : props.combatantState.StatBlock.Name +
       " " +
       props.combatantState.IndexLabel;
   return (
-    <span className="combatant">
+    <span className={classNames.join(" ")}>
       <span className="combatant__leftsection">
         <span className="combatant__name" title={displayName}>
           {displayName}
