90532dea3cf990c31aa7d05344c430184077bbc4
Evan Bailey <baileyevang@gmail.com>
2020-03-29 23:15:52 -0700

    wrap standard prompt field with label

--- client/Commands/Prompts/StandardPromptLayout.tsx
+++ client/Commands/Prompts/StandardPromptLayout.tsx
@@ -12,8 +12,10 @@ export function StandardPromptLayout(props: Props) {
   const classNames = ("p-standard-prompt" + " " + props.className).trim();
   return (
     <div className={classNames}>
-      <label className="p-standard-prompt__label">{props.label}</label>
-      <div className="p-standard-prompt__fields">{props.children}</div>
+      <label>
+        <div className="p-standard-prompt__label">{props.label}</div>
+        <div className="p-standard-prompt__fields">{props.children}</div>
+      </label>
       {props.noSubmit || <SubmitButton />}
     </div>
   );
