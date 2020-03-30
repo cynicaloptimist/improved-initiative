a6128603622dc1a77407d9502eb436066295dc5d
Evan Bailey <baileyevang@gmail.com>
2020-03-29 22:57:01 -0700

    standardize applytemporaryhpprompt

--- client/Commands/Prompts/ApplyTemporaryHPPrompt.tsx
+++ client/Commands/Prompts/ApplyTemporaryHPPrompt.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { PromptProps } from "./PendingPrompts";
 import { Field } from "formik";
 import { SubmitButton } from "../../Components/Button";
+import { StandardPromptLayout } from "./StandardPromptLayout";
 
 type ApplyTemporaryHPModel = { hpAmount: number };
 
@@ -14,11 +15,12 @@ export function ApplyTemporaryHPPrompt(
     initialValues: { hpAmount: 0 },
     autoFocusSelector: ".response",
     children: (
-      <div className="p-apply-temporary-hp">
-        <label className="message">Grant temporary hit points to {combatantNames}: </label>
+      <StandardPromptLayout
+        className="p-apply-temporary-hp"
+        label={`Grant temporary hit points to ${combatantNames}: `}
+      >
         <Field name="hpAmount" className="response" type="number" />
-        <SubmitButton />
-      </div>
+      </StandardPromptLayout>
     )
   };
 }
