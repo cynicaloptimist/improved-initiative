54c4dcc2c4404be1bc1b29db5001683e66be1907
Evan Bailey <baileyevang@gmail.com>
2020-03-29 22:25:29 -0700

    remove dead code

--- client/Commands/Prompts/Prompt.ts
+++ client/Commands/Prompts/Prompt.ts
@@ -7,26 +7,3 @@ export interface LegacyPrompt {
 export type LegacyPromptResolver = (responses: {
   [id: string]: string;
 }) => void;
-
-export class DefaultPrompt implements LegacyPrompt {
-  public InputSelector = ".response";
-  public ComponentName = "defaultprompt";
-
-  constructor(
-    public Query: string,
-    private resolve: LegacyPromptResolver = () => {}
-  ) {}
-
-  public Resolve = (form: HTMLFormElement) => {
-    const inputs = $(form).find(this.InputSelector);
-    const inputsById = {};
-    inputs.map((_, element) => {
-      const elementName = $(element).attr("name");
-      if ($(element).prop("checked") && elementName) {
-        inputsById[elementName] = $(element).val();
-      }
-      inputsById[element.id] = $(element).val();
-    });
-    this.resolve(inputsById);
-  };
-}
