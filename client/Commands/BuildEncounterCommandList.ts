26058b189856f270137429fa1e12c0842f250207
Evan Bailey <baileyevang@gmail.com>
2020-04-06 21:58:45 -0700

    Move DefaultKeybindings to file

--- client/Commands/BuildEncounterCommandList.ts
+++ client/Commands/BuildEncounterCommandList.ts
@@ -10,7 +10,6 @@ export const BuildEncounterCommandList = (
       id: "toggle-menu",
       description: "Toggle Wide Menu",
       actionBinding: c.ToggleToolbarWidth,
-      defaultKeyBinding: "alt+m",
       fontAwesomeIcon: "chevron-right",
       lockOnActionBar: true
     }),
@@ -18,14 +17,12 @@ export const BuildEncounterCommandList = (
       id: "start-encounter",
       description: "Start Encounter",
       actionBinding: c.StartEncounter,
-      defaultKeyBinding: "alt+r",
       fontAwesomeIcon: "play"
     }),
     new Command({
       id: "reroll-initiative",
       description: "Reroll Initiative",
       actionBinding: c.RerollInitiative,
-      defaultKeyBinding: "alt+shift+i",
       fontAwesomeIcon: "sync",
       defaultShowOnActionBar: false
     }),
@@ -33,14 +30,12 @@ export const BuildEncounterCommandList = (
       id: "end-encounter",
       description: "End Encounter",
       actionBinding: c.EndEncounter,
-      defaultKeyBinding: "shift+alt+r",
       fontAwesomeIcon: "stop"
     }),
     new Command({
       id: "clear-encounter",
       description: "Clear Encounter",
       actionBinding: c.ClearEncounter,
-      defaultKeyBinding: "alt+shift+del",
       fontAwesomeIcon: "trash",
       defaultShowOnActionBar: false
     }),
@@ -48,21 +43,18 @@ export const BuildEncounterCommandList = (
       id: "clean-encounter",
       description: "Clean Encounter",
       actionBinding: c.CleanEncounter,
-      defaultKeyBinding: "alt+del",
       fontAwesomeIcon: "broom"
     }),
     new Command({
       id: "open-library",
       description: "Open Library",
       actionBinding: c.ShowLibraries,
-      defaultKeyBinding: "alt+a",
       fontAwesomeIcon: "book"
     }),
     new Command({
       id: "roll-dice",
       description: "Roll Dice",
       actionBinding: c.PromptRollDice,
-      defaultKeyBinding: "d",
       fontAwesomeIcon: "dice",
       defaultShowOnActionBar: false
     }),
@@ -70,7 +62,6 @@ export const BuildEncounterCommandList = (
       id: "quick-add",
       description: "Quick Add Combatant",
       actionBinding: c.QuickAddStatBlock,
-      defaultKeyBinding: "alt+q",
       fontAwesomeIcon: "bolt",
       defaultShowOnActionBar: false
     }),
@@ -78,7 +69,6 @@ export const BuildEncounterCommandList = (
       id: "restore-all-player-character-hp",
       description: "Restore all Player Character HP",
       actionBinding: c.RestoreAllPlayerCharacterHP,
-      defaultKeyBinding: "alt+shift+t",
       fontAwesomeIcon: "clinic-medical",
       defaultShowOnActionBar: false
     }),
@@ -86,7 +76,6 @@ export const BuildEncounterCommandList = (
       id: "player-window",
       description: "Launch Player View",
       actionBinding: c.LaunchPlayerView,
-      defaultKeyBinding: "alt+w",
       fontAwesomeIcon: "users"
     }),
     typeof document.documentElement.requestFullscreen == "function" &&
@@ -94,7 +83,6 @@ export const BuildEncounterCommandList = (
         id: "toggle-full-screen",
         description: "Toggle Full Screen",
         actionBinding: c.ToggleFullScreen,
-        defaultKeyBinding: "f11",
         fontAwesomeIcon: "expand",
         defaultShowOnActionBar: false
       }),
@@ -102,14 +90,12 @@ export const BuildEncounterCommandList = (
       id: "next-turn",
       description: "Next Turn",
       actionBinding: c.NextTurn,
-      defaultKeyBinding: "n",
       fontAwesomeIcon: "step-forward"
     }),
     new Command({
       id: "previous-turn",
       description: "Previous Turn",
       actionBinding: c.PreviousTurn,
-      defaultKeyBinding: "alt+n",
       fontAwesomeIcon: "step-backward",
       defaultShowOnActionBar: false
     }),
@@ -117,14 +103,12 @@ export const BuildEncounterCommandList = (
       id: "save-encounter",
       description: "Save Encounter",
       actionBinding: saveEncounterFn,
-      defaultKeyBinding: "alt+s",
       fontAwesomeIcon: "save"
     }),
     new Command({
       id: "settings",
       description: "Settings",
       actionBinding: c.ShowSettings,
-      defaultKeyBinding: "?",
       fontAwesomeIcon: "cog",
       lockOnActionBar: true
     })
