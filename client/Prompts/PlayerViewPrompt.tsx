8ee22d765ce12e2536199b13c9ff495a49fdfde4
Evan Bailey <baileyevang@gmail.com>
2020-05-10 22:28:22 -0700

    use function component

--- client/Prompts/PlayerViewPrompt.tsx
+++ client/Prompts/PlayerViewPrompt.tsx
@@ -3,6 +3,8 @@ import * as React from "react";
 import { Button, SubmitButton } from "../Components/Button";
 import { env } from "../Environment";
 import { PromptProps } from "./PendingPrompts";
+import { useRef } from "react";
+import { useCallback } from "react";
 
 const promptClassName = "p-launch-player-view";
 const inputClassName = promptClassName + "-button";
@@ -11,72 +13,75 @@ interface PlayerViewPromptComponentProps {
   encounterId: string;
 }
 
-class PlayerViewPromptComponent extends React.Component<
-  PlayerViewPromptComponentProps
-> {
-  private hiddenInput: HTMLInputElement;
+function PlayerViewPromptComponent(props: PlayerViewPromptComponentProps) {
+  const hiddenInput = useRef<HTMLInputElement>(null);
 
-  public render() {
-    const playerViewUrl = `${env.BaseUrl}/p/${this.props.encounterId}`;
-    return (
-      <>
-        <div className="launch-player-view">
-          <input
-            className="hidden-input"
-            readOnly
-            value={playerViewUrl}
-            ref={e => (this.hiddenInput = e)}
-          />
+  const copyUrlToClipboard = useCallback(
+    (e: React.MouseEvent<HTMLButtonElement>) => {
+      if (!hiddenInput.current) {
+        return;
+      }
+
+      hiddenInput.current.select();
+      document.execCommand("copy");
+      const button = e.target as HTMLButtonElement;
+      button.focus();
+    },
+    [hiddenInput.current]
+  );
+
+  const openPlayerViewWindow = useCallback(() => {
+    window.open(`/p/${props.encounterId}`, "Player View");
+  }, [props.encounterId]);
+
+  const playerViewUrl = `${env.BaseUrl}/p/${props.encounterId}`;
+  return (
+    <>
+      <div className="launch-player-view">
+        <input
+          className="hidden-input"
+          readOnly
+          value={playerViewUrl}
+          ref={hiddenInput}
+        />
+        <p>
+          {`Player View launched. Encounter ID: `}
+          <strong>{props.encounterId}</strong>
+        </p>
+        <Button
+          fontAwesomeIcon="copy"
+          text="Copy URL to clipboard"
+          onClick={copyUrlToClipboard}
+        />
+        <Button
+          fontAwesomeIcon="external-link-alt"
+          text="Open Player View in new window"
+          onClick={openPlayerViewWindow}
+          additionalClassNames={inputClassName}
+        />
+        {env.HasEpicInitiative ? (
+          <label>
+            {"Background Image URL: "}
+            <Field type="text" name="backgroundImageUrl" />
+          </label>
+        ) : (
           <p>
-            {`Player View launched. Encounter ID: `}
-            <strong>{this.props.encounterId}</strong>
-          </p>
-          <Button
-            fontAwesomeIcon="copy"
-            text="Copy URL to clipboard"
-            onClick={this.copyUrlToClipboard}
-          />
-          <Button
-            fontAwesomeIcon="external-link-alt"
-            text="Open Player View in new window"
-            onClick={this.openPlayerViewWindow}
-            additionalClassNames={inputClassName}
-          />
-          {env.HasEpicInitiative ? (
             <label>
-              {"Background Image URL: "}
-              <Field type="text" name="backgroundImageUrl" />
+              Epic Initiative patrons can set a background image for your Player
+              View.{" "}
             </label>
-          ) : (
-            <p>
-              <label>
-                Epic Initiative patrons can set a background image for your
-                Player View.{" "}
-              </label>
-              <a
-                href="https://www.patreon.com/bePatron?c=716070&amp;rid=1937132"
-                target="_blank"
-              >
-                Pledge on Patreon
-              </a>
-            </p>
-          )}
-        </div>
-        <SubmitButton />
-      </>
-    );
-  }
-
-  private copyUrlToClipboard = (e: React.MouseEvent<HTMLButtonElement>) => {
-    this.hiddenInput.select();
-    document.execCommand("copy");
-    const button = e.target as HTMLButtonElement;
-    button.focus();
-  };
-
-  private openPlayerViewWindow = () => {
-    window.open(`/p/${this.props.encounterId}`, "Player View");
-  };
+            <a
+              href="https://www.patreon.com/bePatron?c=716070&amp;rid=1937132"
+              target="_blank"
+            >
+              Pledge on Patreon
+            </a>
+          </p>
+        )}
+      </div>
+      <SubmitButton />
+    </>
+  );
 }
 
 interface PlayerViewPromptModel {
