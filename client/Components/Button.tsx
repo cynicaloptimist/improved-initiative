c60edb56c00a01642a517c65e94f6c8795c2c77f
Evan Bailey <baileyevang@gmail.com>
2020-05-14 23:21:41 -0700

    use ternary expression for react happy

--- client/Components/Button.tsx
+++ client/Components/Button.tsx
@@ -39,8 +39,8 @@ export function Button(props: ButtonProps) {
     <button
       type={props.type ?? "button"}
       className={classNames.join(" ")}
-      onClick={!props.disabled && props.onClick}
-      onMouseOver={!props.disabled && props.onMouseOver}
+      onClick={props.disabled ? undefined : props.onClick}
+      onMouseOver={props.disabled ? undefined : props.onMouseOver}
       tabIndex={props.disabled ? -1 : 0}
     >
       {faElement}
