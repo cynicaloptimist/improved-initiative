820a2a25d2c443f4a3ee10cfcdd05def5f56bd2c
Evan Bailey <cynicaloptimist@gmail.com>
2021-02-28 23:16:43 -0800

    Use hooks api

--- client/Components/Tabs.tsx
+++ client/Components/Tabs.tsx
@@ -1,26 +1,20 @@
 import * as React from "react";
 
-interface TabsProps<T> {
+export function Tabs<T>(props: {
   options: T[];
   selected?: T;
   onChoose: (option: T) => void;
-}
+}) {
+  const buttonElements = props.options.map((option, i) => (
+    <button
+      type="button"
+      key={i}
+      className={props.selected == option ? "c-tab s-selected" : "c-tab"}
+      onClick={() => props.onChoose(option)}
+    >
+      {option}
+    </button>
+  ));
 
-interface TabsState {}
-
-export class Tabs<T> extends React.Component<TabsProps<T>, TabsState> {
-  public render() {
-    const buttonElements = this.props.options.map((option, i) => (
-      <button
-        type="button"
-        key={i}
-        className={this.props.selected == option ? "c-tab s-selected" : "c-tab"}
-        onClick={() => this.props.onChoose(option)}
-      >
-        {option}
-      </button>
-    ));
-
-    return <div className="c-tabs">{buttonElements}</div>;
-  }
+  return <div className="c-tabs">{buttonElements}</div>;
 }
