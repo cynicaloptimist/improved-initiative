28a015c3da14029573031094386215288598e11d
Evan Bailey <cynicaloptimist@gmail.com>
2021-05-17 21:59:21 -0700

    Submit move prompt on enter

--- client/Library/Manager/MovePrompt.tsx
+++ client/Library/Manager/MovePrompt.tsx
@@ -10,32 +10,37 @@ export function MovePrompt(props: {
   done: () => void;
 }) {
   const inputRef = React.useRef<HTMLInputElement>();
+  const submit = async () => {
+    if (!inputRef.current) {
+      return;
+    }
+    const pathInput = inputRef.current.value;
+    await Promise.all(
+      props.targets.map(async targetListing => {
+        const item = await props.library.GetItemById(targetListing.Meta().Id);
+        item.Path = pathInput;
+        return await props.library.SaveEditedListing(targetListing, item);
+      })
+    );
+    props.done();
+  };
+
   return (
     <div
       className="prompt"
       style={{ flexDirection: "row", alignItems: "center" }}
     >
       <span style={{ flexGrow: 1 }}>Move selected items to folder</span>
-      <input autoFocus ref={inputRef} />
-      <Button
-        fontAwesomeIcon="check"
-        onClick={async () => {
-          if (!inputRef.current) {
-            return;
+      <input
+        autoFocus
+        ref={inputRef}
+        onKeyDown={keyboardEvent => {
+          if (keyboardEvent.key === "Enter") {
+            submit();
           }
-          const pathInput = inputRef.current.value;
-          await Promise.all(
-            props.targets.map(async targetListing => {
-              const item = await props.library.GetItemById(
-                targetListing.Meta().Id
-              );
-              item.Path = pathInput;
-              return await props.library.SaveEditedListing(targetListing, item);
-            })
-          );
-          props.done();
         }}
       />
+      <Button fontAwesomeIcon="check" onClick={submit} />
     </div>
   );
 }
