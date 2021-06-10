fc4f4a864d4c7deaf21581b16e39746cbe02fb50
5eMagic <stinklikeonion@5emagic.shop>
2021-06-10 13:43:50 -0500

    adds a simple test to ensure custom classes are handled by prompt

--- client/Commands/PromptQueue.test.ts
+++ client/Commands/PromptQueue.test.ts
@@ -10,6 +10,16 @@ function MockPrompt(): PromptProps<{}> {
   };
 }
 
+function MockPromptWithClass(): PromptProps<{}> {
+  return {
+    autoFocusSelector: "",
+    children: null,
+    initialValues: {},
+    onSubmit: jest.fn(() => true),
+    className: "custom-class"
+  };
+}
+
 describe("PromptQueue", () => {
   it("Can list prompts", () => {
     const promptQueue = new PromptQueue();
@@ -24,5 +34,12 @@ describe("PromptQueue", () => {
     const promptId = promptQueue.Add(prompt);
     promptQueue.Remove(promptId);
     expect(prompt.onSubmit).not.toHaveBeenCalled();
+  });
+
+  it("Recognizes custom class names", () => {
+    const promptQueue = new PromptQueue();
+    const prompt = MockPromptWithClass();
+    promptQueue.Add(prompt);
+    expect(promptQueue.GetPrompts()[0][0].className).toBe('custom-class');
   });
 });
