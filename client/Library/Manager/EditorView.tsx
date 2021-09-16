6c2756b0c63b1dc23931d3643e07e6f30c8848a0
Evan Bailey <cynicaloptimist@gmail.com>
2021-09-16 00:10:53 -0700

    extract render methods from EditorView

--- client/Library/Manager/EditorView.tsx
+++ client/Library/Manager/EditorView.tsx
@@ -10,12 +10,12 @@ import { GetDefaultForLibrary, LibraryType } from "../Libraries";
 import { Listing } from "../Listing";
 import { LibraryManagerProps } from "./LibraryManager";
 
-export function EditorView(
-  props: LibraryManagerProps & {
-    editorTypeAndTarget: [LibraryType, Listing<Listable>];
-    closeEditor: () => void;
-  }
-) {
+type EditorViewProps = LibraryManagerProps & {
+  editorTypeAndTarget: [LibraryType, Listing<Listable>];
+  closeEditor: () => void;
+};
+
+export function EditorView(props: EditorViewProps) {
   const [editorType, editorTarget] = props.editorTypeAndTarget;
   const [loadedTarget, loadTarget] = useState<Listable | null>(null);
   const defaultListing = GetDefaultForLibrary(editorType);
@@ -29,75 +29,98 @@ export function EditorView(
   }
 
   if (editorType === "StatBlocks") {
-    const statBlockListing = editorTarget as Listing<StatBlock>;
-    return (
-      <StatBlockEditor
-        statBlock={loadedTarget as StatBlock}
-        editorTarget="library"
-        onSave={newStatBlock =>
-          props.libraries.StatBlocks.SaveEditedListing(
-            statBlockListing,
-            newStatBlock
-          )
-        }
-        onDelete={() =>
-          props.libraries.StatBlocks.DeleteListing(statBlockListing.Meta().Id)
-        }
-        onSaveAsCharacter={statBlock =>
-          props.libraries.StatBlocks.SaveEditedListing(
-            statBlockListing,
-            statBlock
-          )
-        }
-        onSaveAsCopy={statBlock =>
-          props.libraries.StatBlocks.SaveNewListing(statBlock)
-        }
-        onClose={props.closeEditor}
-      />
-    );
+    return RenderStatBlockEditor(editorTarget, loadedTarget, props);
   }
 
   if (editorType === "PersistentCharacters") {
-    const persistentCharacter = loadedTarget as PersistentCharacter;
-    const hpDown =
-      persistentCharacter.StatBlock.HP.Value - persistentCharacter.CurrentHP;
-    return (
-      <StatBlockEditor
-        statBlock={persistentCharacter.StatBlock}
-        editorTarget="persistentcharacter"
-        onSave={(statBlock: StatBlock) =>
-          props.librariesCommander.UpdatePersistentCharacterStatBlockInLibraryAndEncounter(
-            persistentCharacter.Id,
-            statBlock,
-            hpDown
-          )
-        }
-        onDelete={() =>
-          props.libraries.PersistentCharacters.DeleteListing(
-            persistentCharacter.Id
-          )
-        }
-        currentListings={props.libraries.PersistentCharacters.GetAllListings()}
-        onClose={props.closeEditor}
-      />
-    );
+    return RenderPersistentCharacterEditor(loadedTarget, props);
   }
 
   if (editorType === "Spells") {
-    const spellListing = editorTarget as Listing<Spell>;
-    const spell = loadedTarget as Spell;
-
-    return (
-      <SpellEditor
-        spell={spell}
-        onSave={spell =>
-          props.libraries.Spells.SaveEditedListing(spellListing, spell)
-        }
-        onDelete={props.libraries.Spells.DeleteListing}
-        onClose={props.closeEditor}
-      />
-    );
+    return RenderSpellEditor(editorTarget, loadedTarget, props);
   }
 
   return <div>Editor: {editorTarget.Meta().Name}</div>;
+}
+
+function RenderStatBlockEditor(
+  editorTarget: Listing<Listable>,
+  loadedTarget: Listable,
+  props: EditorViewProps
+) {
+  const statBlockListing = editorTarget as Listing<StatBlock>;
+  return (
+    <StatBlockEditor
+      statBlock={loadedTarget as StatBlock}
+      editorTarget="library"
+      onSave={newStatBlock =>
+        props.libraries.StatBlocks.SaveEditedListing(
+          statBlockListing,
+          newStatBlock
+        )
+      }
+      onDelete={() =>
+        props.libraries.StatBlocks.DeleteListing(statBlockListing.Meta().Id)
+      }
+      onSaveAsCharacter={statBlock =>
+        props.libraries.StatBlocks.SaveEditedListing(
+          statBlockListing,
+          statBlock
+        )
+      }
+      onSaveAsCopy={statBlock =>
+        props.libraries.StatBlocks.SaveNewListing(statBlock)
+      }
+      onClose={props.closeEditor}
+    />
+  );
+}
+
+function RenderPersistentCharacterEditor(
+  loadedTarget: Listable,
+  props: EditorViewProps
+) {
+  const persistentCharacter = loadedTarget as PersistentCharacter;
+  const hpDown =
+    persistentCharacter.StatBlock.HP.Value - persistentCharacter.CurrentHP;
+  return (
+    <StatBlockEditor
+      statBlock={persistentCharacter.StatBlock}
+      editorTarget="persistentcharacter"
+      onSave={(statBlock: StatBlock) =>
+        props.librariesCommander.UpdatePersistentCharacterStatBlockInLibraryAndEncounter(
+          persistentCharacter.Id,
+          statBlock,
+          hpDown
+        )
+      }
+      onDelete={() =>
+        props.libraries.PersistentCharacters.DeleteListing(
+          persistentCharacter.Id
+        )
+      }
+      currentListings={props.libraries.PersistentCharacters.GetAllListings()}
+      onClose={props.closeEditor}
+    />
+  );
+}
+
+function RenderSpellEditor(
+  editorTarget: Listing<Listable>,
+  loadedTarget: Listable,
+  props: EditorViewProps
+) {
+  const spellListing = editorTarget as Listing<Spell>;
+  const spell = loadedTarget as Spell;
+
+  return (
+    <SpellEditor
+      spell={spell}
+      onSave={spell =>
+        props.libraries.Spells.SaveEditedListing(spellListing, spell)
+      }
+      onDelete={props.libraries.Spells.DeleteListing}
+      onClose={props.closeEditor}
+    />
+  );
 }
