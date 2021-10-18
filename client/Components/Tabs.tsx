cc55e2b891e86da2b20f1aa83636237d36c83133
Evan Bailey <cynicaloptimist@gmail.com>
2021-10-17 17:10:16 -0700

    rename type parameter

--- client/Components/Tabs.tsx
+++ client/Components/Tabs.tsx
@@ -1,20 +1,22 @@
 import * as React from "react";
 
-export function Tabs<T extends string>(props: {
-  optionNamesById: Record<T, string>;
-  selected?: T;
-  onChoose: (option: T) => void;
+export function Tabs<TKey extends string>(props: {
+  optionNamesById: Record<TKey, string>;
+  selected?: TKey;
+  onChoose: (option: TKey) => void;
 }) {
-  const buttonElements = Object.keys(props.optionNamesById).map((key: T, i) => (
-    <button
-      type="button"
-      key={key}
-      className={props.selected == key ? "c-tab s-selected" : "c-tab"}
-      onClick={() => props.onChoose(key)}
-    >
-      {props.optionNamesById[key]}
-    </button>
-  ));
+  const buttonElements = Object.keys(props.optionNamesById).map(
+    (key: TKey, i) => (
+      <button
+        type="button"
+        key={key}
+        className={props.selected == key ? "c-tab s-selected" : "c-tab"}
+        onClick={() => props.onChoose(key)}
+      >
+        {props.optionNamesById[key]}
+      </button>
+    )
+  );
 
   return <div className="c-tabs">{buttonElements}</div>;
 }
