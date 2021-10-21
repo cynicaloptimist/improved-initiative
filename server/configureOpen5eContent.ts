61b57bfec66462085b76579559842f46516f13e2
Evan Bailey <cynicaloptimist@gmail.com>
2021-10-20 22:39:44 -0700

    Fall back to classic server if open5e is down

--- server/configureOpen5eContent.ts
+++ server/configureOpen5eContent.ts
@@ -30,20 +30,25 @@ export function configureOpen5eContent(app: express.Application) {
   const includeFields =
     "name,slug,size,type,subtype,alignment,challenge_rating,document__title,document__slug";
 
-  const sourceUrl = `https://api.open5e.com/monsters/?limit=1500&fields=${includeFields}`;
+  const sourceUrl = `https://apix.open5e.com/monsters/?limit=1500&fields=${includeFields}`;
 
   let basicRulesListings: ListingMeta[] = [];
   let additionalListings: ListingMeta[] = [];
-  axios.get(sourceUrl).then(response => {
-    if (Array.isArray(response?.data?.results)) {
-      const [basicRulesResults, additionalResults] = _.partition(
-        response.data.results,
-        r => r.document__slug == "wotc-srd"
-      );
-      basicRulesListings = basicRulesResults.map(getMeta);
-      additionalListings = additionalResults.map(getMeta);
-    }
-  });
+  axios
+    .get(sourceUrl)
+    .then(response => {
+      if (Array.isArray(response?.data?.results)) {
+        const [basicRulesResults, additionalResults] = _.partition(
+          response.data.results,
+          r => r.document__slug == "wotc-srd"
+        );
+        basicRulesListings = basicRulesResults.map(getMeta);
+        additionalListings = additionalResults.map(getMeta);
+      }
+    })
+    .catch(error => {
+      console.warn("Could not cache open5e index: ", error);
+    });
 
   app.get("/open5e/basicrules/", (req: Req, res: Res) => {
     res.json(basicRulesListings);
