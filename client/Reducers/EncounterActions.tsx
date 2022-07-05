f3ff6e35fe88843321d86cdf7431737830eca8d0
Evan Bailey <cynicaloptimist@gmail.com>
2022-07-04 18:39:00 -0700

    notes: encounter actions and combatant actions

---
+++ client/Reducers/EncounterActions.tsx
@@ -0,0 +1,29 @@
+/* 
+    >EncounterActions
+    AddCombatantFromStatBlock
+    AddCombatantFromState
+    RemoveCombatant
+    StartEncounter
+    EndEncounter
+    ClearEncounter
+    CleanEncounter
+    RestoreAllPlayerCharacterHP
+    NextTurn
+    PreviousTurn
+
+    >CombatantActions
+    SetStatBlock
+    ApplyDamage
+    ApplyHealing
+    ApplyTemporaryHP
+    SetInitiative
+    LinkInitiative
+    AddTag
+    RemoveTag
+    UpdateNotes
+    SetAlias
+    ToggleHidden
+    ToggleRevealedAC
+    MoveDown
+    MoveUp
+*/
