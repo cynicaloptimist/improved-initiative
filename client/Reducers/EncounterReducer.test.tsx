0e26227a361303bd3861a792d9f481d88ad49f53
Evan Bailey <cynicaloptimist@gmail.com>
2022-07-17 19:30:28 -0700

    Add tests for StartEncounter

--- client/Reducers/EncounterReducer.test.tsx
+++ client/Reducers/EncounterReducer.test.tsx
@@ -2,6 +2,8 @@ import { EncounterState } from "../../common/EncounterState";
 import { CombatantState } from "../../common/CombatantState";
 import { StatBlock } from "../../common/StatBlock";
 import { EncounterReducer } from "./EncounterReducer";
+import { initial } from "lodash";
+import { InitializeCombatantFromStatBlock } from "./InitializeCombatantFromStatBlock";
 
 describe("EncounterReducer", () => {
   it("Should add combatant from StatBlock", () => {
@@ -17,5 +19,35 @@ describe("EncounterReducer", () => {
 
     expect(insertedCombatant.StatBlock.Name).toEqual("Goblin");
     expect(insertedCombatant.Id).toEqual("goblinId");
+  });
+
+  it("Should sort combatants by initiative when starting the encounter", () => {
+    const statBlock = StatBlock.Default();
+    statBlock.Name = "Goblin";
+    const initialState = EncounterState.Default<CombatantState>();
+    initialState.Combatants = [
+      InitializeCombatantFromStatBlock(statBlock, "fast"),
+      InitializeCombatantFromStatBlock(statBlock, "medium"),
+      InitializeCombatantFromStatBlock(statBlock, "slow")
+    ];
+
+    const updatedEncounter = EncounterReducer(initialState, {
+      type: "StartEncounter",
+      payload: {
+        initiativesByCombatantId: {
+          medium: 5,
+          fast: 10,
+          slow: 1
+        }
+      }
+    });
+
+    expect(updatedEncounter.Combatants.map(c => c.Id)).toEqual([
+      "fast",
+      "medium",
+      "slow"
+    ]);
+
+    expect(updatedEncounter.ActiveCombatantId).toEqual("fast");
   });
 });
