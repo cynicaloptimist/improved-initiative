71714f4d317e9e80f597d71e01f6e1a5fe767423
Evan Bailey <cynicaloptimist@gmail.com>
2023-03-30 23:23:00 -0700

    Upgrade webpack and polyfill path

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -1,25 +1,29 @@
-const path = require('path');
+const path = require("path");
 const appVersion = require("./package.json").version;
 
 module.exports = {
-  entry: './client/Index.ts',
+  entry: "./client/Index.ts",
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: 'ts-loader',
+        use: "ts-loader",
         exclude: /node_modules/
       },
       {
         test: /\.md$/,
-        use: 'raw-loader'
-      }]
+        use: "raw-loader"
+      }
+    ]
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.js']
+    extensions: [".tsx", ".ts", ".js"],
+    fallback: {
+      path: require.resolve("path-browserify")
+    }
   },
   output: {
-    filename: 'ImprovedInitiative.' + appVersion + '.js',
-    path: path.resolve(__dirname, 'public', 'js')
-  },
+    filename: "ImprovedInitiative." + appVersion + ".js",
+    path: path.resolve(__dirname, "public", "js")
+  }
 };
