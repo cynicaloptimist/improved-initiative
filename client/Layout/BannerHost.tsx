4a2dfd64e97af83f79d11aa5982f255737d75880
Evan Bailey <cynicaloptimist@gmail.com>
2023-07-31 20:30:34 -0700

    open banner link in new window

--- client/Layout/BannerHost.tsx
+++ client/Layout/BannerHost.tsx
@@ -8,7 +8,10 @@ export function BannerHost(): JSX.Element {
 
   return (
     <div className="footer-banner">
-      <a href="https://www.dmsguild.com/browse.php?filters=45680_0_0_0_0_0&affiliate_id=282190">
+      <a
+        href="https://www.dmsguild.com/browse.php?filters=45680_0_0_0_0_0&affiliate_id=282190"
+        target="_blank"
+      >
         <img
           src="https://www.dmsguild.com/images/site_resources/DMsGuild-GuildAdept-Banner-sm.png"
           alt="Guild Adept PDFs - Available exclusively @ Dungeon Masters Guild"
