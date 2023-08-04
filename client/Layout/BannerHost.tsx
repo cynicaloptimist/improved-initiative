bc715510152baa0c8c9ebcd3a1d3241255ef1862
Evan Bailey <cynicaloptimist@gmail.com>
2023-08-04 14:14:58 -0700

    Track banner clicks

--- client/Layout/BannerHost.tsx
+++ client/Layout/BannerHost.tsx
@@ -1,16 +1,21 @@
 import * as React from "react";
 import { env } from "../Environment";
+import { Metrics } from "../Utility/Metrics";
 
 export function BannerHost(): JSX.Element {
   if (env.IsLoggedIn && (env.HasStorage || env.HasEpicInitiative)) {
     return null;
   }
 
+  const href =
+    "https://www.dmsguild.com/browse.php?filters=45680_0_0_0_0_0&affiliate_id=282190";
+
   return (
     <div className="footer-banner">
       <a
-        href="https://www.dmsguild.com/browse.php?filters=45680_0_0_0_0_0&affiliate_id=282190"
+        href={href}
         target="_blank"
+        onClick={() => Metrics.TrackAnonymousEvent("BannerClick", { href })}
       >
         <img
           src="https://www.dmsguild.com/images/site_resources/DMsGuild-GuildAdept-Banner-sm.png"
