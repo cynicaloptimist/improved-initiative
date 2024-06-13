3c5ae6e9a48f30e58353701f5d8592b74568fa89
tomkoevoets <tomkoevoets1@gmail.com>
2024-06-13 13:19:59 +0200

    Update TutorialSteps.ts
       - added step 10 and 11

--- client/Tutorial/TutorialSteps.ts
+++ client/Tutorial/TutorialSteps.ts
@@ -23,111 +23,107 @@ function getLocation(element: HTMLElement) {
 
 export const TutorialSteps: TutorialStep[] = [
   // step 1, add creature
-  {
-    Message:
-      "Let's start by adding a few creatures to the encounter. <strong>Click on any creature</strong> to add one to the encounter pane.",
-    RaiseSelector: ".left-column, .prompt, .combatants, .listings, .c-tab:nth-child(1)",
-    AwaitAction: "CreatureAdded", 
-    CalculatePosition: elements => {
-      const location = getLocation(elements.item(0));
-      const left = location.left + location.width + 10;
-      const top = location.top + 200;
-      return { left, top };
-    },
-    HighlightSelector: ".libraries, .c-tab:nth-child(1)",
-  },
-  // step 2 - select characters tab
-  {
-    Message:
-      "When you're ready to add some adventurers, select the <strong>Characters</strong> tab at the top of the library.",
-    RaiseSelector: ".c-tab:nth-child(2) ",
-    AwaitAction: "SelectCharactersTab",
-    CalculatePosition: elements => {
-      const element = _.last(elements);
-      const location = getLocation(element);
-      const left = location.left + location.width + 10;
-      const top = location.top + 5;
-      return { left, top };
-    },
-    HighlightSelector: ".c-tab:nth-child(2)",
-  },
-  // step 3 - add sample characters
-  {
-    Message:
-      "It's easy to add your own player characters to Improved Initiative. For now, <strong>add a few sample characters</strong>.",
-    RaiseSelector: ".left-column, .combatants",
-    AwaitAction: "PlayerCharacterAdded",
-    CalculatePosition: elements => {
-      const location = getLocation(elements[0]);
-      const left = location.left + location.width + 10;
-      const top = location.top + 200;
-      return { left, top };
-    },
-    HighlightSelector: ".libraries",
-  },
-  // step 4 - select encounters tab
-  {
-    Message:
-      "Select the <strong>Encounters</strong> tab at the top of the library.",
-    RaiseSelector: ".c-tab:nth-child(3)",
-    AwaitAction: "SelectEncountersTab",
-    CalculatePosition: elements => {
-      const element = _.last(elements);
-      const location = getLocation(element);
-      const left = location.left + location.width + 10;
-      const top = location.top + 5;
-      return { left, top };
-    },
-    HighlightSelector: ".c-tab:nth-child(3)",
-  },
-  // step 5 - encounter tab explanation
-  {
-    Message:
-      "The Encounters tab is where you can <strong>save and load</strong> encounters.",
-    RaiseSelector: ".left-column",
-    CalculatePosition: elements => {
-      const element = _.last(elements);
-      const location = getLocation(element);
-      const left = location.left + location.width + 10;
-      const top = location.top + 5;
-      return { left, top };
-    },
-    HighlightSelector: ".libraries",
-  },
-  // step 6 - select spells tab
-  {
-    Message:
-      "Select the <strong>Spells</strong> tab at the top of the library.",
-    RaiseSelector: ".c-tab:nth-child(4)",
-    AwaitAction: "SelectSpellsTab",
-    CalculatePosition: elements => {
-      const element = _.last(elements);
-      const location = getLocation(element);
-      const left = location.left + location.width + 10;
-      const top = location.top + 5;
-      return { left, top };
-    },
-    HighlightSelector: ".c-tab:nth-child(4)",
-  },
-  // step 7 - spell tab explanation
-  {
-    Message:
-      "The Spells tab is where you can <strong>select and view</strong> spell information.",
-    RaiseSelector: ".left-column",
-    CalculatePosition: elements => {
-      const element = _.last(elements);
-      const location = getLocation(element);
-      const left = location.left + location.width + 10;
-      const top = location.top + 5;
-      return { left, top };
-    },
-    HighlightSelector: ".libraries",
-  },
-
-
-
-
-
+  // {
+  //   Message:
+  //     "Let's start by adding a few creatures to the encounter. <strong>Click on any creature</strong> to add one to the encounter pane.",
+  //   RaiseSelector: ".left-column, .prompt, .combatants, .listings, .c-tab:nth-child(1)",
+  //   AwaitAction: "CreatureAdded", 
+  //   CalculatePosition: elements => {
+  //     const location = getLocation(elements.item(0));
+  //     const left = location.left + location.width + 10;
+  //     const top = location.top + 200;
+  //     return { left, top };
+  //   },
+  //   HighlightSelector: ".libraries, .c-tab:nth-child(1)",
+  // },
+  // // step 2 - select characters tab
+  // {
+  //   Message:
+  //     "When you're ready to add some adventurers, select the <strong>Characters</strong> tab at the top of the library.",
+  //   RaiseSelector: ".c-tab:nth-child(2) ",
+  //   AwaitAction: "SelectCharactersTab",
+  //   CalculatePosition: elements => {
+  //     const element = _.last(elements);
+  //     const location = getLocation(element);
+  //     const left = location.left + location.width + 10;
+  //     const top = location.top + 5;
+  //     return { left, top };
+  //   },
+  //   HighlightSelector: ".c-tab:nth-child(2)",
+  // },
+  // // step 3 - add sample characters
+  // {
+  //   Message:
+  //     "It's easy to add your own player characters to Improved Initiative. For now, <strong>add a few sample characters</strong>.",
+  //   RaiseSelector: ".left-column, .combatants",
+  //   AwaitAction: "PlayerCharacterAdded",
+  //   CalculatePosition: elements => {
+  //     const location = getLocation(elements[0]);
+  //     const left = location.left + location.width + 10;
+  //     const top = location.top + 200;
+  //     return { left, top };
+  //   },
+  //   HighlightSelector: ".libraries",
+  // },
+  // // step 4 - select encounters tab
+  // {
+  //   Message:
+  //     "Select the <strong>Encounters</strong> tab at the top of the library.",
+  //   RaiseSelector: ".c-tab:nth-child(3)",
+  //   AwaitAction: "SelectEncountersTab",
+  //   CalculatePosition: elements => {
+  //     const element = _.last(elements);
+  //     const location = getLocation(element);
+  //     const left = location.left + location.width + 10;
+  //     const top = location.top + 5;
+  //     return { left, top };
+  //   },
+  //   HighlightSelector: ".c-tab:nth-child(3)",
+  // },
+  // // step 5 - encounter tab explanation
+  // {
+  //   Message:
+  //     "The Encounters tab is where you can <strong>save and load</strong> encounters.",
+  //   RaiseSelector: ".left-column",
+  //   CalculatePosition: elements => {
+  //     const element = _.last(elements);
+  //     const location = getLocation(element);
+  //     const left = location.left + location.width + 10;
+  //     const top = location.top + 5;
+  //     return { left, top };
+  //   },
+  //   HighlightSelector: ".libraries",
+  // },
+  // // step 6 - select spells tab
+  // {
+  //   Message:
+  //     "Select the <strong>Spells</strong> tab at the top of the library.",
+  //   RaiseSelector: ".c-tab:nth-child(4)",
+  //   AwaitAction: "SelectSpellsTab",
+  //   CalculatePosition: elements => {
+  //     const element = _.last(elements);
+  //     const location = getLocation(element);
+  //     const left = location.left + location.width + 10;
+  //     const top = location.top + 5;
+  //     return { left, top };
+  //   },
+  //   HighlightSelector: ".c-tab:nth-child(4)",
+  // },
+  // // step 7 - spell tab explanation
+  // {
+  //   Message:
+  //     "The Spells tab is where you can <strong>select and view</strong> spell information.",
+  //   RaiseSelector: ".left-column",
+  //   CalculatePosition: elements => {
+  //     const element = _.last(elements);
+  //     const location = getLocation(element);
+  //     const left = location.left + location.width + 10;
+  //     const top = location.top + 5;
+  //     return { left, top };
+  //   },
+  //   HighlightSelector: ".libraries",
+  // },
+  // step 8 - start encounter
   {
     Message:
       "Press 'alt-r' or <strong>click 'Start Encounter'</strong> to roll initiative.",
@@ -139,11 +135,13 @@ export const TutorialSteps: TutorialStep[] = [
       const left = location.left + location.width + 10;
       const top = location.top + 5;
       return { left, top };
-    }
+    },
+    HighlightSelector: ".c-button--start-encounter",
   },
+  // step 9 - initiative dialog
   {
     Message:
-      "Enter initiative rolls, or <strong>press enter</strong> to take the pre-rolled results.",
+      "Enter initiative rolls and <strong>press</strong> the button to continue.",
     RaiseSelector: ".prompt",
     AwaitAction: "CompleteInitiativeRolls",
     CalculatePosition: elements => {
@@ -156,11 +154,27 @@ export const TutorialSteps: TutorialStep[] = [
       const location = getLocation(element);
       const left = location.left;
       const top =
-        location.top -
+        location.top +
         (document.getElementsByClassName("tutorial")[0].clientHeight || 0 + 10);
       return { left, top };
-    }
+    },
+    HighlightSelector: ".prompt",
   },
+  // step 10 - view active combatant
+  {
+    Message:
+      "On the left, you can see the active combatant.",
+    RaiseSelector: ".left-column",
+    CalculatePosition: elements => {
+      const element = elements[0];
+      const location = getLocation(element);
+      const left = location.left + location.width + 10;
+      const top = location.top + 5;
+      return { left, top };
+    },
+    HighlightSelector: ".active-combatant",
+  },
+  // step 11 - select combatant
   {
     Message:
       "Select a combatant by clicking. You can select multiple combatants by holding the control key.",
@@ -171,7 +185,8 @@ export const TutorialSteps: TutorialStep[] = [
       const left = location.left + 5;
       const top = location.top + location.height + 10;
       return { left, top };
-    }
+    },
+    HighlightSelector: ".right-column",
   },
   {
     Message:
