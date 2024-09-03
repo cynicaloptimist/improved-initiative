f9190a2028de5f1c1646f3c50e317c882fda9be6
Evan Bailey <cynicaloptimist@gmail.com>
2024-09-02 20:28:57 -0700

    Use ErrorBoundary to catch both render-time and mount-time errors

--- client/Components/StatBlock.tsx
+++ client/Components/StatBlock.tsx
@@ -7,6 +7,7 @@ import {
 import { StatBlockHeader } from "./StatBlockHeader";
 import { useContext } from "react";
 import { LoadingIndicator } from "./LoadingIndicator";
+import ErrorBoundary from "./ErrorBoundary";
 
 interface StatBlockProps {
   statBlock: StatBlock;
@@ -16,216 +17,222 @@ interface StatBlockProps {
 }
 
 export function StatBlockComponent(props: StatBlockProps) {
+  return (
+    <ErrorBoundary
+      renderError={error => (
+        <div className="c-statblock">
+          <div>
+            <p>There was a problem with this StatBlock:</p>
+            <pre className="c-statblock__error">{error.toString()}</pre>
+            <p>Please open it in the StatBlock Editor to check your JSON</p>
+          </div>
+        </div>
+      )}
+    >
+      <StatBlockComponentNoError {...props} />
+    </ErrorBoundary>
+  );
+}
+
+function StatBlockComponentNoError(props: StatBlockProps) {
   const textEnricher = useContext(TextEnricherContext);
   const statBlock = props.statBlock;
 
-  try {
-    const modifierTypes = [
-      { name: "Saves", data: statBlock.Saves },
-      { name: "Skills", data: statBlock.Skills }
-    ];
+  const modifierTypes = [
+    { name: "Saves", data: statBlock.Saves },
+    { name: "Skills", data: statBlock.Skills }
+  ];
 
-    const keywordSetTypes = [
-      { name: "Senses", data: statBlock.Senses },
-      { name: "Damage Vulnerabilities", data: statBlock.DamageVulnerabilities },
-      { name: "Damage Resistances", data: statBlock.DamageResistances },
-      { name: "Damage Immunities", data: statBlock.DamageImmunities },
-      { name: "Condition Immunities", data: statBlock.ConditionImmunities },
-      { name: "Languages", data: statBlock.Languages }
-    ];
+  const keywordSetTypes = [
+    { name: "Senses", data: statBlock.Senses },
+    { name: "Damage Vulnerabilities", data: statBlock.DamageVulnerabilities },
+    { name: "Damage Resistances", data: statBlock.DamageResistances },
+    { name: "Damage Immunities", data: statBlock.DamageImmunities },
+    { name: "Condition Immunities", data: statBlock.ConditionImmunities },
+    { name: "Languages", data: statBlock.Languages }
+  ];
 
-    const powerTypes = [
-      { name: "Traits", data: statBlock.Traits },
-      { name: "Actions", data: statBlock.Actions },
-      { name: "Bonus Actions", data: statBlock.BonusActions },
-      { name: "Reactions", data: statBlock.Reactions },
-      { name: "Legendary Actions", data: statBlock.LegendaryActions },
-      { name: "Mythic Actions", data: statBlock.MythicActions }
-    ];
+  const powerTypes = [
+    { name: "Traits", data: statBlock.Traits },
+    { name: "Actions", data: statBlock.Actions },
+    { name: "Bonus Actions", data: statBlock.BonusActions },
+    { name: "Reactions", data: statBlock.Reactions },
+    { name: "Legendary Actions", data: statBlock.LegendaryActions },
+    { name: "Mythic Actions", data: statBlock.MythicActions }
+  ];
 
-    const headerEntries = (
-      <>
-        {props.hideName || (
-          <StatBlockHeader
-            name={statBlock.Name}
-            imageUrl={statBlock.ImageURL}
-            source={statBlock.Source}
-            type={statBlock.Type}
-          />
-        )}
+  const headerEntries = (
+    <>
+      {props.hideName || (
+        <StatBlockHeader
+          name={statBlock.Name}
+          imageUrl={statBlock.ImageURL}
+          source={statBlock.Source}
+          type={statBlock.Type}
+        />
+      )}
 
-        <hr />
-      </>
-    );
+      <hr />
+    </>
+  );
 
-    const statEntries = (
-      <>
-        <div className="AC">
-          <span className="stat-label">Armor Class</span>
-          <span className="stat-value">{statBlock.AC.Value}</span>
-          <span className="notes">
-            {textEnricher.EnrichText(statBlock.AC.Notes)}
-          </span>
-        </div>
+  const statEntries = (
+    <>
+      <div className="AC">
+        <span className="stat-label">Armor Class</span>
+        <span className="stat-value">{statBlock.AC.Value}</span>
+        <span className="notes">
+          {textEnricher.EnrichText(statBlock.AC.Notes)}
+        </span>
+      </div>
 
-        <div className="HP">
-          <span className="stat-label">Hit Points</span>
-          <span className="stat-value">{statBlock.HP.Value}</span>
-          <span className="notes">
-            {textEnricher.EnrichText(statBlock.HP.Notes)}
-          </span>
-        </div>
+      <div className="HP">
+        <span className="stat-label">Hit Points</span>
+        <span className="stat-value">{statBlock.HP.Value}</span>
+        <span className="notes">
+          {textEnricher.EnrichText(statBlock.HP.Notes)}
+        </span>
+      </div>
 
-        <div className="speed">
-          <span className="stat-label">Speed</span>
-          <span className="stat-value">
-            {statBlock.Speed.map((speed, i) => (
-              <span className="stat-value__item" key={"stat-value__speed-" + i}>
-                {speed}
-              </span>
-            ))}
-          </span>
-        </div>
-
-        <div className="Abilities">
-          {Object.keys(StatBlock.Default().Abilities).map(abilityName => {
-            const abilityScore = statBlock.Abilities[abilityName];
-            const abilityModifier =
-              textEnricher.GetEnrichedModifierFromAbilityScore(abilityScore);
-            return (
-              <div className="Ability" key={abilityName}>
-                <div className="stat-label">{abilityName}</div>
-                <span className={"score " + abilityName}>{abilityScore}</span>
-                <span className={"modifier " + abilityName}>
-                  {abilityModifier}
-                </span>
-              </div>
-            );
-          })}
-        </div>
-
-        <hr />
-
-        <div className="modifiers">
-          {modifierTypes
-            .filter(modifierType => modifierType.data.length > 0)
-            .map(modifierType => (
-              <div key={modifierType.name} className={modifierType.name}>
-                <span className="stat-label">{modifierType.name}</span>
-                {modifierType.data.map((modifier, i) => (
-                  <span className="stat-value" key={i + modifier.Name}>
-                    {modifier.Name}
-                    {textEnricher.EnrichModifier(modifier.Modifier)}{" "}
-                  </span>
-                ))}
-              </div>
-            ))}
-        </div>
-
-        <div className="keyword-sets">
-          {keywordSetTypes
-            .filter(keywordSetType => keywordSetType.data.length > 0)
-            .map(keywordSetType => (
-              <div key={keywordSetType.name} className={keywordSetType.name}>
-                <span className="stat-label">{keywordSetType.name}</span>
-                <span className="stat-value">
-                  <span className="stat-value__item">
-                    {keywordSetType.data.map((keyword, index) => {
-                      return (
-                        <span
-                          className="stat-value__item"
-                          key={`stat-value__${keywordSetType.name}-${index}`}
-                        >
-                          {keyword}
-                        </span>
-                      );
-                    })}
-                  </span>
-                </span>
-              </div>
-            ))}
-        </div>
-
-        {statBlock.Challenge && (
-          <div className="Challenge">
-            <span className="stat-label">
-              {statBlock.Player == "player" ? "Level" : "Challenge"}
+      <div className="speed">
+        <span className="stat-label">Speed</span>
+        <span className="stat-value">
+          {statBlock.Speed.map((speed, i) => (
+            <span className="stat-value__item" key={"stat-value__speed-" + i}>
+              {speed}
             </span>
-            <span className="stat-value">{statBlock.Challenge}</span>
-          </div>
-        )}
+          ))}
+        </span>
+      </div>
 
-        <hr />
-      </>
-    );
+      <div className="Abilities">
+        {Object.keys(StatBlock.Default().Abilities).map(abilityName => {
+          const abilityScore = statBlock.Abilities[abilityName];
+          const abilityModifier =
+            textEnricher.GetEnrichedModifierFromAbilityScore(abilityScore);
+          return (
+            <div className="Ability" key={abilityName}>
+              <div className="stat-label">{abilityName}</div>
+              <span className={"score " + abilityName}>{abilityScore}</span>
+              <span className={"modifier " + abilityName}>
+                {abilityModifier}
+              </span>
+            </div>
+          );
+        })}
+      </div>
 
-    const actionEntries = powerTypes
-      .filter(powerType => powerType?.data?.length > 0)
-      .map(powerType => (
-        <div key={powerType.name} className={powerType.name}>
-          <h4 className="stat-label">{powerType.name}</h4>
-          {powerType.data.map((power, j) => (
-            <div key={j + power.Name}>
-              {power.Name?.length ? (
-                <span className="stat-label">{power.Name}</span>
-              ) : null}
-              {power.Usage && <span className="stat-label">{power.Usage}</span>}
-              <span className="power-content">
-                {textEnricher.EnrichText(power.Content)}
+      <hr />
+
+      <div className="modifiers">
+        {modifierTypes
+          .filter(modifierType => modifierType.data.length > 0)
+          .map(modifierType => (
+            <div key={modifierType.name} className={modifierType.name}>
+              <span className="stat-label">{modifierType.name}</span>
+              {modifierType.data.map((modifier, i) => (
+                <span className="stat-value" key={i + modifier.Name}>
+                  {modifier.Name}
+                  {textEnricher.EnrichModifier(modifier.Modifier)}{" "}
+                </span>
+              ))}
+            </div>
+          ))}
+      </div>
+
+      <div className="keyword-sets">
+        {keywordSetTypes
+          .filter(keywordSetType => keywordSetType.data.length > 0)
+          .map(keywordSetType => (
+            <div key={keywordSetType.name} className={keywordSetType.name}>
+              <span className="stat-label">{keywordSetType.name}</span>
+              <span className="stat-value">
+                <span className="stat-value__item">
+                  {keywordSetType.data.map((keyword, index) => {
+                    return (
+                      <span
+                        className="stat-value__item"
+                        key={`stat-value__${keywordSetType.name}-${index}`}
+                      >
+                        {keyword}
+                      </span>
+                    );
+                  })}
+                </span>
               </span>
             </div>
           ))}
-          <hr />
-        </div>
-      ));
-
-    const description = statBlock.Description && (
-      <div className="Description">
-        {textEnricher.EnrichText(statBlock.Description)}
       </div>
-    );
 
-    let innerEntries;
-
-    if (props.isLoading) {
-      return (
-        <div className="c-statblock">
-          {headerEntries}
-          <LoadingIndicator />
+      {statBlock.Challenge && (
+        <div className="Challenge">
+          <span className="stat-label">
+            {statBlock.Player == "player" ? "Level" : "Challenge"}
+          </span>
+          <span className="stat-value">{statBlock.Challenge}</span>
         </div>
-      );
-    }
-    if (props.displayMode == "active") {
-      innerEntries = (
-        <>
-          {actionEntries}
-          {statEntries}
-        </>
-      );
-    } else {
-      innerEntries = (
-        <>
-          {statEntries}
-          {actionEntries}
-        </>
-      );
-    }
+      )}
+
+      <hr />
+    </>
+  );
+
+  const actionEntries = powerTypes
+    .filter(powerType => powerType?.data?.length > 0)
+    .map(powerType => (
+      <div key={powerType.name} className={powerType.name}>
+        <h4 className="stat-label">{powerType.name}</h4>
+        {powerType.data.map((power, j) => (
+          <div key={j + power.Name}>
+            {power.Name?.length ? (
+              <span className="stat-label">{power.Name}</span>
+            ) : null}
+            {power.Usage && <span className="stat-label">{power.Usage}</span>}
+            <span className="power-content">
+              {textEnricher.EnrichText(power.Content)}
+            </span>
+          </div>
+        ))}
+        <hr />
+      </div>
+    ));
+
+  const description = statBlock.Description && (
+    <div className="Description">
+      {textEnricher.EnrichText(statBlock.Description)}
+    </div>
+  );
+
+  let innerEntries;
+
+  if (props.isLoading) {
     return (
       <div className="c-statblock">
         {headerEntries}
-        {innerEntries}
-        {description}
-      </div>
-    );
-  } catch (error) {
-    return (
-      <div className="c-statblock">
-        <div>
-          <p>There was a problem with this StatBlock:</p>
-          <pre className="c-statblock__error">{error.toString()}</pre>
-          <p>Please open it in the StatBlock Editor to check your JSON</p>
-        </div>
+        <LoadingIndicator />
       </div>
     );
   }
+  if (props.displayMode == "active") {
+    innerEntries = (
+      <>
+        {actionEntries}
+        {statEntries}
+      </>
+    );
+  } else {
+    innerEntries = (
+      <>
+        {statEntries}
+        {actionEntries}
+      </>
+    );
+  }
+  return (
+    <div className="c-statblock">
+      {headerEntries}
+      {innerEntries}
+      {description}
+    </div>
+  );
 }
