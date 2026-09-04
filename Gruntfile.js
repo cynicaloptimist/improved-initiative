a37aeeeadb4a3bbfc0cbda18a51ec5ca0de3236a
Evan Bailey <cynicaloptimist@gmail.com>
2026-09-03 21:07:24 -0700

    build: cache production webpack output

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,6 @@
 const appVersion = require("./package.json").version;
 const { spawnSync } = require("child_process");
+const webpack = require("webpack");
 
 module.exports = function (grunt) {
   grunt.loadNpmTasks("grunt-webpack");
@@ -54,8 +55,33 @@ module.exports = function (grunt) {
     return result.status === 0;
   });
 
+  grunt.registerTask(
+    "webpack:build",
+    "Build the production client",
+    function () {
+      const done = this.async();
+      const compiler = webpack(require("./webpack.config.prod"));
+
+      compiler.run((error, stats) => {
+        if (error) {
+          grunt.log.error(error);
+        } else {
+          grunt.log.writeln(stats.toString({ colors: true }));
+        }
+
+        const failed = Boolean(error) || Boolean(stats && stats.hasErrors());
+        compiler.close(closeError => {
+          if (closeError) {
+            grunt.log.error(closeError);
+          }
+          done(!failed && !closeError);
+        });
+      });
+    }
+  );
+
   grunt.registerTask("build_dev", ["webpack:dev", "ts:server", "less"]);
-  grunt.registerTask("build_min", ["webpack:prod", "ts:server", "less"]);
+  grunt.registerTask("build_min", ["webpack:build", "ts:server", "less"]);
   grunt.registerTask("server_watch", ["ts:server", "watch"]);
   grunt.registerTask("default", ["build_dev", "watch"]);
 };
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -4,12 +4,22 @@ const baseConfig = require("./webpack.config.base");
 const merge = require("webpack-merge");
 
 module.exports = merge(baseConfig, {
+  cache: {
+    type: "filesystem",
+    buildDependencies: {
+      config: [__filename, require.resolve("./webpack.config.base")]
+    }
+  },
   mode: "production",
   optimization: {
     minimize: true,
     minimizer: [new TerserPlugin()]
   },
   plugins: [
+    new webpack.IgnorePlugin({
+      resourceRegExp: /^\.\/locale$/,
+      contextRegExp: /moment$/
+    }),
     new webpack.DefinePlugin({
       "process.env.NODE_ENV": JSON.stringify("production"),
       "process.env.VERSION": JSON.stringify(require("./package.json").version)
